875aca11ba90d32f235bcc079b869db60274e26a
Nick <nick.strayer@rstudio.com>
2021-07-27 11:34:05 -0400

    Styled GridCard titles a bit

--- src/components/GridCard/index.tsx
+++ src/components/GridCard/index.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 export function GridCard(props: { title: string; gridArea: string }) {
   return (
     <div className="grid-card" style={{ "--grid-area": props.gridArea }}>
-      <h3>{props.title}</h3>
+      <h3 className="grid-card--title">{props.title}</h3>
     </div>
   );
 }
