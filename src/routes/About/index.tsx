04e4c7217c945f1f552a0c09e734ea48732ddf1e
Nick <nick.strayer@rstudio.com>
2021-07-28 18:26:04 -0400

    Added simple styles for about page

--- src/routes/About/index.tsx
+++ src/routes/About/index.tsx
@@ -1,13 +1,14 @@
 import { GridLayoutTemplate } from "../../types";
+import classes from "./style.module.css";
 
 export function About(props: { layout: GridLayoutTemplate }) {
   return (
-    <>
-      <h2>Welcome to the Shiny Visual Layout Editor!</h2>
+    <div class={classes.about}>
+      <h1>Welcome to the Shiny Visual Layout Editor!</h1>
       <p>
         The currently selected layout is <strong>{props.layout.name}</strong>
       </p>
       <p>This is some informational text about what is going on here!</p>
-    </>
+    </div>
   );
 }
