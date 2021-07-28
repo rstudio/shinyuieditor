bf3d3b70ec4799d52d8354f92beae2c45b7a826f
Nick <nick.strayer@rstudio.com>
2021-07-28 14:22:46 -0400

    Added helper two-column grid component

--- src/components/GridContainer/index.tsx
+++ src/components/GridContainer/index.tsx
@@ -5,8 +5,15 @@ import classes from "./style.module.css";
 export const GridContainer: FunctionComponent<{
   defs: GridLayoutDef;
   className?: string;
-}> = ({ defs: { cols, rows, gap }, children, className }) => {
+  styles?: JSX.CSSProperties;
+}> = ({
+  defs: { cols, rows, gap },
+  children,
+  className,
+  styles: extraStyles,
+}) => {
   const styles: JSX.CSSProperties = {
+    ...extraStyles,
     "--gap": gap,
   };
   if (cols) styles.gridTemplateColumns = cols.join(" ");
@@ -19,5 +26,16 @@ export const GridContainer: FunctionComponent<{
     <div class={containerClass} style={styles}>
       {children}
     </div>
+  );
+};
+
+export const TwoColumnGrid: FunctionComponent = ({ children }) => {
+  return (
+    <GridContainer
+      defs={{ cols: ["1fr", "1fr"], gap: "0.5rem" }}
+      styles={{ alignItems: "center" }}
+    >
+      {children}
+    </GridContainer>
   );
 };
--- src/routes/LayoutEditor/index.tsx
+++ src/routes/LayoutEditor/index.tsx
@@ -1,22 +1,25 @@
+import { useState } from "preact/hooks";
 import { CssUnitInput } from "../../components/CssUnitInput";
 import { FakeBrowser } from "../../components/FakeBrowser";
 import { GridCard } from "../../components/GridCard";
-import { GridContainer } from "../../components/GridContainer";
+import { GridContainer, TwoColumnGrid } from "../../components/GridContainer";
 import { GridPreviewItem } from "../../components/GridPreviewItem";
+import { parseCSSMeasure } from "../../helper-scripts/css-helpers";
 import { GridLayoutTemplate } from "../../types";
 import classes from "./style.module.css";
 
 export default function LayoutEditor(props: { layout: GridLayoutTemplate }) {
   const { layout } = props;
 
-  const newGapSize = (newGap: string) => {
-    console.log(`New grid gap: ${newGap}`);
-  };
+  const [currentGap, updateGap] = useState(parseCSSMeasure(layout.gap));
 
   return (
     <div className={classes.editor}>
       <GridCard title="Settings" gridArea="settings">
-        <CssUnitInput startUnit={"fr"} startCount={2} onChange={newGapSize} />
+        <TwoColumnGrid>
+          <span> Grid Gap: </span>
+          <CssUnitInput startValue={currentGap} onChange={updateGap} />
+        </TwoColumnGrid>
       </GridCard>
       <GridCard title="Instructions" gridArea="instructions" />
       <GridCard title="Elements" gridArea="elements" />
