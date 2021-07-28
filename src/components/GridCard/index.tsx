3787888b82517ae314fed6f7920f1a033f6e567b
Nick <nick.strayer@rstudio.com>
2021-07-28 15:40:03 -0400

    Added optional icon for the grid card

--- src/components/GridCard/index.tsx
+++ src/components/GridCard/index.tsx
@@ -3,14 +3,20 @@ import classes from "./style.module.css";
 
 export const GridCard: FunctionComponent<{
   title?: string;
+  icon?: JSX.Element;
   gridArea: string;
-}> = ({ title, gridArea, children }) => {
+}> = ({ title, icon, gridArea, children }) => {
   return (
     <div
       className={title ? classes.cardWTitle : classes.cardNoTitle}
       style={{ "--grid-area": gridArea }}
     >
-      {title ? <h3 className={classes.title}>{title}</h3> : null}
+      {title ? (
+        <h3 className={classes.title}>
+          {icon}
+          {title}
+        </h3>
+      ) : null}
       <div class={classes.body}>{children}</div>
     </div>
   );
