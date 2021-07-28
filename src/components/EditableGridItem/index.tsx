7138dd396083c7d3ee52cf8ac5e24f59a5136e3a
Nick <nick.strayer@rstudio.com>
2021-07-28 17:59:50 -0400

    Added drag icons to the EditableGridItem component

--- src/components/EditableGridItem/index.tsx
+++ src/components/EditableGridItem/index.tsx
@@ -1,4 +1,51 @@
+import {
+  BottomLeftArrow,
+  BottomRightArrow,
+  DragIcon,
+  TopLeftArrow,
+  TopRightArrow,
+} from "../icons";
 import classes from "./style.module.css";
+
+type DragDir = "topLeft" | "topRight" | "bottomLeft" | "bottomRight" | "middle";
+
+const MoveIcon = ({ type }: { type: DragDir }) => {
+  switch (type) {
+    case "middle":
+      return (
+        <span class={classes.middle}>
+          <DragIcon />
+        </span>
+      );
+    case "topLeft":
+      return (
+        <span class={classes.topLeft}>
+          <TopLeftArrow />
+        </span>
+      );
+    case "topRight":
+      return (
+        <span class={classes.topRight}>
+          <TopRightArrow />
+        </span>
+      );
+    case "bottomLeft":
+      return (
+        <span class={classes.bottomLeft}>
+          <BottomLeftArrow />
+        </span>
+      );
+    case "bottomRight":
+      return (
+        <span class={classes.bottomRight}>
+          <BottomRightArrow />
+        </span>
+      );
+    default:
+      console.error("That shouldnt happen");
+      return null;
+  }
+};
 
 export function EditableGridItem(props: {
   rows: [number, number];
@@ -11,6 +58,12 @@ export function EditableGridItem(props: {
         "--cols": props.cols.join("/"),
         "--rows": props.rows.join("/"),
       }}
-    ></div>
+    >
+      <MoveIcon type={"middle"} />
+      <MoveIcon type={"topLeft"} />
+      <MoveIcon type={"topRight"} />
+      <MoveIcon type={"bottomLeft"} />
+      <MoveIcon type={"bottomRight"} />
+    </div>
   );
 }
