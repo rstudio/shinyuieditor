840b962e4110e717616b42e8abd0711ccb3a8f4a
Nick <nick.strayer@rstudio.com>
2021-07-29 11:28:56 -0400

    Added tract lines to the layout editor grid container

--- src/components/TheAppGridContainer/index.tsx
+++ src/components/TheAppGridContainer/index.tsx
@@ -1,21 +1,36 @@
 import { FunctionComponent, JSX } from "preact";
 import { GridLayoutDef } from "../../types";
 import { GridContainer } from "../GridContainer";
+import { GridItem } from "../GridItem";
 import classes from "./style.module.css";
 
 // A grid container that also displays a grid of all cells in background
 export const TheAppGridContainer: FunctionComponent<{
   defs: GridLayoutDef;
-  className?: string;
   styles?: JSX.CSSProperties;
-}> = ({ defs, children, className, styles: extraStyles }) => {
-  const { cols, rows, gap } = defs;
+}> = ({ defs, children, styles: extraStyles }) => {
+  const { cols = [], rows = [], gap } = defs;
 
-  let gridCells: Array<JSX.Element>;
+  const rowTractLines = rows.map((r, i) => (
+    <GridItem
+      cols={[1, -1]}
+      rows={[i + 1, i + 1]}
+      className={classes.rowTractBoundary}
+    />
+  ));
+  const colTractLines = cols.map((c, i) => (
+    <GridItem
+      rows={[1, -1]}
+      cols={[i + 1, i + 1]}
+      className={classes.colTractBoundary}
+    />
+  ));
 
   return (
-    <GridContainer defs={defs} styles={extraStyles}>
+    <GridContainer defs={defs} styles={{ ...extraStyles, "--gap": gap }}>
       {children}
+      {rowTractLines}
+      {colTractLines}
     </GridContainer>
   );
 };
