70bdb70ba85d6309a59892a9b60c07afd6270510
Nick <nick.strayer@rstudio.com>
2021-07-29 11:33:56 -0400

    Fixed extra tract added at end due to not subtracting by one from length

--- src/components/TheAppGridContainer/index.tsx
+++ src/components/TheAppGridContainer/index.tsx
@@ -11,14 +11,16 @@ export const TheAppGridContainer: FunctionComponent<{
 }> = ({ defs, children, styles: extraStyles }) => {
   const { cols = [], rows = [], gap } = defs;
 
-  const rowTractLines = rows.map((r, i) => (
+  // We need to make one less tract line that there are tracts because the
+  // controls only go between and there are N-1 spots between N elements
+  const rowTractLines = Array.from({ length: rows.length - 1 }, (_, i) => (
     <GridItem
       cols={[1, -1]}
       rows={[i + 1, i + 1]}
       className={classes.rowTractBoundary}
     />
   ));
-  const colTractLines = cols.map((c, i) => (
+  const colTractLines = Array.from({ length: cols.length - 1 }, (_, i) => (
     <GridItem
       rows={[1, -1]}
       cols={[i + 1, i + 1]}
