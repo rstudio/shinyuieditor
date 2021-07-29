51665aa1fde996b748099f45ef381a02f49c1119
Nick <nick.strayer@rstudio.com>
2021-07-29 18:33:56 -0400

    Updated GridCard component to have padding option with default instead of a single css declaration in the css class

--- src/components/GridCard/index.tsx
+++ src/components/GridCard/index.tsx
@@ -1,36 +1,39 @@
 import { ComponentChildren, JSX } from "preact";
+import { CSSMeasure } from "../../types";
 import { GridItem } from "../GridItem";
 import classes from "./style.module.css";
 
-type GridCardReqProps = {
+type GridCardCommonProps = {
   gridArea: string;
   children: ComponentChildren;
+  padding?: CSSMeasure;
 };
 
 export function GridCard(
-  props: GridCardReqProps & { title: string; icon?: JSX.Element }
+  props: GridCardCommonProps & { title: string; icon?: JSX.Element }
 ): JSX.Element;
 export function GridCard(
-  props: GridCardReqProps & { header: JSX.Element }
+  props: GridCardCommonProps & { header: JSX.Element }
 ): JSX.Element;
 export function GridCard(
-  props: GridCardReqProps & {
+  props: GridCardCommonProps & {
     title?: string;
     icon?: JSX.Element;
     header?: JSX.Element;
   }
 ) {
+  const { padding = "0.5rem" } = props;
   return (
     <GridItem className={classes.cardWHeader} gridArea={props.gridArea}>
       {"header" in props ? (
         props.header
       ) : (
-        <h3 className={classes.title}>
+        <h3 className={classes.title} style={{ padding }}>
           {props.icon}
           {props.title}
         </h3>
       )}
-      <div class={classes.body}>{props.children}</div>
+      <div style={{ padding }}>{props.children}</div>
     </GridItem>
   );
 }
