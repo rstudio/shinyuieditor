2dc184d28e26a8241cb65352db6e5db82aca03a4
Nick <nick.strayer@rstudio.com>
2021-07-29 13:41:04 -0400

    Changed signature of GridCard to use overloading and hopefully make type checking clearer

--- src/components/GridCard/index.tsx
+++ src/components/GridCard/index.tsx
@@ -2,22 +2,35 @@ import { ComponentChildren, JSX } from "preact";
 import { GridItem } from "../GridItem";
 import classes from "./style.module.css";
 
-export const GridCard = (props: {
+type GridCardReqProps = {
   gridArea: string;
   children: ComponentChildren;
-  title?: string;
-  icon?: JSX.Element;
-  header?: JSX.Element;
-}) => (
-  <GridItem className={classes.cardWHeader} gridArea={props.gridArea}>
-    {"header" in props ? (
-      props.header
-    ) : (
-      <h3 className={classes.title}>
-        {props.icon}
-        {props.title}
-      </h3>
-    )}
-    <div class={classes.body}>{props.children}</div>
-  </GridItem>
-);
+};
+
+export function GridCard(
+  props: GridCardReqProps & { title: string; icon?: JSX.Element }
+): JSX.Element;
+export function GridCard(
+  props: GridCardReqProps & { header: JSX.Element }
+): JSX.Element;
+export function GridCard(
+  props: GridCardReqProps & {
+    title?: string;
+    icon?: JSX.Element;
+    header?: JSX.Element;
+  }
+) {
+  return (
+    <GridItem className={classes.cardWHeader} gridArea={props.gridArea}>
+      {"header" in props ? (
+        props.header
+      ) : (
+        <h3 className={classes.title}>
+          {props.icon}
+          {props.title}
+        </h3>
+      )}
+      <div class={classes.body}>{props.children}</div>
+    </GridItem>
+  );
+}
