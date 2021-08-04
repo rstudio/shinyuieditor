7ddb21915534a0e72c5e3d119dc2620b0f742c5e
Nick <nick.strayer@rstudio.com>
2021-08-04 11:08:42 -0400

    Moved logic inside jsx for feedback rect

--- src/components/DragFeedbackRect/index.tsx
+++ src/components/DragFeedbackRect/index.tsx
@@ -2,18 +2,21 @@ import { DragPos } from "../../state-logic/drag-logic";
 import classes from "./style.module.css";
 
 export function DragFeedbackRect({ status }: { status: DragPos }) {
-  if (status)
-    return (
-      <div
-        className={classes.rect}
-        style={{
-          "--top": status.YStart + "px",
-          "--left": status.XStart + "px",
-          width: status.width,
-          height: status.height,
-        }}
-      ></div>
-    );
-
-  return <div className={classes.hidden}></div>;
+  return (
+    <>
+      {status ? (
+        <div
+          className={classes.rect}
+          style={{
+            "--top": status.YStart + "px",
+            "--left": status.XStart + "px",
+            width: status.width,
+            height: status.height,
+          }}
+        ></div>
+      ) : (
+        <div className={classes.hidden}></div>
+      )}
+    </>
+  );
 }
