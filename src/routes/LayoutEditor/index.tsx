37f7212abfe5953e3d7e40fe41f78d26de101428
Nick <nick.strayer@rstudio.com>
2021-08-09 15:57:00 -0400

    Brought the grid layout updating logic into a custom hook and took it out of the layout editor component

--- src/routes/LayoutEditor/index.tsx
+++ src/routes/LayoutEditor/index.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useRef } from "preact/hooks";
+import { useRef } from "preact/hooks";
 import { CssUnitInput } from "../../components/CssUnitInput";
 import { EditableGridItems } from "../../components/EditableGridItems";
 import { EditorGridContainer } from "../../components/EditorGridContainer";
@@ -7,7 +7,7 @@ import { EditorItemsListView } from "../../components/EditorItemsListView";
 import { EditorSettings, SettingPane } from "../../components/EditorSettings";
 import { GridTractControls } from "../../components/GridTractControls";
 import { DragFeedback, useDragHandler } from "../../state-logic/drag-logic";
-import { layoutUpdater } from "../../state-logic/layout-updating-logic";
+import { useGridLayoutState } from "../../state-logic/layout-updating-logic";
 import type { GridLayoutTemplate } from "../../types";
 import classes from "./style.module.css";
 
@@ -16,8 +16,9 @@ export default function LayoutEditor({
 }: {
   startingLayout: GridLayoutTemplate;
 }) {
-  const [layout, updateLayout] = useReducer(layoutUpdater, startingLayout);
-
+  const { layout, deleteItem, setTract, setGap } = useGridLayoutState(
+    startingLayout
+  );
   // We need a reference to the main parent element of everything so we can
   // attach event handlers for drag detection to it.
   const editorRef = useRef<HTMLDivElement>(null);
@@ -30,23 +31,13 @@ export default function LayoutEditor({
     <div className={classes.editor} ref={editorRef}>
       <EditorSettings>
         <SettingPane label={"Gap Size"}>
-          <CssUnitInput
-            value={gap}
-            onChange={(gap) => updateLayout({ type: "Set-Gap", gap })}
-          />
+          <CssUnitInput value={gap} onChange={setGap} />
         </SettingPane>
       </EditorSettings>
       <EditorInstructions />
-      <EditorItemsListView
-        items={items}
-        deleteItem={(name) => updateLayout({ type: "Delete-Item", name })}
-      />
+      <EditorItemsListView items={items} deleteItem={deleteItem} />
       <EditorGridContainer layout={layout} onDrag={startDrag}>
-        <GridTractControls
-          rows={rows}
-          cols={cols}
-          setTract={(tract) => updateLayout({ type: "Set-Tract", tract })}
-        />
+        <GridTractControls rows={rows} cols={cols} setTract={setTract} />
         <EditableGridItems items={items} onDrag={startDrag} />
         <DragFeedback dragState={dragState} />
       </EditorGridContainer>
--- src/state-logic/layout-updating-logic.ts
+++ src/state-logic/layout-updating-logic.ts
@@ -1,4 +1,10 @@
-import { GridItemDef, GridLayoutTemplate, TractValue } from "../types";
+import { useReducer } from "preact/hooks";
+import {
+  CSSMeasure,
+  GridItemDef,
+  GridLayoutTemplate,
+  TractValue,
+} from "../types";
 
 type LayoutUpdateActions =
   | {
@@ -18,57 +24,51 @@ type LayoutUpdateActions =
       itemDef: GridItemDef;
     };
 
-export type LayoutUpdateDispatch = (a: LayoutUpdateActions) => void;
-
 export const layoutUpdater = (
-  layout: GridLayoutTemplate,
+  oldLayout: GridLayoutTemplate,
   action: LayoutUpdateActions
 ) => {
+  // This is not a true copy as everything but the gap and name will be copied
+  // by ref so it may need to be updated once time-travel like state is added
+  const layout = { ...oldLayout };
+
   switch (action.type) {
     case "Set-Gap":
-      return { ...layout, gap: action.gap };
+      layout.gap = action.gap;
+      break;
+
     case "Set-Tract":
-      return setTractValue(layout, action.tract);
+      layout[action.tract.dir][action.tract.index] = action.tract.val;
+      break;
+
     case "Delete-Item":
-      return deleteItem(layout, action.name);
+      layout.items = layout.items.filter((item) => item.name !== action.name);
+      break;
+
     case "Move-Item":
-      return moveItem(layout, action.itemDef);
+      layout.items = layout.items.map((item) => {
+        if (item.name === action.itemDef.name) {
+          item.rows = action.itemDef.rows;
+          item.cols = action.itemDef.cols;
+        }
+        return item;
+      });
+      break;
+
     default:
       throw new Error("Unexpected action");
   }
-};
 
-const copyLayout = (oldLayout: GridLayoutTemplate) => {
-  // This is not a true copy as everything but the gap and name will be copied
-  // by ref so it may need to be updated once time-travel like state is added
-  return { ...oldLayout };
-};
-
-const setTractValue = (oldLayout: GridLayoutTemplate, tract: TractValue) => {
-  const layout = copyLayout(oldLayout);
-  const { dir, index, val } = tract;
-  layout[dir][index] = val;
   return layout;
 };
 
-const deleteItem = (oldLayout: GridLayoutTemplate, name: string) => {
-  const layout = copyLayout(oldLayout);
-  layout.items = layout.items.filter((item) => item.name !== name);
-  return layout;
-};
+export function useGridLayoutState(startingLayout: GridLayoutTemplate) {
+  const [layout, updateLayout] = useReducer(layoutUpdater, startingLayout);
 
-const moveItem = (
-  oldLayout: GridLayoutTemplate,
-  { name, rows, cols }: GridItemDef
-) => {
-  const layout = copyLayout(oldLayout);
-
-  layout.items = layout.items.map((item) => {
-    if (item.name === name) {
-      item.rows = rows;
-      item.cols = cols;
-    }
-    return item;
-  });
-  return layout;
-};
+  return {
+    layout,
+    deleteItem: (name: string) => updateLayout({ type: "Delete-Item", name }),
+    setTract: (tract: TractValue) => updateLayout({ type: "Set-Tract", tract }),
+    setGap: (gap: CSSMeasure) => updateLayout({ type: "Set-Gap", gap }),
+  };
+}
