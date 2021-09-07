31e5bf19f89f122fc7c5f0fd0fcb1224a12045ec
Nick <nick.strayer@rstudio.com>
2021-09-07 12:31:24 -0400

    Simplified the use of dragger for the new items on grid container

--- src/components/EditorGridContainer/index.tsx
+++ src/components/EditorGridContainer/index.tsx
@@ -1,28 +1,20 @@
 import type { FunctionComponent } from "preact";
-import { useEffect, useRef } from "preact/hooks";
 import { useGridDragger } from "../../state-logic/dragging/hooks";
 import { GridCard } from "../GridCard";
 import { GridCells } from "../GridCells/GridCells";
 import { GridTractControls } from "../GridTractControls";
-import { FakeBrowserBar } from "../TheFakeBrowserBar";
 import { RowAndColBoundaries } from "../RowAndColBoundaries";
+import { FakeBrowserBar } from "../TheFakeBrowserBar";
 import classes from "./style.module.css";
 
 // A grid container that also displays a grid of all cells in background
 export const EditorGridContainer: FunctionComponent = ({ children }) => {
   // Setup the new-item drag behavior
   const onMouseDown = useGridDragger({});
-  const containerRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    containerRef.current?.addEventListener("mousedown", onMouseDown);
-    () => {
-      containerRef.current?.removeEventListener("mousedown", onMouseDown);
-    };
-  }, []);
 
   return (
     <GridCard gridArea="editor" header={FakeBrowserBar} padding="0px">
-      <div ref={containerRef} className={classes.mainGridContainer}>
+      <div onMouseDown={onMouseDown} className={classes.mainGridContainer}>
         <RowAndColBoundaries />
         <GridTractControls />
         {children}
