fc2071ada81861d395543e5e562a322cb8ea30e3
Nick <nick.strayer@rstudio.com>
2021-09-07 14:58:03 -0400

    Cleaned up the list view a bit more

--- src/components/EditorItemsListView/index.tsx
+++ src/components/EditorItemsListView/index.tsx
@@ -4,19 +4,27 @@ import { GridCard } from "../GridCard";
 import { SvgIcon } from "../Icons";
 import classes from "./style.module.css";
 
-export const EditorItemsListView = ({itemNamesAtom, deleteItem}: {itemNamesAtom: ItemNamesAtom; deleteItem:(name: string)=> void}) => {
+export const EditorItemsListView = ({
+  itemNamesAtom,
+  deleteItem,
+}: {
+  itemNamesAtom: ItemNamesAtom;
+  deleteItem: (name: string) => void;
+}) => {
   const itemNames = useRecoilValue(itemNamesAtom);
   return (
-    <GridCard title="Items" icon={"items"} gridArea="items">
+    <GridCard title="Items" icon="items" gridArea="items">
       {itemNames.map((name) => (
-        <div key={name} className={`${classes.item} ${classes.isDeletable}`}>
-        <span style={{ justifySelf: "start" }}>{name}</span>
-        <button onClick={() => deleteItem(name)} title={`Delete ${name} item`}>
-          <SvgIcon name={"trashcan"} />
-        </button>
-      </div>
+        <div key={name} className={classes.item}>
+          {name}
+          <button
+            onClick={() => deleteItem(name)}
+            title={`Delete ${name} item`}
+          >
+            <SvgIcon name="trashcan" />
+          </button>
+        </div>
       ))}
     </GridCard>
   );
 };
-
