a9f4def99defea1925c490aa4aaccae549c69631
Nick <nick.strayer@rstudio.com>
2021-09-27 15:59:54 -0400

    Added back the no-text-selection style declaration for dragging

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,15 @@ const globalStyles = css`
     --card-header-height: 35px;
   }
 
+  .disable-text-selection * {
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently
+                          supported by Chrome, Edge, Opera and Firefox */
+  }
   * {
     box-sizing: border-box;
   }
