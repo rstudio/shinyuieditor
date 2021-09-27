457e5ee759d7192f6009c0677aed50262c61fcdf
Nick <nick.strayer@rstudio.com>
2021-09-27 15:57:44 -0400

    Switched the newer GridItemDiv styled component

--- src/components/EditableGridItem.tsx
+++ src/components/EditableGridItem.tsx
@@ -1,22 +1,11 @@
 /** @jsxImportSource @emotion/react */
-import styled from "@emotion/styled";
 import * as React from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { makeTractPos } from "../grid-helpers";
-import { GridItemDef } from "../GridTypes";
 import {
   GridItemAtom,
   setSelectedItemNameState,
 } from "../state-logic/gridItems";
-
-const ItemDiv = styled.div(
-  ({ startRow, endRow, startCol, endCol }: GridItemDef) => ({
-    gridColumn: makeTractPos(startCol, endCol),
-    gridRow: makeTractPos(startRow, endRow),
-    backgroundColor: "var(--color, rgba(34, 139, 34, 0.835))",
-    borderRadius: "var(--corner-radius)",
-  })
-);
+import { GridItemDiv } from "./GridItemDiv";
 
 export default function EditableGridItem({
   name,
@@ -29,6 +18,17 @@ export default function EditableGridItem({
   const itemDef = useRecoilValue(itemDefState);
 
   return (
-    <ItemDiv {...itemDef} onClick={() => setSelectedItem(name)} title={name} />
+    <GridItemDiv
+      css={{
+        backgroundColor: "var(--color, rgba(34, 139, 34, 0.835))",
+        borderRadius: "var(--corner-radius)",
+      }}
+      {...itemDef}
+      onClick={() => {
+        console.log(`Clicked item ${name}`);
+        setSelectedItem(name);
+      }}
+      title={name}
+    />
   );
 }
