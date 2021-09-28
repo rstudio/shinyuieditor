eabc7f5e0962a35b7742fc84c7fbc79cb6158615
Nick <nick.strayer@rstudio.com>
2021-09-28 16:32:00 -0400

    Added a super-basic running test

---
+++ cypress/integration/testing-the-tests.ts
@@ -0,0 +1,13 @@
+// testing-the-tests.ts created with Cypress
+/// <reference types="cypress" />
+
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(true);
+  });
+});
+export {};
