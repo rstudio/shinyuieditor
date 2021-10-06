c91bebd3c50e33fbc581d01e25cae049c8156206
Nick <nick.strayer@rstudio.com>
2021-10-06 15:17:33 -0400

    Added an RStudio button variant

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,23 @@
 import { extendTheme } from "@chakra-ui/react";
 import "./cssVariables.css";
+const rstudio_blue = "#75aadb";
+const rstudio_grey = "#404040";
+const rstudio_white = "#ffffff";
+
 export const theme = extendTheme({
   components: {
     Heading: {
       baseStyle: {
         fontWeight: "normal",
         color: "var(--rstudio-grey)",
+      },
+    },
+    Button: {
+      variants: {
+        main: {
+          bg: rstudio_blue,
+          color: rstudio_white,
+        },
       },
     },
   },
