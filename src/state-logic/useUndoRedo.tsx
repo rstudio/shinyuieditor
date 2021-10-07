567dda65bec785c9747cdccc6848ce421050acb7
Nick <nick.strayer@rstudio.com>
2021-10-07 11:44:26 -0400

    Moved logic for history stack into separate module

--- src/state-logic/useUndoRedo.tsx
+++ src/state-logic/useUndoRedo.tsx
@@ -2,99 +2,36 @@ import { GridLayoutTemplate } from "GridTypes";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { fullAppState } from "state-logic/gridLayout/atoms";
-
-type StateHistory = {
-  stack: GridLayoutTemplate[];
-  stepsBack: number;
-  lastRequested: GridLayoutTemplate | null;
-};
-
-const stateHistoryInit: StateHistory = {
-  stack: [],
-  stepsBack: 0,
-  lastRequested: null,
-};
+import {
+  addEntry,
+  getEntryFromHistory,
+  hasFutureState,
+  hasPreviousState,
+  StateHistory,
+  stateHistoryInit,
+} from "../modules/StateHistory";
 
 export function useUndoRedo() {
   const currentLayout = useRecoilValue(fullAppState);
+  const setUpNewLayout = useSetRecoilState(fullAppState);
+
   const [canGoForward, setCanGoForward] = useState(false);
   const [canGoBackward, setCanGoBackward] = useState(false);
-
-  const setUpNewLayout = useSetRecoilState(fullAppState);
   const stateHistory = useRef<StateHistory>(stateHistoryInit);
 
   useEffect(() => {
-    const isLayoutFromHistory = sameLayout(
-      currentLayout,
-      stateHistory.current.lastRequested
-    );
-
-    if (isEmptyTemplate(currentLayout)) {
-      // When initially loading an empty template is provided. We can just
-      // ignore this one entirely to not pollute the history
-      return;
-    }
-
-    if (isLayoutFromHistory) {
-      // We're looking at a previous state if steps back is non-zero
-      setCanGoForward(stateHistory.current.stepsBack > 0);
-      return;
-    }
-
-    // The user has modified the layout while back in history so we need to
-    // erase the future history as they've started a new branch
-    if (stateHistory.current.stepsBack > 0) {
-      stateHistory.current.stack = stateHistory.current.stack.slice(
-        0,
-        -stateHistory.current.stepsBack
-      );
-      stateHistory.current.stepsBack = 0;
-      setCanGoForward(false);
-    }
-
-    // Add latest history to the stack.
-    const noChangeInLayout = sameLayout(
-      currentLayout,
-      stateHistory.current.stack[stateHistory.current.stack.length - 1]
-    );
-    if (noChangeInLayout) {
-      console.log(
-        "Nothing has changed between steps so nothing is added to history stack"
-      );
-      return;
-    }
-
-    stateHistory.current.stack = [...stateHistory.current.stack, currentLayout];
+    addEntry(currentLayout, stateHistory.current);
+    setCanGoBackward(hasPreviousState(stateHistory.current));
+    setCanGoForward(hasFutureState(stateHistory.current));
   }, [currentLayout]);
-
-  useEffect(() => {
-    const numStatesRemaining =
-      stateHistory.current.stack.length - stateHistory.current.stepsBack - 1;
-    setCanGoBackward(
-      stateHistory.current.stack.length > 1 && numStatesRemaining > 0
-    );
-  }, [currentLayout]);
-
-  const goToHistoryEntry = useCallback(
-    (numStepsBackwards: number) => {
-      const numSnapshots = stateHistory.current.stack.length;
-      const newHistoryIndex = numSnapshots - numStepsBackwards - 1;
-      stateHistory.current.lastRequested =
-        stateHistory.current.stack[newHistoryIndex];
-      setUpNewLayout(stateHistory.current.lastRequested);
-    },
-    [setUpNewLayout]
-  );
 
   const goBackward = useCallback(() => {
-    stateHistory.current.stepsBack++;
-    goToHistoryEntry(stateHistory.current.stepsBack);
-  }, [goToHistoryEntry]);
+    setUpNewLayout(getEntryFromHistory(stateHistory.current, -1));
+  }, [setUpNewLayout]);
 
   const goForward = useCallback(() => {
-    stateHistory.current.stepsBack--;
-    goToHistoryEntry(stateHistory.current.stepsBack);
-  }, [goToHistoryEntry]);
+    setUpNewLayout(getEntryFromHistory(stateHistory.current, 1));
+  }, [setUpNewLayout]);
 
   return {
     goBackward,
@@ -103,6 +40,7 @@ export function useUndoRedo() {
     canGoForward,
   };
 }
+
 function sameLayout(
   a: GridLayoutTemplate,
   b: GridLayoutTemplate | null
@@ -110,10 +48,4 @@ function sameLayout(
   if (b === null) return false;
 
   return JSON.stringify(a) === JSON.stringify(b);
-}
-
-function isEmptyTemplate(template?: GridLayoutTemplate) {
-  if (typeof template === "undefined") return true;
-  const { rows, cols, items } = template;
-  return rows.length === 0 && cols.length === 0 && items.length === 0;
 }
