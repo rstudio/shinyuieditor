cbc5c2a2f49f94269546ff40a845168cd6577559
Nick <nick.strayer@rstudio.com>
2021-10-07 13:48:35 -0400

    Removed the browser history api version of undo-redo

--- src/components/HistoryNav.tsx
+++ src/components/HistoryNav.tsx
@@ -2,7 +2,6 @@ import { IconButton } from "@chakra-ui/button";
 import { HStack } from "@chakra-ui/layout";
 import { FaRedo, FaUndo } from "react-icons/fa";
 import { useUndoRedo } from "../state-logic/useUndoRedo";
-// import { useUndoRedo } from "../state-logic/useUndoRedoHistory";
 
 export default function HistoryNav() {
   const { goBackward, goForward, canGoBackward, canGoForward } = useUndoRedo();
