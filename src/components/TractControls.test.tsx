fba53bba85c223c934848be7d09ce1ba90902150
Nick <nick.strayer@rstudio.com>
2021-10-13 11:10:21 -0400

    Added more tests for the tract controls related to deletion of tracts

--- src/components/TractControls.test.tsx
+++ src/components/TractControls.test.tsx
@@ -1,8 +1,10 @@
 import { screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { TractSizers } from "components/TractSizers";
-import { rowsState } from "state-logic/gridLayout/atoms";
+import { TractControls } from "components/TractControls";
+import { combinedItemsState } from "state-logic/gridItems";
+import { colsState, rowsState } from "state-logic/gridLayout/atoms";
 import { parseCSSMeasure } from "utils/css-helpers";
+import InfoModal from "views/InfoModal";
 import { CSSMeasure, GridItemDef, GridLayoutTemplate } from "../GridTypes";
 import { AppWLayout, RecoilObserver, renderWithRecoil } from "../test-helpers";
 
@@ -30,7 +32,7 @@ describe("Tract updaters properly update the state of the app.", () => {
     renderWithRecoil(
       <AppWLayout layout={LayoutToTest}>
         <RecoilObserver node={rowsState} onChange={onChange} />
-        <TractSizers dir="rows" />
+        <TractControls dir="rows" />
       </AppWLayout>
     );
 
@@ -64,7 +66,7 @@ describe("Tract updaters properly update the state of the app.", () => {
     renderWithRecoil(
       <AppWLayout layout={LayoutToTest}>
         <RecoilObserver node={rowsState} onChange={onChange} />
-        <TractSizers dir="rows" />
+        <TractControls dir="rows" />
       </AppWLayout>
     );
 
@@ -93,7 +95,7 @@ describe("Tract updaters properly update the state of the app.", () => {
     renderWithRecoil(
       <AppWLayout layout={LayoutToTest}>
         <RecoilObserver node={rowsState} onChange={onChange} />
-        <TractSizers dir="rows" />
+        <TractControls dir="rows" />
       </AppWLayout>
     );
 
@@ -117,5 +119,84 @@ describe("Tract updaters properly update the state of the app.", () => {
     userEvent.type(textInput, "{arrowdown}{arrowdown}");
     changedValues[0] = "3fr";
     expect(onChange).toHaveBeenLastCalledWith(changedValues);
+  });
+});
+
+describe("Deleting tracts", () => {
+  test("Deleting a tract with no items", () => {
+    LayoutToTest.rows = ["1fr", "2fr", "3fr"];
+    const onChange = jest.fn();
+
+    renderWithRecoil(
+      <AppWLayout layout={LayoutToTest}>
+        <RecoilObserver node={rowsState} onChange={onChange} />
+        <TractControls dir="rows" />
+      </AppWLayout>
+    );
+
+    userEvent.click(screen.getByLabelText(/delete row 0/i));
+    expect(onChange).toHaveBeenLastCalledWith(["2fr", "3fr"]);
+
+    // There's a new "row 0" now
+    userEvent.click(screen.getByLabelText(/delete row 0/i));
+    expect(onChange).toHaveBeenLastCalledWith(["3fr"]);
+  });
+
+  test("Items not fully contained in deleted tract get adjusted to new layout", () => {
+    const LayoutToTest: GridLayoutTemplate = {
+      name: "test",
+      rows: ["1fr", "2fr"],
+      cols: ["1fr", "1fr"],
+      gap: "2rem",
+      items: [
+        { name: "a", startRow: 1, endRow: 2, startCol: 1, endCol: 1 },
+        { name: "b", startRow: 1, endRow: 2, startCol: 2, endCol: 2 },
+      ] as GridItemDef[],
+    };
+
+    const onItemsChange = jest.fn();
+    const onRowsChange = jest.fn();
+
+    renderWithRecoil(
+      <AppWLayout layout={LayoutToTest}>
+        <RecoilObserver node={combinedItemsState} onChange={onItemsChange} />
+        <RecoilObserver node={rowsState} onChange={onRowsChange} />
+        <TractControls dir="rows" />
+      </AppWLayout>
+    );
+
+    userEvent.click(screen.getByLabelText(/delete row 0/i));
+    expect(onRowsChange).toHaveBeenLastCalledWith(["2fr"]);
+    expect(onItemsChange).toHaveBeenLastCalledWith([
+      { name: "a", startRow: 1, endRow: 1, startCol: 1, endCol: 1 },
+      { name: "b", startRow: 1, endRow: 1, startCol: 2, endCol: 2 },
+    ]);
+  });
+
+  test("Items fully contained in deleted tract will stop tract from being deleted", () => {
+    const LayoutToTest: GridLayoutTemplate = {
+      name: "test",
+      rows: ["1fr", "1fr"],
+      cols: ["1fr", "2fr"],
+      gap: "2rem",
+      items: [
+        { name: "a", startRow: 1, endRow: 2, startCol: 1, endCol: 1 },
+        { name: "b", startRow: 1, endRow: 2, startCol: 2, endCol: 2 },
+      ] as GridItemDef[],
+    };
+
+    const onColsChange = jest.fn();
+
+    renderWithRecoil(
+      <AppWLayout layout={LayoutToTest}>
+        <RecoilObserver node={colsState} onChange={onColsChange} />
+        <TractControls dir="cols" />
+        <InfoModal />
+      </AppWLayout>
+    );
+
+    userEvent.click(screen.getByLabelText(/delete column 0/i));
+    expect(onColsChange).toHaveBeenLastCalledWith(["1fr", "2fr"]);
+    expect(screen.getByRole("alertdialog")).toBeInTheDocument();
   });
 });
