756fd587ed34bc635e1c91f1de20943f9df1b356
Nick <nick.strayer@rstudio.com>
2021-10-27 10:04:44 -0400

    Added more full-fledged real-world tests

--- R/tests/testthat/test-is_known_ui_fn.R
+++ R/tests/testthat/test-is_known_ui_fn.R
@@ -1,4 +1,12 @@
 test_that("Works when passed the a proper call expression", {
+
+  expect_true(is_known_ui_fn(rlang::expr(gridlayout::grid_page(
+    layout = "|2rem |1fr    |
+            |80px |header |
+            |1fr  |plot   |",
+    header = gridlayout::title_panel("This is my header"),
+    plot = shiny::plotOutput("distPlot")
+  ))))
   expect_true(is_known_ui_fn(rlang::expr(gridlayout::grid_page())))
 
   expect_false(is_known_ui_fn(rlang::expr(my_custom_ui_page())))
@@ -16,4 +24,15 @@ test_that("Spits errors when the wrong type of input is passed", {
   testthat::expect_error(is_known_ui_fn(evaluated_ui))
 
   expect_error(is_known_ui_fn(rlang::expr(gridlayout::grid_page)))
+})
+
+test_that("Doesn't evaluate calls", {
+  expect_false(
+    is_known_ui_fn(rlang::expr(
+      my_undefined_ui_page(
+        "un-named arg",
+        a = "named arg"
+      )
+    ))
+  )
 })
