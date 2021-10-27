bf5410e90d99059d3ffaf8a0ce10a0856933c6a5
Nick <nick.strayer@rstudio.com>
2021-10-27 16:23:54 -0400

    Story is now smarter about how it lets you specify the area based upon the provided grid

--- src/stories/GridlayoutTitlePanel.stories.tsx
+++ src/stories/GridlayoutTitlePanel.stories.tsx
@@ -1,24 +1,30 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import GridlayoutTitlePanel from "components/shiny-ui/GridlayoutTitlePanel";
 import React from "react";
+import { buildGridTemplateAreas } from "utils/parseGridTemplateAreas";
 import { AreaLabeledGridHolder } from "../components/GridHolder";
+
+const layoutAreas = [
+  ["a", "a", "b", "other"],
+  ["c", "d", "d", "other"],
+];
+const { uniqueAreas } = buildGridTemplateAreas(layoutAreas);
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "ShinyUI/TitlePanel",
   component: GridlayoutTitlePanel,
   argTypes: {
-    title: { control: "text" },
+    area: {
+      control: { type: "select", options: uniqueAreas },
+    },
   },
 } as ComponentMeta<typeof GridlayoutTitlePanel>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof GridlayoutTitlePanel> = (args) => (
   <AreaLabeledGridHolder
-    areas={[
-      ["a", "a", "b"],
-      ["c", "d", "d"],
-    ]}
+    areas={layoutAreas}
     rowSizes={"200px"}
     colSizes={"200px"}
     gapSize={"1rem"}
@@ -31,5 +37,5 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   title: "My App Title",
-  area: "a",
+  area: uniqueAreas[0],
 };
