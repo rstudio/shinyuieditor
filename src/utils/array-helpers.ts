c2c9d93183ae6ee7081c143f5bfdf9e02486cf8e
Nick <nick.strayer@rstudio.com>
2021-10-27 17:00:50 -0400

    Added matrix dimension helper function

--- src/utils/array-helpers.ts
+++ src/utils/array-helpers.ts
@@ -6,8 +6,22 @@ export function removeAtIndex<T>(arr: T[], index: number): T[] {
   return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
 
+export type Matrix<T> = T[][];
+
+export function matrixDimensions<ElementType>(mat: Matrix<ElementType>) {
+  const numRows = mat.length;
+  const numCols = mat[0].length;
+
+  // Make sure the matrix is properly formed
+  for (let row of mat) {
+    if (row.length !== numCols)
+      throw new Error("Inconsistant number of columns in matrix");
+  }
+
+  return { numRows, numCols };
+}
 export function uniqueMatrixElements<ElementType>(
-  mat: ElementType[][]
+  mat: Matrix<ElementType>
 ): ElementType[] {
   const seen = new Set<ElementType>();
 
