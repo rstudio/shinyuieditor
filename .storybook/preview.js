e6f3499179267787c3364d5c9ce5c8b6fe1f25b6
Nick <nick.strayer@rstudio.com>
2021-11-01 17:52:49 -0400

    Attempted to add Chakra stuff for storybook components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { addDecorator } from "@storybook/react";
+import React from "react";
 import "../src/cssVariables.css";
+import { theme } from "../src/theme";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,3 +13,9 @@ export const parameters = {
     },
   },
 };
+
+addDecorator((storyFn) => (
+  <>
+    <ChakraProvider theme={theme}>{storyFn()}</ChakraProvider>
+  </>
+));
