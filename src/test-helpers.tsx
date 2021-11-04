a1c505b1e461452e97018e805384e1a36eb0e796
Nick <nick.strayer@rstudio.com>
2021-11-04 12:11:08 -0400

    Helper function to clear out text before typing in an input

--- src/test-helpers.tsx
+++ src/test-helpers.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React, { useEffect } from "react";
 import { RecoilRoot, RecoilValue, useRecoilValue } from "recoil";
 import { useInitializeToLayout } from "./state-logic/gridLayout/hooks";
@@ -29,4 +30,13 @@ export function RecoilObserver<T>({
   const value = useRecoilValue(node);
   useEffect(() => onChange(value), [onChange, value]);
   return null;
+}
+
+export function clearThenType(
+  inputOrig: HTMLInputElement | HTMLElement,
+  textToType: string | number
+) {
+  const input = inputOrig as HTMLInputElement;
+  input.setSelectionRange(0, input.value.length);
+  userEvent.type(input, String(textToType));
 }
