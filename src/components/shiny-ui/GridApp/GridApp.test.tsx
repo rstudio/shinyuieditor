c32e1bc0b6688b9195fd90cba7174749d5e6e1b0
Nick <nick.strayer@rstudio.com>
2021-11-05 16:34:57 -0400

    No need for extra import

--- src/components/shiny-ui/GridApp/GridApp.test.tsx
+++ src/components/shiny-ui/GridApp/GridApp.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { TemplatedGridProps } from "utils/parseGridTemplateAreas";
 import GridApp from ".";
 
