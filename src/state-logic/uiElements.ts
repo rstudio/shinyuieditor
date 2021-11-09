035d52a7c67ebca9af8e1fcb1b63c92c243c4b96
Nick <nick.strayer@rstudio.com>
2021-11-09 17:51:03 -0500

    Added selected ui element selector

--- src/state-logic/uiElements.ts
+++ src/state-logic/uiElements.ts
@@ -1,9 +1,26 @@
 import { ShinyUiNameAndProps } from "components/shiny-ui/componentTypes";
-import { atomFamily } from "recoil";
+import { atomFamily, selector } from "recoil";
+import { selectedItemNameState } from "./gridItems";
 
 export type UiElementAtom = ShinyUiNameAndProps | "unset";
 
 export const uiElementAtoms = atomFamily<UiElementAtom, string>({
   key: "uiElementAtoms",
   default: "unset",
+});
+
+export const selectedUiElement = selector<UiElementAtom | null>({
+  key: "selectedUiElement",
+  get: ({ get }) => {
+    const selectedItemName = get(selectedItemNameState);
+    if (!selectedItemName) return null;
+
+    return get(uiElementAtoms(selectedItemName));
+  },
+  set: ({ get, set }, newDef) => {
+    const selectedItemName = get(selectedItemNameState);
+    if (!selectedItemName || !newDef) return;
+
+    set(uiElementAtoms(selectedItemName), newDef);
+  },
 });
