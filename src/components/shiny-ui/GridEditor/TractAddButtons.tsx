dca3adba6311c78ba71eff2a65f8d6c72432833b
Nick <nick.strayer@rstudio.com>
2021-11-15 12:03:17 -0500

    Add first tract add button and place/style better

--- src/components/shiny-ui/GridEditor/TractAddButtons.tsx
+++ src/components/shiny-ui/GridEditor/TractAddButtons.tsx
@@ -1,10 +1,12 @@
-import { IconButton } from "@chakra-ui/button";
+import styled from "@emotion/styled";
 import React from "react";
 import { FaPlus as PlusIcon } from "react-icons/fa";
+import { TractDirection } from "state-logic/gridLayout/atoms";
 import { seqArray } from "utils/array-helpers";
 import { addTract, NewTract } from "utils/gridTemplates/addTract";
 import { ParsedGridTemplate } from "utils/gridTemplates/parseGridTemplateAreas";
 
+const directions: TractDirection[] = ["rows", "cols"];
 export function TractAddButtons({
   numCols,
   numRows,
@@ -12,36 +14,30 @@ export function TractAddButtons({
 }: Pick<ParsedGridTemplate, "numCols" | "numRows"> & {
   onAdd: (tract: Parameters<typeof addTract>[1]) => void;
 }) {
-  const rowAddButtons = seqArray(numRows).map((i) => {
-    const rowIndex = i + 1;
-    return (
-      <TractAddButton
-        key={rowIndex}
-        dir="rows"
-        afterIndex={rowIndex}
-        size="30px"
-        onClick={onAdd}
-      />
-    );
-  });
+  const tractButtons: Record<TractDirection, JSX.Element[]> = {
+    rows: [],
+    cols: [],
+  };
 
-  const colAddButtons = seqArray(numCols).map((i) => {
-    const colIndex = i + 1;
-    return (
+  for (let dir of directions) {
+    // Need one more button to add a tract before others
+    const numButtons = (dir === "rows" ? numRows : numCols) + 1;
+
+    tractButtons[dir] = seqArray(numButtons).map((i) => (
       <TractAddButton
-        key={colIndex}
-        dir="cols"
-        afterIndex={colIndex}
+        key={i}
+        dir={dir}
+        afterIndex={i}
         size="30px"
         onClick={onAdd}
       />
-    );
-  });
+    ));
+  }
 
   return (
     <>
-      {rowAddButtons}
-      {colAddButtons}
+      {tractButtons.rows}
+      {tractButtons.cols}
     </>
   );
 }
@@ -49,36 +45,52 @@ function TractAddButton({
   onClick,
   ...tract
 }: Parameters<typeof addTract>[1] & { onClick: (t: NewTract) => void }) {
-  const buttonSize = "30px";
-  const pad = "3px";
-  const offset = `calc(-${buttonSize} - ${pad})`;
+  const pad = "2px";
+  const offsetOutOfGrid = `calc(-2*var(--gap) - ${pad})`;
+  const offsetToTractCenter = `calc(-1*var(--gap, 100px))`;
   const { dir, afterIndex, size = "50px" } = tract;
+
+  const firstPosition = afterIndex === 0;
+  const placementIndex = firstPosition ? 1 : afterIndex;
 
   const placement: React.CSSProperties =
     dir === "rows"
       ? {
-          gridRow: afterIndex,
+          gridRow: placementIndex,
           gridColumn: 1,
-          alignSelf: "end",
-          marginLeft: offset,
-          marginBottom: offset,
+          alignSelf: firstPosition ? "start" : "end",
+          marginLeft: offsetOutOfGrid,
+          [firstPosition ? "marginTop" : "marginBottom"]: offsetToTractCenter,
         }
       : {
-          gridColumn: afterIndex,
+          gridColumn: placementIndex,
           gridRow: 1,
-          justifySelf: "end",
-          marginRight: offset,
-          marginTop: offset,
+          justifySelf: firstPosition ? "start" : "end",
+          [firstPosition ? "marginLeft" : "marginRight"]: offsetToTractCenter,
+          marginTop: offsetOutOfGrid,
         };
 
   return (
-    <IconButton
-      variant="ghost"
-      aria-label="Add tract"
-      css={{ width: buttonSize, height: buttonSize }}
+    <SquareButton
+      size="var(--gap, 100px)"
+      aria-label={`Add ${singular(dir)} after ${singular(dir)} ${afterIndex}`}
       style={placement}
       onClick={() => onClick({ dir, afterIndex, size })}
-      icon={<PlusIcon />}
-    />
+    >
+      <PlusIcon />
+    </SquareButton>
   );
+}
+
+const SquareButton = styled.button(({ size }: { size: string }) => ({
+  width: size,
+  height: size,
+  maxHeight: size,
+  maxWidth: size,
+  display: "grid",
+  placeContent: "center",
+}));
+
+function singular(dir: TractDirection): "row" | "column" {
+  return dir === "rows" ? "row" : "column";
 }
