36e329029d0eadd105a73e0a69328610da478b9b
Nick Strayer <nicholasstrayer@Nicks-MacBook-Pro-2.local>
2021-12-06 13:20:49 -0500

    Added toggle for going between UI edit mode and layout edit mode

--- src/components/shiny-ui/GridEditor/index.tsx
+++ src/components/shiny-ui/GridEditor/index.tsx
@@ -1,4 +1,5 @@
 /** @jsxImportSource @emotion/react */
+import { Button } from "@chakra-ui/button";
 import styled from "@emotion/styled";
 import { GridLocString } from "GridTypes";
 import React from "react";
@@ -25,6 +26,7 @@ export const SetLayoutContext = React.createContext<React.Dispatch<
   React.SetStateAction<TemplatedGridProps>
 > | null>(null);
 
+type EditMode = "UI" | "Layout";
 export default function GridEditor({
   items,
   onNewArea,
@@ -36,6 +38,8 @@ export default function GridEditor({
     gapSize: "1rem",
     ...initialLayoutDef,
   });
+
+  const [editMode, setEditMode] = React.useState<EditMode>("UI");
 
   const itemGridLocations = React.useMemo(
     () => areasToItemLocations(layout.areas),
@@ -93,14 +97,20 @@ export default function GridEditor({
       <div
         css={{
           "--gap": layout.gapSize,
+          "--settings-bar": "50px",
           "--row-gutter": "150px",
           "--col-gutter": "100px",
           display: "grid",
           gridTemplateColumns: "var(--row-gutter) 1fr",
-          gridTemplateRows: "var(--col-gutter) 1fr",
-          gridTemplateAreas: `". column-controls"\n"row-controls main"`,
+          gridTemplateRows: "var(--settings-bar) var(--col-gutter) 1fr",
+          gridTemplateAreas: `"           settings settings"\n
+                              "           . column-controls"\n
+                              "row-controls main"`,
         }}
       >
+        <SettingsBar>
+          <EditModeToggle selected={editMode} onSelect={setEditMode} />
+        </SettingsBar>
         <GridDisplay style={styles}>
           <TractControls areas={layout.areas} sizes={sizes} />
           <GridCells
@@ -109,7 +119,7 @@ export default function GridEditor({
             cellLocRef={gridCellLocations}
             onClick={addNewItem}
           />
-          {areaOverlays}
+          {editMode === "Layout" ? areaOverlays : null}
           {Object.values(items)}
         </GridDisplay>
       </div>
@@ -117,9 +127,72 @@ export default function GridEditor({
   );
 }
 
+function EditModeToggle({ selected, onSelect }: { selected: EditMode; onSelect: (selection: EditMode) => void; }) {
+  return <ToggleButtons className={selected === "UI" ? "left-selected" : "right-selected"}>
+    <button name="UI" className="left" onClick={() => onSelect("UI")}>UI</button>
+    <button name="Layout" className="right" onClick={() => onSelect("Layout")}>Layout</button>
+  </ToggleButtons>
+}
+
 const GridDisplay = styled.div({
   gridArea: "main",
-  gridRow: 2,
+  gridRow: 3,
   gridColumn: 2,
   display: "grid",
 });
+
+const SettingsBar = styled.div({
+  gridArea: "settings",
+  display: "grid",
+});
+
+const ToggleButtons = styled.div({
+  "--roundness": "8px",
+  "--w": "70px",
+  "--h": "40px",
+  "--bar-transform": "var(--roundness)",
+  "--color-bar": "var(--rstudio-blue, forestgreen)",
+  "--color-selected": "var(--rstudio-blue, forestgreen)",
+  "--color-selected-text": "var(--rstudio-white, blue)",
+  "--color-border": "var(--light-grey, red)",
+  "position": "relative",
+  width: "calc(2*var(--w))",
+  height: "var(--h)",
+  "& > button": {
+    padding: "5px 10px",
+    width: "var(--w)",
+    transition: "borderColor borderWidth 0.2s ease-in",
+    backgroundColor: "var(--light-grey)",
+    color: "#303030",
+  },
+  "& > button.left": {
+    borderTopLeftRadius: "var(--roundness)",
+    borderBottomLeftRadius: "var(--roundness)",
+  },
+  "&.left-selected > button.left": {
+    backgroundColor: "var(--color-selected)",
+    color: "var(--color-selected-text)",
+  },
+  "&.right-selected > button.right": {
+    backgroundColor: "var(--color-selected)",
+    color: "var(--color-selected-text)",
+  },
+  "& > button.right": {
+    borderTopRightRadius: "var(--roundness)",
+    borderBottomRightRadius: "var(--roundness)",
+  },
+  "&.right-selected": {
+    "--bar-transform": "var(--w)",
+  },
+  "&::after": {
+    content: `""`,
+    position: "absolute",
+    bottom: "2px",
+    height: "3px",
+    left: 0,
+    transform: "translateX(var(--bar-transform))",
+    width: "calc(var(--w) - var(--roundness))",
+    backgroundColor: "var(--color-bar)",
+    transition: "transform 0.1s ease-in",
+  },
+})
