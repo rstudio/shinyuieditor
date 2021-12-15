dae893ceb5680b044037847da7f5ecfde4fa0773
Nick Strayer <nick.strayer@gmail.com>
2021-12-15 16:43:04 -0500

    Simple function to print a UI element from basic information

---
+++ R/R/printElement.R
@@ -0,0 +1,20 @@
+
+printElement <- function(element_def){
+
+  args <- element_def$componentProps
+
+  args_wrapped <- lapply(args, function(arg){
+    if(typeof(arg) == "character") return(paste0('"',arg ,'"'))
+    arg
+  })
+
+  args_written <- paste0(names(args), "=", args_wrapped, collapse = ", ")
+
+  paste0(
+    element_def$componentName, "(", args_written, ")"
+  )
+}
+
+printAllElements <- function(element_defs){
+  lapply(element_defs, printElement)
+}
