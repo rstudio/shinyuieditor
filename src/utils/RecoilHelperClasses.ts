76de7add9bef0920e401e85cd81ce8430cdeecff
Nick Strayer <nick.strayer@gmail.com>
2022-01-07 11:10:50 -0500

    Remove recoil and xstate deps

--- src/utils/RecoilHelperClasses.ts
+++
@@ -1,7 +0,0 @@
-// Some helper generics to make it easier to type out functions for pulling
-// updating logic out of callbacks etc..
-
-import { RecoilValue, RecoilState } from "recoil";
-
-export type RecoilGetter<T> = (a: RecoilValue<T>) => T;
-export type RecoilSetter<T> = (s: RecoilState<T>, u: T) => void;
