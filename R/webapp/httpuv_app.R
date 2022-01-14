1965b2a8e8d95238aadafc257386f6b11a3b7fe0
Nick Strayer <nick.strayer@gmail.com>
2022-01-14 12:47:42 -0500

    No longer need the library imports for the scratch code

--- R/webapp/httpuv_app.R
+++ R/webapp/httpuv_app.R
@@ -1,10 +1,5 @@
 # Basic server to communicate between react app and R
 
-library(httpuv)
-library(here)
-library(magrittr)
-
-
 s <- launch_editor(
-  here("webapp/ui.R")
+  here::here("webapp/ui.R")
 )
