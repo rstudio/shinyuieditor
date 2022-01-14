5c84a61c24836bc35a59cb9624f2ce172ab962d6
Nick Strayer <nick.strayer@gmail.com>
2022-01-14 11:13:53 -0500

    Cleaned up the initialState logic to its own module

--- src/EditorContainer.tsx
+++ src/EditorContainer.tsx
@@ -1,26 +1,10 @@
 import styled from "@emotion/styled";
-import GridApp, { Panels } from "components/Shiny-Ui-Elements/Layouts/GridApp";
+import GridApp from "components/Shiny-Ui-Elements/Layouts/GridApp";
 import * as React from "react";
 import { useQuery } from "react-query";
 import { makeBoxShadow } from "utils/css-helpers";
-import { TemplatedGridProps } from "utils/gridTemplates/types";
+import { getInitialState } from "./getInitialState";
 import { Header } from "./Header";
-
-type InitialState = {
-  elements: Panels;
-  layout: { type: "gridlayout"; options: TemplatedGridProps };
-};
-
-async function getInitialState(): Promise<InitialState> {
-  const response = await fetch("app-please", { method: "GET" });
-
-  if (!response.ok) {
-    console.error(`HTTP error! status: ${response.status}`);
-    return backupState;
-  }
-
-  return await response.json();
-}
 
 export function EditorContainer() {
   const { isLoading, error, data } = useQuery("initial-state", getInitialState);
@@ -32,64 +16,16 @@ export function EditorContainer() {
   if (error || !data) {
     return <h3 style={{ color: "orangered" }}>Error with server request</h3>;
   }
-  const initialLayout: TemplatedGridProps = data.layout.options;
-  const initialPanels: Panels = data.elements;
-  console.log(data);
 
   return (
     <Container>
       <Header />
       <EditorHolderContainer>
-        <GridApp layout={initialLayout} panels={initialPanels} />
+        <GridApp layout={data.layout.options} panels={data.elements} />
       </EditorHolderContainer>
     </Container>
   );
 }
-
-const backupState: InitialState = {
-  layout: {
-    type: "gridlayout",
-    options: {
-      rowSizes: ["120px", "1fr", "100px"],
-      colSizes: ["250px", "1fr"],
-      gapSize: "2rem",
-      areas: [
-        ["header", "header"],
-        ["sidebar", "plot"],
-        ["footer", "footer"],
-      ],
-    },
-  },
-  elements: {
-    header: {
-      uiName: "gridlayout::title_panel",
-      uiArguments: {
-        title: "Header from backup state",
-      },
-    },
-    plot: {
-      uiName: "shiny::plotOutput",
-      uiArguments: {
-        outputId: "distPlot",
-      },
-    },
-    sidebar: {
-      uiName: "shiny::sliderInput",
-      uiArguments: {
-        inputId: "numBins",
-        min: "5",
-        max: "10",
-        value: "7",
-      },
-    },
-    footer: {
-      uiName: "gridlayout::title_panel",
-      uiArguments: {
-        title: "My app's footer",
-      },
-    },
-  },
-};
 
 const EditorHolderContainer = styled.div({
   padding: "2rem",
---
+++ src/getInitialState.tsx
@@ -0,0 +1,61 @@
+import { Panels } from "components/Shiny-Ui-Elements/Layouts/GridApp";
+import { TemplatedGridProps } from "utils/gridTemplates/types";
+
+export type InitialState = {
+  elements: Panels;
+  layout: { type: "gridlayout"; options: TemplatedGridProps };
+};
+export async function getInitialState(): Promise<InitialState> {
+  const response = await fetch("app-please", { method: "GET" });
+
+  if (!response.ok) {
+    console.error(`HTTP error! status: ${response.status}`);
+    return backupState;
+  }
+
+  return await response.json();
+}
+const backupState: InitialState = {
+  layout: {
+    type: "gridlayout",
+    options: {
+      rowSizes: ["120px", "1fr", "100px"],
+      colSizes: ["250px", "1fr"],
+      gapSize: "2rem",
+      areas: [
+        ["header", "header"],
+        ["sidebar", "plot"],
+        ["footer", "footer"],
+      ],
+    },
+  },
+  elements: {
+    header: {
+      uiName: "gridlayout::title_panel",
+      uiArguments: {
+        title: "Header from backup state",
+      },
+    },
+    plot: {
+      uiName: "shiny::plotOutput",
+      uiArguments: {
+        outputId: "distPlot",
+      },
+    },
+    sidebar: {
+      uiName: "shiny::sliderInput",
+      uiArguments: {
+        inputId: "numBins",
+        min: "5",
+        max: "10",
+        value: "7",
+      },
+    },
+    footer: {
+      uiName: "gridlayout::title_panel",
+      uiArguments: {
+        title: "My app's footer",
+      },
+    },
+  },
+};
