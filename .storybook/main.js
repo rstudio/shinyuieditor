dcf43722b3522b23ab29cf45d6ed309eb8900a55
Nick Strayer <nick.strayer@gmail.com>
2022-01-18 10:52:04 -0500

    Did some voodoo via `npx sb upgrade` to get storybook working again

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,3 @@
-const path = require("path");
-const toPath = (_path) => path.join(process.cwd(), _path);
-
-// This nonsense is a fix for mismatched version of emotion messing up styles
-// it is copies from this gh issue: https://github.com/chakra-ui/chakra-ui/issues/2527
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -10,17 +5,10 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
   ],
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": toPath("node_modules/@emotion/react"),
-          "emotion-theming": toPath("node_modules/@emotion/react"),
-        },
-      },
-    };
+  features: {
+    emotionAlias: false,
+  },
+  core: {
+    builder: "webpack5",
   },
 };
