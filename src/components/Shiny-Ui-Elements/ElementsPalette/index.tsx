a849e413072348cddb6102300f7237548187c826
Nick Strayer <nick.strayer@gmail.com>
2022-01-25 11:34:25 -0500

    Added basic component with list of the available elements as draggable icons

---
+++ src/components/Shiny-Ui-Elements/ElementsPalette/index.tsx
@@ -0,0 +1,42 @@
+import {
+  ShinyUiNameAndArguments,
+  ShinyUiNames,
+} from "components/Shiny-Ui-Elements/Elements/componentTypes";
+import * as React from "react";
+import { BiSliderAlt, BiText } from "react-icons/bi";
+import { GoGraph } from "react-icons/go";
+import { defaultSettingsForElements } from "../Elements/uiComponentAndSettings";
+import classes from "./styles.module.css";
+
+export default function ElementsPalette({
+  availableUi = defaultSettingsForElements,
+}: {
+  availableUi?: ShinyUiNameAndArguments[];
+}) {
+  return (
+    <div className={classes.OptionsList}>
+      {availableUi.map((ui) => {
+        const { uiName: name } = ui;
+        return (
+          <div
+            className={classes.OptionItem}
+            key={name}
+            draggable
+            onDragStart={(e) => {
+              e.dataTransfer.setData("element-type", name);
+            }}
+          >
+            <code>{name.replace(/[\w]+::/, "")}</code>
+            {previewIcons[name]}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const previewIcons: Record<ShinyUiNames, JSX.Element> = {
+  "shiny::plotOutput": <GoGraph size="40px" />,
+  "shiny::sliderInput": <BiSliderAlt size="40px" />,
+  "gridlayout::title_panel": <BiText size="40px" />,
+};
