10a9c86cc280b908bfc0fda5b5fa9f6112adb6be
Nick Strayer <nick.strayer@gmail.com>
2022-01-28 11:34:10 -0500

    Moved RadioInputs to the Inputs component folder and simplified grid_panel settings component even further

---
+++ src/components/Inputs/RadioInputs.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import classes from "./RadioInputs.module.css";
+
+export function RadioInputs<OptionType extends string>({
+  name,
+  options,
+  currentSelection,
+  onChange,
+}: {
+  name: string;
+  options: OptionType[];
+  currentSelection: OptionType;
+  onChange: (selection: OptionType) => void;
+}) {
+  return (
+    <div className={classes.formSection}>
+      <p>{name}:</p>
+      <fieldset
+        className={classes.radioInputs}
+        onChange={(e) => {
+          onChange((e.target as HTMLInputElement).value as OptionType);
+        }}
+      >
+        {options.map((option) => (
+          <div key={option}>
+            <input
+              name={name}
+              type="radio"
+              value={option}
+              defaultChecked={option === currentSelection}
+            />
+            <label htmlFor={option}>{option}</label>
+          </div>
+        ))}
+      </fieldset>
+    </div>
+  );
+}
--- src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPanel/SettingsPanel.tsx
+++ src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPanel/SettingsPanel.tsx
@@ -1,25 +1,23 @@
+import { RadioInputs } from "components/Inputs/RadioInputs";
 import { SettingsUpdaterComponent } from "components/Shiny-Ui-Elements/uiNodeTypes";
 import * as React from "react";
 import { GridPanelSettings } from ".";
-import classes from "./SettingsPanel.module.css";
 
 export const GridlayoutGridPanelSettings: SettingsUpdaterComponent<
   GridPanelSettings
-> = ({ settings: currentSettings, onChange }) => {
-  const settings = { ...currentSettings };
-
+> = ({ settings, onChange }) => {
   return (
     <>
       <RadioInputs
         name="Vertical Alignment"
         options={["top", "center", "bottom", "spread"]}
-        currentSelection={currentSettings.verticalAlign ?? "spread"}
+        currentSelection={settings.verticalAlign ?? "spread"}
         onChange={(verticalAlign) => onChange({ ...settings, verticalAlign })}
       />
       <RadioInputs
         name="Horizontal Alignment"
         options={["left", "center", "right", "spread"]}
-        currentSelection={currentSettings.horizontalAlign ?? "spread"}
+        currentSelection={settings.horizontalAlign ?? "spread"}
         onChange={(horizontalAlign) =>
           onChange({ ...settings, horizontalAlign })
         }
@@ -27,39 +25,3 @@ export const GridlayoutGridPanelSettings: SettingsUpdaterComponent<
     </>
   );
 };
-
-function RadioInputs<OptionType extends string>({
-  name,
-  options,
-  currentSelection,
-  onChange,
-}: {
-  name: string;
-  options: OptionType[];
-  currentSelection: OptionType;
-  onChange: (selection: OptionType) => void;
-}) {
-  return (
-    <div className={classes.formSection}>
-      <p>{name}:</p>
-      <fieldset
-        className={classes.radioInputs}
-        onChange={(e) => {
-          onChange((e.target as HTMLInputElement).value as OptionType);
-        }}
-      >
-        {options.map((option) => (
-          <div key={option}>
-            <input
-              name={name}
-              type="radio"
-              value={option}
-              defaultChecked={option === currentSelection}
-            />
-            <label htmlFor={option}>{option}</label>
-          </div>
-        ))}
-      </fieldset>
-    </div>
-  );
-}
