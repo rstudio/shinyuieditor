6bf654bcb06080bc0f2c9dddd5923510d4467935
Nick Strayer <nick.strayer@gmail.com>
2022-01-31 11:16:47 -0500

    Add simple grid-gap sizing grid_page

--- src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPage/SettingsPanel.tsx
+++ src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPage/SettingsPanel.tsx
@@ -1,3 +1,4 @@
+import { CSSUnitInput } from "components/CSSUnitInput";
 import { SettingsUpdaterComponent } from "components/Shiny-Ui-Elements/uiNodeTypes";
 import * as React from "react";
 import { TemplatedGridProps } from "utils/gridTemplates/types";
@@ -5,10 +6,17 @@ import classes from "./SettingsPanel.module.css";
 
 export const GridlayoutGridPageSettings: SettingsUpdaterComponent<
   TemplatedGridProps
-> = ({ settings: currentSettings, onChange }) => {
+> = ({ settings, onChange }) => {
   return (
     <>
       <div className={classes.formSection}>Settings for grid page:</div>
+
+      <p>Gap Size:</p>
+      <CSSUnitInput
+        value={settings.gapSize}
+        units={["px", "rem"]}
+        onChange={(gapSize) => onChange({ ...settings, gapSize })}
+      />
     </>
   );
 };
