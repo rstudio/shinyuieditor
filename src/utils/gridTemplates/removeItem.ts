ab86b9c43466d2f5e66865c2aa04e635448e8c89
Nick Strayer <nick.strayer@gmail.com>
2022-02-02 16:39:36 -0500

    Converted removeItem to immer

--- src/utils/gridTemplates/removeItem.ts
+++ src/utils/gridTemplates/removeItem.ts
@@ -1,24 +1,23 @@
-import clone from "just-clone";
+import produce from "immer";
 import { matrixDimensions } from "utils/matrix-helpers";
 import { emptyCell } from "./itemLocations";
 import { TemplatedGridProps } from "./types";
 
 export default function removeItem(
-  template: TemplatedGridProps,
+  originalTemplate: TemplatedGridProps,
   itemName: string
 ): TemplatedGridProps {
-  const areasCopy = clone(template.areas);
-
-  const { numRows, numCols } = matrixDimensions(template.areas);
-  // i and j are in 0-indexed coordinates where as {row,col}{Start,End} are in
-  // grid coordinates, hence the funky bounds math.
-  for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numCols; j++) {
-      if (areasCopy[i][j] === itemName) {
-        areasCopy[i][j] = emptyCell;
+  const { numRows, numCols } = matrixDimensions(originalTemplate.areas);
+  return produce(originalTemplate, (template) => {
+    const areas = template.areas;
+    // i and j are in 0-indexed coordinates where as {row,col}{Start,End} are in
+    // grid coordinates, hence the funky bounds math.
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        if (areas[i][j] === itemName) {
+          areas[i][j] = emptyCell;
+        }
       }
     }
-  }
-
-  return { ...template, areas: areasCopy };
+  });
 }
