4ce941bb9974465de7285393bf7b5cd700fea8a3
Nick Strayer <nick.strayer@gmail.com>
2022-02-03 11:30:49 -0500

    Added a basic Button component with a regular and delete state

---
+++ src/components/Inputs/Button.tsx
@@ -0,0 +1,23 @@
+import classes from "./Button.module.css";
+
+type PropsForEl<T extends HTMLElement> = React.DetailedHTMLProps<
+  React.HTMLAttributes<T>,
+  T
+>;
+
+const Button: React.FC<
+  {
+    variant?: "delete" | "regular";
+  } & PropsForEl<HTMLButtonElement>
+> = ({ children, variant = "regular", ...passthroughProps }) => {
+  return (
+    <button
+      className={classes.button + " " + classes[variant]}
+      {...passthroughProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
