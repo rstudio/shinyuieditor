37e57d73945b664bcc437d8e41786ea8629f326d
Nick Strayer <nick.strayer@gmail.com>
2022-02-03 13:04:14 -0500

    Hooked up confirm logic to new item modal

--- src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPage/index.tsx
+++ src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPage/index.tsx
@@ -5,6 +5,7 @@ import { TractControls } from "components/Shiny-Ui-Elements/Layouts/GridApp/Trac
 import NodeUpdateContext from "components/Shiny-Ui-Elements/UiNode/NodeUpdateContext";
 import { ShinyUiNames } from "components/Shiny-Ui-Elements/uiNodeTypes";
 import { GridLocString } from "GridTypes";
+import PortalModal from "PortalModal";
 import React from "react";
 import { subtractElements } from "utils/array-helpers";
 import { enumerateGridDims, toStringLoc } from "utils/grid-helpers";
@@ -18,7 +19,6 @@ import {
 } from "../uiComponentAndSettings";
 import { GridLayoutAction, gridLayoutReducer } from "./gridLayoutReducer";
 import classes from "./styles.module.css";
-import PortalModal from "PortalModal";
 
 export type GridCellBounds = Record<GridLocString, ItemBoundingBox>;
 export type CellLocRef = React.MutableRefObject<GridCellBounds>;
@@ -102,6 +102,45 @@ const GridlayoutGridPage: UiNodeComponent<TemplatedGridProps> = ({
     "--col-gutter": "100px",
   } as React.CSSProperties;
 
+  const addNewGridItem = React.useCallback(
+    (name: string, info: NewItemInfo) => {
+      // For right now we'll just use the default settings for the
+      // dropped ui element
+      const newElement = defaultSettingsForElements.find(
+        ({ uiName }) => uiName === info.uiName
+      );
+
+      if (!newElement) {
+        throw new Error(
+          "Could not find default settings for node of type " + info.uiName
+        );
+      }
+      handleLayoutUpdate({
+        type: "ADD_ITEM",
+        name: name,
+        pos: info.pos,
+      });
+      // Let the state know we have a new child node
+      nodeUpdaters({
+        type: "ADD_NODE",
+        parentPath: [],
+        newNode: {
+          uiName: "gridlayout::grid_panel",
+          uiArguments: {
+            area: name,
+            horizontalAlign: "spread",
+            verticalAlign: "spread",
+          },
+          uiChildren: [newElement],
+        },
+      });
+
+      // Reset the modal/new item info state
+      setShowModal(null);
+    },
+    [handleLayoutUpdate, nodeUpdaters]
+  );
+
   return (
     <LayoutDispatchContext.Provider value={handleLayoutUpdate}>
       <div
@@ -130,25 +169,9 @@ const GridlayoutGridPage: UiNodeComponent<TemplatedGridProps> = ({
                 "element-type"
               ) as ShinyUiNames;
 
-              console.log("Cell had " + nameOfDroppedUi + " dropped on it");
-
-              // For right now we'll just use the default settings for the
-              // dropped ui element
-              const newElement = defaultSettingsForElements.find(
-                ({ uiName }) => uiName === nameOfDroppedUi
-              );
-
-              if (!newElement) {
-                throw new Error(
-                  "Could not find default settings for node of type " +
-                    nameOfDroppedUi
-                );
-              }
-
               // This will eventually filter by element type
               const allowedDrop = true;
               if (!allowedDrop) return;
-              const newAreaName = "MyNewGridArea";
               setShowModal({
                 uiName: nameOfDroppedUi,
                 pos: {
@@ -158,32 +181,6 @@ const GridlayoutGridPage: UiNodeComponent<TemplatedGridProps> = ({
                   colEnd: col,
                 },
               });
-
-              // handleLayoutUpdate({
-              //   type: "ADD_ITEM",
-              //   name: newAreaName,
-              //   pos: {
-              //     rowStart: row,
-              //     rowEnd: row,
-              //     colStart: col,
-              //     colEnd: col,
-              //   },
-              // });
-
-              // // Let the state know we have a new child node
-              // nodeUpdaters({
-              //   type: "ADD_NODE",
-              //   parentPath: [],
-              //   newNode: {
-              //     uiName: "gridlayout::grid_panel",
-              //     uiArguments: {
-              //       area: newAreaName,
-              //       horizontalAlign: "spread",
-              //       verticalAlign: "spread",
-              //     },
-              //     uiChildren: [newElement],
-              //   },
-              // });
             }}
           />
         ))}
@@ -196,6 +193,7 @@ const GridlayoutGridPage: UiNodeComponent<TemplatedGridProps> = ({
         <NameNewPanelModal
           info={showModal}
           onCancel={() => setShowModal(null)}
+          onDone={(name) => addNewGridItem(name, showModal)}
         />
       ) : null}
     </LayoutDispatchContext.Provider>
@@ -205,14 +203,19 @@ const GridlayoutGridPage: UiNodeComponent<TemplatedGridProps> = ({
 function NameNewPanelModal({
   info: { uiName, pos },
   onCancel,
+  onDone,
 }: {
   info: NewItemInfo;
   onCancel: () => void;
+  onDone: (name: string) => void;
 }) {
+  const [newItemName, setNewItemName] = React.useState<string>(
+    "NewGridItemFromPortal"
+  );
   return (
     <PortalModal
       title="Name new grid panel"
-      onConfirm={() => console.log("Good to go with item")}
+      onConfirm={() => onDone(newItemName)}
       onCancel={onCancel}
     >
       <p>UiElement: {uiName}</p>
