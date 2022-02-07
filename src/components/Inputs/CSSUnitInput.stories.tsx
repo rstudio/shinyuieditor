d80a0c58fc1952f884cebf8bc28b47193ea72715
Nick Strayer <nick.strayer@gmail.com>
2022-02-07 16:43:53 -0500

    Added super simple storybook story for css input

---
+++ src/components/Inputs/CSSUnitInput.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import { CSSUnitInput } from "./CSSUnitInput";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Inputs/CSSUnitInput",
+  component: CSSUnitInput,
+} as ComponentMeta<typeof CSSUnitInput>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof CSSUnitInput> = (args) => (
+  <CSSUnitInput {...args} />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  value: "1rem",
+};
