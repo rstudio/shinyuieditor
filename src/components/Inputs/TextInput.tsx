ca44cd74e001cec4df30da5012d076ee65ebd920
Nick Strayer <nick.strayer@gmail.com>
2022-02-07 11:22:41 -0500

    Added autoFocus to the TextInput component

--- src/components/Inputs/TextInput.tsx
+++ src/components/Inputs/TextInput.tsx
@@ -1,6 +1,7 @@
 import { FormControl, FormLabel, Input } from "@chakra-ui/react";
 import classes from "./TextInput.module.css";
 import inputClasses from "./Inputs.module.css";
+import React from "react";
 
 export function TextInputChakra({
   label,
@@ -24,18 +25,31 @@ export function TextInput({
   label,
   value,
   onChange,
+  autoFocus = false,
 }: {
   name: string;
   label?: string;
   value: string;
   onChange: (x: string) => void;
+  autoFocus?: boolean;
 }) {
+  const inputElement = React.useRef<HTMLInputElement>(null);
+  React.useEffect(() => {
+    if (inputElement.current && autoFocus) {
+      // We need a timeout so we're sure the element is rendered before setting
+      // focus. This is only neccesary for scenarios like rendering in a portal
+      // but since we do that... we need it
+      setTimeout(() => inputElement?.current?.focus(), 1);
+    }
+  }, [autoFocus]);
+
   return (
     <div className={inputClasses.container}>
       <label className={inputClasses.label} htmlFor={name}>
         {label ?? name}:
       </label>
       <input
+        ref={inputElement}
         className={classes.input}
         type="text"
         name={name}
