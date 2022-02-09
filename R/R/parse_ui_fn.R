32c460ba138bc9b09dc5d2699b4a7919fb7ab48f
Nick Strayer <nick.strayer@gmail.com>
2022-02-09 17:57:17 -0500

    Re-enable the argument-name filling now that gridlayout has been updated

--- R/R/parse_ui_fn.R
+++ R/R/parse_ui_fn.R
@@ -34,7 +34,7 @@ parse_ui_fn <- function(ui_node_expr) {
   if(is_constant) return(ui_node_expr)
 
   # Fill in all the names of unnamed arguments
-  # ui_expr <- rlang::call_standardise(ui_expr)
+  ui_node_expr <- rlang::call_standardise(ui_node_expr)
 
   # Since first element of the AST is the function call itself, it makes our
   # life easier going forward if we remove it before walking through arguments
