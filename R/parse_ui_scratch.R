ca740fb1486d897c0229896e2e277f03cf115c85
Nick Strayer <nick.strayer@gmail.com>
2022-02-09 17:57:40 -0500

    Update the ui scratch template to use unnamed arguments

--- R/parse_ui_scratch.R
+++ R/parse_ui_scratch.R
@@ -2,27 +2,26 @@
 library(gridlayout)
 
 ui_expr <- rlang::expr(
-  grid_page(
+  gridlayout::grid_page(
     layout = "
 |2rem  |200px   |1fr    |
 |80px  |header  |header |
 |1fr   |sidebar |plot   |",
     gridlayout::title_panel(
-      area = "header",
+      "header",
       title = "My App Title"
     ),
     gridlayout::grid_panel(
-      area = "sidebar",
+      "sidebar",
       shiny::sliderInput(inputId="slider1", label="my slider is cool", min="1", max="10", value="7"),
       shiny::sliderInput(inputId="slider3", min="2", max="10", value="5")
     ),
     gridlayout::grid_panel(
-      area = "plot",
-      plotOutput(inputId = "distPlot")
+      "plot",
+      shiny::plotOutput("distPlot")
     )
   )
 )
-
 
 
 gridlayout_node <- parse_ui_fn(ui_expr)
