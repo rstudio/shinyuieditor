13fda780425726cbc9c43a736b7f311c50aa5823
Nick Strayer <nick.strayer@gmail.com>
2022-02-11 17:14:06 -0500

    Added alignment icons

--- src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPanel/SettingsPanel.tsx
+++ src/components/Shiny-Ui-Elements/Elements/GridlayoutGridPanel/SettingsPanel.tsx
@@ -2,7 +2,30 @@ import { RadioInputs } from "components/Inputs/RadioInputs";
 import { TextInput } from "components/Inputs/TextInput";
 import { SettingsUpdaterComponent } from "components/Shiny-Ui-Elements/uiNodeTypes";
 import * as React from "react";
+import { AiOutlineVerticalAlignMiddle } from "react-icons/ai";
+import {
+  CgAlignBottom,
+  CgAlignCenter,
+  CgAlignLeft,
+  CgAlignMiddle,
+  CgAlignRight,
+  CgAlignTop,
+} from "react-icons/cg";
 import { GridPanelSettings } from ".";
+
+const verticalAlignIcons = {
+  top: <CgAlignTop size="25px" />,
+  bottom: <CgAlignBottom size="25px" />,
+  center: <AiOutlineVerticalAlignMiddle size="25px" />,
+  spread: <CgAlignMiddle size="25px" />,
+};
+
+const horizontalAlignIcons = {
+  left: <CgAlignLeft size="25px" />,
+  spread: <CgAlignCenter size="25px" />,
+  center: <CgAlignCenter size="25px" />,
+  right: <CgAlignRight size="25px" />,
+};
 
 export const GridlayoutGridPanelSettings: SettingsUpdaterComponent<
   GridPanelSettings
@@ -17,17 +40,21 @@ export const GridlayoutGridPanelSettings: SettingsUpdaterComponent<
       />
       <RadioInputs
         name="Vertical Alignment"
-        options={["top", "center", "bottom", "spread"]}
+        options={["top", "spread", "bottom"]}
+        optionIcons={verticalAlignIcons}
         currentSelection={settings.verticalAlign ?? "spread"}
         onChange={(verticalAlign) => onChange({ ...settings, verticalAlign })}
+        optionsPerColumn={3}
       />
       <RadioInputs
         name="Horizontal Alignment"
-        options={["left", "center", "right", "spread"]}
+        options={["left", "spread", "right"]}
+        optionIcons={horizontalAlignIcons}
         currentSelection={settings.horizontalAlign ?? "spread"}
         onChange={(horizontalAlign) =>
           onChange({ ...settings, horizontalAlign })
         }
+        optionsPerColumn={3}
       />
     </>
   );
