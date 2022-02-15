328e20f02b15caea6ae1f3a82e150335dc3c3d3c
Nick Strayer <nick.strayer@gmail.com>
2022-02-15 17:58:22 -0500

    Update Ui.R after formatting of the grid layout table is improved

--- R/webapp/ui.R
+++ R/webapp/ui.R
@@ -1,8 +1,12 @@
 gridlayout::grid_page(
-  layout = "| 2rem | 250px   | 1fr    |\n|------|---------|--------|\n| 80px | header  | header |\n| 1fr  | sidebar |  plot  |",
+  layout = "
+| 2rem | 350px   | 1fr    |
+|------|---------|--------|
+| 80px | header  | header |
+| 1fr  | sidebar |  plot  |",
   gridlayout::title_panel(
     area = "header",
-    title = "My App Title from R2"
+    title = "My App Title from R3"
   ),
   gridlayout::grid_panel(
     area = "sidebar",
