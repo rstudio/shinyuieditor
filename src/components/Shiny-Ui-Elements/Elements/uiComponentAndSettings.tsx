2ac0a99877115b6fee01835cd25cf525d9093de5
Nick Strayer <nick.strayer@gmail.com>
2022-03-01 11:16:11 -0500

    Update the elements palette to use the icon presence and default values within the main component info type

--- src/components/Shiny-Ui-Elements/Elements/uiComponentAndSettings.tsx
+++ src/components/Shiny-Ui-Elements/Elements/uiComponentAndSettings.tsx
@@ -3,17 +3,14 @@ import {
   SettingsUpdaterComponent,
   ShinyUiArguments,
   ShinyUiNames,
-  ShinyUiNode,
 } from "components/Shiny-Ui-Elements/uiNodeTypes";
 import { DragAndDropHandlers } from "../DragAndDropHelpers/useDragAndDropElements";
 import { gridlayoutGridPageInfo } from "./GridlayoutGridPage";
 import { gridLayoutGridPanelInfo } from "./GridlayoutGridPanel";
-import {
-  gridlayoutVerticalStackPanelDefaultSettings,
-  gridlayoutVerticalStackPanelInfo,
-} from "./GridlayoutVerticalStackPanel";
+import { gridlayoutVerticalStackPanelInfo } from "./GridlayoutVerticalStackPanel";
 import { shinyPlotOutputInfo } from "./ShinyPlotOutput";
 import { shinySliderInputInfo } from "./ShinySliderInput";
+import { UiComponentInfo } from "./UiComponentInfo";
 
 type AllowedBaseElements = HTMLDivElement;
 type PassthroughProps = DragAndDropHandlers &
@@ -30,11 +27,7 @@ export type UiNodeComponent<NodeSettings extends object> = React.FC<
 >;
 
 export const uiComponentAndSettings: {
-  [UiName in ShinyUiNames]: {
-    UiComponent: UiNodeComponent<ShinyUiArguments[UiName]>;
-    SettingsComponent: SettingsUpdaterComponent<ShinyUiArguments[UiName]>;
-    acceptsChildren: boolean;
-  };
+  [UiName in ShinyUiNames]: UiComponentInfo<ShinyUiArguments[UiName]>;
 } = {
   "shiny::plotOutput": shinyPlotOutputInfo,
   "shiny::sliderInput": shinySliderInputInfo,
@@ -43,35 +36,3 @@ export const uiComponentAndSettings: {
   "gridlayout::grid_page": gridlayoutGridPageInfo,
   "gridlayout::vertical_stack_panel": gridlayoutVerticalStackPanelInfo,
 };
-
-export const defaultSettingsForElements: ShinyUiNode[] = [
-  {
-    uiName: "shiny::plotOutput",
-    uiArguments: { outputId: "plot" },
-  },
-  {
-    uiName: "shiny::sliderInput",
-    uiArguments: {
-      inputId: "slider",
-      label: "Slider",
-      min: 0,
-      value: 5,
-      max: 10,
-    },
-  },
-  {
-    uiName: "gridlayout::title_panel",
-    uiArguments: { title: "Title from Chooser" },
-  },
-  {
-    uiName: "gridlayout::grid_panel",
-    uiArguments: {
-      verticalAlign: "center",
-      horizontalAlign: "center",
-    },
-  },
-  {
-    uiName: "gridlayout::vertical_stack_panel",
-    uiArguments: gridlayoutVerticalStackPanelDefaultSettings,
-  },
-];
--- src/components/Shiny-Ui-Elements/ElementsPalette/index.tsx
+++ src/components/Shiny-Ui-Elements/ElementsPalette/index.tsx
@@ -1,37 +1,45 @@
-import { ShinyUiNode } from "components/Shiny-Ui-Elements/uiNodeTypes";
+import {
+  ShinyUiNames,
+  ShinyUiNode,
+} from "components/Shiny-Ui-Elements/uiNodeTypes";
 import * as React from "react";
 import { assignElementDragData } from "../DragAndDropHelpers/useDragAndDropElements";
-import { defaultSettingsForElements } from "../Elements/uiComponentAndSettings";
-import elementIcons from "./elementIcons";
+import { uiComponentAndSettings } from "../Elements/uiComponentAndSettings";
 import classes from "./styles.module.css";
 
 export default function ElementsPalette({
-  availableUi = defaultSettingsForElements,
+  availableUi = uiComponentAndSettings,
 }: {
-  availableUi?: ShinyUiNode[];
+  availableUi?: typeof uiComponentAndSettings;
 }) {
+  const uiNames = Object.keys(availableUi) as ShinyUiNames[];
+
   return (
     <div className={classes.OptionsList}>
-      {availableUi.map((node) => {
-        return <ElementIcon key={node.uiName} {...node} />;
+      {uiNames.map((uiName) => {
+        const { iconSrc, title, defaultSettings } = availableUi[uiName];
+        if (iconSrc === undefined) {
+          return null;
+        }
+        const uiNode = {
+          uiName,
+          uiArguments: defaultSettings,
+        } as ShinyUiNode;
+        return (
+          <div
+            key={uiName}
+            className={classes.OptionItem}
+            draggable
+            onDragStart={(e) => {
+              // Tag the drag event with the element type being dragged
+              assignElementDragData(e, { node: uiNode });
+            }}
+          >
+            <img src={iconSrc} alt={title} />
+            <label>{title}</label>
+          </div>
+        );
       })}
-    </div>
-  );
-}
-
-function ElementIcon(node: ShinyUiNode) {
-  const { iconSrc, title } = elementIcons[node.uiName];
-  return (
-    <div
-      className={classes.OptionItem}
-      draggable
-      onDragStart={(e) => {
-        // Tag the drag event with the element type being dragged
-        assignElementDragData(e, { node });
-      }}
-    >
-      <img src={iconSrc} alt={title} />
-      <label>{title}</label>
     </div>
   );
 }
