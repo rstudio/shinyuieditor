96288cf8daaaddac503b80e1b3e1ebcbefc1d595
Nick Strayer <nick.strayer@gmail.com>
2022-03-02 12:39:29 -0500

    Added a simple check to make sure a node can't be moved to it's own child

--- src/components/Shiny-Ui-Elements/UiNode/TreeManipulation/moveNode.ts
+++ src/components/Shiny-Ui-Elements/UiNode/TreeManipulation/moveNode.ts
@@ -3,6 +3,7 @@ import {
   ShinyUiNode,
 } from "components/Shiny-Ui-Elements/Elements/uiNodeTypes";
 import produce from "immer";
+import { sameArray } from "utils/equalityCheckers";
 
 import {
   addNodeMutating,
@@ -19,6 +20,10 @@ export default function moveNode({
   fromPath: NodePath;
   toPath: NodePath;
 }) {
+  // Sanity checks before performing move
+  if (invalidMove({ fromPath, toPath })) {
+    throw new Error("Invalid move request");
+  }
   return produce(tree, (treeDraft) => {
     // Gather node first
     const nodeToMove = getNode(treeDraft, fromPath);
@@ -33,4 +38,28 @@ export default function moveNode({
     // Remove it from its previous position
     removeNodeMutating({ tree: treeDraft, path: fromPath });
   });
+}
+
+function invalidMove({
+  fromPath,
+  toPath,
+}: {
+  fromPath: NodePath;
+  toPath: NodePath;
+}): boolean {
+  // Can't make an item its own child
+  if (aChildOfB(toPath, fromPath)) return true;
+
+  return false;
+}
+
+function aChildOfB(aPath: NodePath, bPath: NodePath): boolean {
+  const aDepth = aPath.length;
+  const bDepth = bPath.length;
+  if (aDepth < bDepth) {
+    return false;
+  }
+
+  // If the path up to the depth of b is the same, then we have a child
+  return sameArray(aPath.slice(0, bDepth), bPath);
 }
