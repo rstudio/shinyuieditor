09988859d4919ce34f041d89514bb36eec645489
Nick Strayer <nick.strayer@gmail.com>
2022-03-02 15:20:56 -0500

    Add split in rendering for the container and leaf nodes

--- src/components/Shiny-Ui-Elements/UiNode/index.tsx
+++ src/components/Shiny-Ui-Elements/UiNode/index.tsx
@@ -11,6 +11,7 @@ import {
   NodePath,
   ShinyUiNode,
   shinyUiNodeInfo,
+  UiContainerNodeComponent,
   UiNodeComponent,
 } from "../Elements/uiNodeTypes";
 
@@ -26,40 +27,55 @@ const UiNode = ({
 }: { path?: NodePath; selectedPath: NodePath | null } & ShinyUiNode) => {
   const { uiName, uiArguments, uiChildren } = node;
   const setNodeSelection = React.useContext(NodeSelectionContext);
+  const isSelected = selectedPath ? sameArray(path, selectedPath) : false;
 
   const componentInfo = shinyUiNodeInfo[uiName];
-  const Comp = componentInfo.UiComponent as UiNodeComponent<typeof uiArguments>;
-
-  const isSelected = selectedPath ? sameArray(path, selectedPath) : false;
 
   const dragAndDropCallbacks = useDragAndDropElements(
     path,
     componentInfo.acceptsChildren
   );
-
   const handleClick = (e: React.MouseEvent) => {
     e.stopPropagation();
     setNodeSelection(path);
   };
-
   const handleStartDrag = createDragStartCallback({ node, currentPath: path });
+
+  if (componentInfo.acceptsChildren === true) {
+    const Comp = componentInfo.UiComponent as UiContainerNodeComponent<
+      typeof uiArguments
+    >;
+
+    return (
+      <Comp
+        uiArguments={uiArguments}
+        uiChildren={node.uiChildren ?? []}
+        {...dragAndDropCallbacks}
+        draggable
+        onDragStart={handleStartDrag}
+        onClick={handleClick}
+      >
+        {uiChildren?.map((childNode, i) => (
+          <UiNode
+            key={path.join(".") + i}
+            path={[...path, i]}
+            selectedPath={selectedPath}
+            {...childNode}
+          />
+        ))}
+        {isSelected ? <div className={classes.selectedOverlay} /> : null}
+      </Comp>
+    );
+  }
+  const Comp = componentInfo.UiComponent as UiNodeComponent<typeof uiArguments>;
 
   return (
     <Comp
       uiArguments={uiArguments}
-      {...dragAndDropCallbacks}
       draggable
       onDragStart={handleStartDrag}
       onClick={handleClick}
     >
-      {uiChildren?.map((childNode, i) => (
-        <UiNode
-          key={path.join(".") + i}
-          path={[...path, i]}
-          selectedPath={selectedPath}
-          {...childNode}
-        />
-      ))}
       {isSelected ? <div className={classes.selectedOverlay} /> : null}
     </Comp>
   );
