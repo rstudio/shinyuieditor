687f29fa6263e80c4a4e4905b44f3d78a8a60821
Nick Strayer <nick.strayer@gmail.com>
2022-03-29 16:54:09 -0400

    Remove react-query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import "App.css";
 import * as React from "react";
 
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 
 import { EditorContainer } from "./EditorContainer";
 import { store } from "./state/store";
 
-// Create a client
-const queryClient = new QueryClient();
-
 export const App = () => {
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <EditorContainer />
-      </QueryClientProvider>
+      <EditorContainer />
     </Provider>
   );
 };
