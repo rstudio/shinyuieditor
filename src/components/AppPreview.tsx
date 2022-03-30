9f240fb74200ebfb7462d3cd1ff0ede48a1f3d6d
Nick Strayer <nick.strayer@gmail.com>
2022-03-30 14:31:29 -0400

    Added basic support for expanding and shrinking the preview

--- src/components/AppPreview.tsx
+++ src/components/AppPreview.tsx
@@ -1,8 +1,25 @@
+import React from "react";
+
 import classes from "./AppPreview.module.css";
+import Button from "./Inputs/Button";
 
 export default function AppPreview({ url }: { url: string }) {
+  const [isFullScreen, setIsFullScreen] = React.useState(false);
+
   return (
-    <div className={classes.container}>
+    <div
+      className={
+        classes.container + (isFullScreen ? " " + classes.fullScreen : "")
+      }
+    >
+      <Button
+        className={classes.expandButton}
+        onClick={() =>
+          setIsFullScreen((currentlyFullScreen) => !currentlyFullScreen)
+        }
+      >
+        {isFullScreen ? "Shrink" : "Expand"}
+      </Button>
       <iframe
         className={classes.previewFrame}
         src={url}
