7f4c3fb27fbde1ec142f06be350bc600bdac9b37
Nick Strayer <nick.strayer@gmail.com>
2022-04-06 12:59:19 -0400

    Remove debugging header

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -3,8 +3,5 @@ import type { GlobalProvider } from "@ladle/react";
 import ReduxProvider from "../src/state/ReduxProvider";
 
 export const Provider: GlobalProvider = ({ children, globalState }) => (
-  <ReduxProvider>
-    <h1>Theme: {globalState.theme}</h1>
-    {children}
-  </ReduxProvider>
+  <ReduxProvider>{children}</ReduxProvider>
 );
