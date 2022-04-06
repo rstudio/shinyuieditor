ea1f0dc0b1fab4f993b6408f334aa38ddddd4a92
Nick Strayer <nick.strayer@gmail.com>
2022-04-06 15:04:45 -0400

    Give styles to everything used in ladle

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -2,6 +2,8 @@ import type { GlobalProvider } from "@ladle/react";
 
 import ReduxProvider from "../src/state/ReduxProvider";
 
+import "../src/App.css";
+
 export const Provider: GlobalProvider = ({ children, globalState }) => (
   <ReduxProvider>{children}</ReduxProvider>
 );
