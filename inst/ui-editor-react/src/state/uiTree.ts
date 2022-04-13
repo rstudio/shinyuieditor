17df84e81923816349a092cb7e3073a2d6ea2e22
Nick Strayer <nick.strayer@gmail.com>
2022-04-13 17:22:43 -0400

    Add any default values for omitted arguments in the initially provided ui tree
    This prevents out-of-sync state between the settings panels and uiArguments that were omitted by the user in hand-writing their code

--- inst/ui-editor-react/src/state/uiTree.ts
+++ inst/ui-editor-react/src/state/uiTree.ts
@@ -1,12 +1,17 @@
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
-import type { ShinyUiNode } from "components/Shiny-Ui-Elements/uiNodeTypes";
+import type {
+  ShinyUiChildren,
+  ShinyUiNode,
+} from "components/Shiny-Ui-Elements/uiNodeTypes";
+import { shinyUiNodeInfo } from "components/Shiny-Ui-Elements/uiNodeTypes";
 import type { PlaceNodeArguments } from "components/UiNode/TreeManipulation/placeNode";
 import { placeNodeMutating } from "components/UiNode/TreeManipulation/placeNode";
 import type { RemoveNodeArguments } from "components/UiNode/TreeManipulation/removeNode";
 import { removeNodeMutating } from "components/UiNode/TreeManipulation/removeNode";
 import type { UpdateNodeArguments } from "components/UiNode/TreeManipulation/updateNode";
 import { updateNodeMutating } from "components/UiNode/TreeManipulation/updateNode";
+import { subtractElements } from "utils/array-helpers";
 
 export const initialUiTree: ShinyUiNode = {
   uiName: "gridlayout::grid_page",
@@ -104,7 +109,7 @@ export const uiTreeSlice = createSlice({
   initialState: initialUiTree as ShinyUiNode,
   reducers: {
     INIT_STATE: (tree, action: PayloadAction<{ initialState: ShinyUiNode }>) =>
-      action.payload.initialState,
+      fillInDefaultValues(action.payload.initialState),
     UPDATE_NODE: (tree, action: PayloadAction<UpdateNodeArguments>) => {
       updateNodeMutating(tree, action.payload);
     },
@@ -116,6 +121,45 @@ export const uiTreeSlice = createSlice({
     },
   },
 });
+
+/**
+ *
+ * @param uiTree Shiny Ui Tree
+ * @returns The ui tree modified by adding default values of any ommitted
+ * properties with default values.
+ *
+ * It's not terribly clear if this is a good idea or not as it will immediately
+ * mess with the user's code but will eliminate confusion when the settings
+ * options controls don't actually match the presented values
+ */
+function fillInDefaultValues(uiTree: ShinyUiNode): ShinyUiNode {
+  const { uiName, uiArguments, uiChildren } = uiTree;
+
+  const defaultSettingsForNode = shinyUiNodeInfo[uiName].defaultSettings;
+
+  const newUiArguments = { ...defaultSettingsForNode, ...uiArguments };
+
+  const addedSettings = subtractElements(
+    Object.keys(newUiArguments),
+    Object.keys(uiArguments)
+  );
+  if (addedSettings.length > 0) {
+    console.log(
+      "Filled in missing arguments for node " + uiTree.uiName,
+      addedSettings
+    );
+  }
+
+  const newUiChildren = uiChildren
+    ? uiChildren.map((childNode) => fillInDefaultValues(childNode))
+    : undefined;
+
+  return {
+    uiName,
+    uiArguments: newUiArguments,
+    uiChildren: newUiChildren,
+  } as ShinyUiNode;
+}
 
 // Action creators are generated for each case reducer function
 export const { UPDATE_NODE, PLACE_NODE, DELETE_NODE, INIT_STATE } =
