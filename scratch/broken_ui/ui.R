7256ba1ba1a0b62e91c520459211d6c00f4e64a7
Nick Strayer <nick.strayer@gmail.com>
2022-05-09 14:41:36 -0400

    Add example of app with ui that can't be parsed

---
+++ scratch/broken_ui/ui.R
@@ -0,0 +1,4 @@
+my_custom_ui_fn(
+  app_title = "hi there",
+  app_contents = div("App body")
+)
--- scratch/start_editor.R
+++ scratch/start_editor.R
@@ -11,3 +11,6 @@ launch_editor(app_loc = here::here("scratch/just_server"))
 launch_editor(app_loc = here::here("scratch/just_ui"))
 launch_editor(app_loc = here::here("scratch/empty_directory"))
 launch_editor(app_loc = here::here("scratch/non_existant"))
+launch_editor(app_loc = here::here("scratch/broken_ui"))
+
+launch_editor(app_loc = here::here("inst/app-templates/empty/"))
