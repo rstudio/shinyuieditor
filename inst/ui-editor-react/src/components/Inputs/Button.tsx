be8c537029292148bafd3bef3a8b5df87d73a983
Nick Strayer <nick.strayer@gmail.com>
2022-05-13 17:27:54 -0400

    Add new transparent variant and the ability to chain variants together for buttons

--- inst/ui-editor-react/src/components/Inputs/Button.tsx
+++ inst/ui-editor-react/src/components/Inputs/Button.tsx
@@ -2,18 +2,24 @@ import type { ButtonHTMLAttributes } from "react";
 
 import classes from "./Button.module.css";
 
+type ButtonVariant = "delete" | "regular" | "icon" | "transparent";
 const Button: React.FC<
   {
-    variant?: "delete" | "regular" | "icon";
+    variant?: ButtonVariant | ButtonVariant[];
     className?: string;
   } & ButtonHTMLAttributes<HTMLButtonElement>
 > = ({ children, variant = "regular", className, ...passthroughProps }) => {
+  const variant_classes: string = variant
+    ? Array.isArray(variant)
+      ? variant.map((v) => classes[v]).join(" ")
+      : classes[variant]
+    : "";
   return (
     <button
       className={
         classes.button +
         " " +
-        classes[variant] +
+        variant_classes +
         (className ? " " + className : "")
       }
       {...passthroughProps}
