2964402155c049063490fdb4df09cd45a281f907
Nick Strayer <nick.strayer@gmail.com>
2022-05-17 11:09:37 -0400

    Add option to omit text from delete button

--- inst/ui-editor-react/src/components/DeleteNodeButton/index.tsx
+++ inst/ui-editor-react/src/components/DeleteNodeButton/index.tsx
@@ -5,7 +5,13 @@ import type { NodePath } from "components/Shiny-Ui-Elements/uiNodeTypes";
 
 import classes from "./styles.module.css";
 
-function DeleteNodeButton({ path }: { path: NodePath }) {
+function DeleteNodeButton({
+  path,
+  justIcon = false,
+}: {
+  path: NodePath;
+  justIcon?: boolean;
+}) {
   const deletePanel = useDeleteNode(path);
 
   return (
@@ -16,7 +22,7 @@ function DeleteNodeButton({ path }: { path: NodePath }) {
       title="Delete Element"
     >
       <Trash />
-      Delete Element
+      {justIcon ? null : "Delete Element"}
     </Button>
   );
 }
