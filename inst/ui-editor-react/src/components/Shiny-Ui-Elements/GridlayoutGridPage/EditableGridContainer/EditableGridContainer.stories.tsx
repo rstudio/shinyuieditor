18c418a2447dd43be4020ddb47de1222cb51ae22
Nick Strayer <nick.strayer@gmail.com>
2022-05-27 15:24:41 -0400

    Add some simple children to the grid layout

--- inst/ui-editor-react/src/components/Shiny-Ui-Elements/GridlayoutGridPage/EditableGridContainer/EditableGridContainer.stories.tsx
+++ inst/ui-editor-react/src/components/Shiny-Ui-Elements/GridlayoutGridPage/EditableGridContainer/EditableGridContainer.stories.tsx
@@ -10,11 +10,31 @@ export const EditableGridContainerShowcase: Story<{
   return (
     <div style={containerStyles}>
       <div style={{ outline: "1px solid forestgreen", height: "100%" }}>
-        <h1>Hi there!</h1>
+        <GridItem area="a" color="Tomato" />
+        <GridItem area="b" color="LightSeaGreen" />
+        <GridItem area="c" color="MediumOrchid" />
+        <GridItem area="d" color="Peru" />
       </div>
     </div>
   );
 };
+
+function GridItem({ area, color }: { area: string; color: string }) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeContent: "center",
+        minHeight: "100px",
+        minWidth: "100px",
+        gridArea: area,
+        backgroundColor: color,
+      }}
+    >
+      {area}
+    </div>
+  );
+}
 
 const containerStyles: React.CSSProperties = {
   outline: "1px solid red",
