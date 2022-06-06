7d37a2ed777c6f04a3713fc2d7064ded495bb50d
Nick Strayer <nick.strayer@gmail.com>
2022-06-06 17:58:41 -0400

    Add unknown args example to scratch runner

--- scratch/start_editor.R
+++ scratch/start_editor.R
@@ -7,6 +7,7 @@ launch_editor(
 
 
 launch_editor(app_loc = here::here("scratch/webapp"))
+launch_editor(app_loc = here::here("scratch/unknown-args"))
 launch_editor(app_loc = here::here("scratch/just_server"))
 launch_editor(app_loc = here::here("scratch/just_ui"))
 launch_editor(app_loc = here::here("scratch/empty_directory"))
