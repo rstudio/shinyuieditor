37850d845db54cc39e17e773ae115237acb06f59
Nick Strayer <nick.strayer@gmail.com>
2022-06-08 18:55:20 -0400

    Add ability to load libraries app uses into parsing environment before starting app parsing

--- R/ui_code_to_tree.R
+++ R/ui_code_to_tree.R
@@ -1,5 +1,27 @@
-ui_code_to_tree <- function(ui_expr){
+ui_code_to_tree <- function(ui_expr, packages = c()){
+
+  # Setup an environment for parsing that has the proper libraries in it
+  parsing_env <- create_env_with_packages(packages)
+
   ui_expr %>%
-    parse_ui_fn() %>%
+    parse_ui_fn(env = parsing_env) %>%
     update_ui_nodes()
+}
+
+
+create_env_with_packages <- function(packages){
+
+  env <- new.env(parent=rlang::caller_env())
+
+  if (length(packages) == 0) return(env)
+
+  rlang::env_bind( env, packages=packages )
+
+  local({
+    for(pkg in packages){
+      library(pkg, character.only = TRUE)
+    }
+  }, envir = env)
+
+  env
 }
