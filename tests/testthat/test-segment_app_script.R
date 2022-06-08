98416fb88b0b5de7132512105e3522e10d5464f3
Nick Strayer <nick.strayer@gmail.com>
2022-06-08 14:25:07 -0400

    Added simple test to make sure that the ui locating is working

---
+++ tests/testthat/test-segment_app_script.R
@@ -0,0 +1,50 @@
+start_app_code <- 'library(shiny)
+library(gridlayout)
+
+# A comment about this app
+ui <- grid_page(
+  layout = c(
+    "header",
+    "distPlot"
+  ),
+  grid_panel_text(
+    area = "header",
+    content = "Single File App",
+    h_align = "start",
+    is_title = FALSE
+  ),
+  grid_panel_plot(area = "distPlot")
+)
+
+
+geyser_data <- faithful[, 2]
+nbins <- 30
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    bins <- seq(min(geyser_data), max(geyser_data), length.out = nbins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(geyser_data, breaks = bins, col = "darkgray", border = "white")
+  })
+}
+
+shinyApp(ui, server)
+'
+start_app_lines <- strsplit(start_app_code, "\n")[[1]]
+
+file_ui_definition_bounds(start_app_lines)
+
+test_that("Can properly locate ui definition", {
+  start_app_bounds <- file_ui_definition_bounds(start_app_lines)
+
+  expect_equal(
+    start_app_bounds$start_line, 5L
+  )
+  expect_equal(
+    start_app_bounds$end_line, 17L
+  )
+})
