a46625941d196a4f558ed318de7ce6f63ef2189a
Nick Strayer <nick.strayer@gmail.com>
2022-06-09 18:32:39 -0400

    Broke up the known ui script into some namespace util functions

--- R/is_known_ui_fn.R
+++ R/is_known_ui_fn.R
@@ -49,39 +49,8 @@ ui_fn_names_namespaced <- c(
   "gridlayout::grid_panel_stack"
 )
 
-
 remove_fn_namespace <- function(fn_name){
   gsub(pattern = "\\w+::", replacement = "", x = fn_name, perl = TRUE)
 }
 
-
-get_namespace_and_fn <- function(namespaced_fn) {
-
-  match_res <- regmatches(
-    x = namespaced_fn,
-    m = gregexec(text = namespaced_fn, pattern = "(\\w+)::(\\w+)")
-  )[[1]]
-
-  list(
-    namespace = match_res[2,],
-    fn = match_res[3,]
-  )
-}
-
-
-
 ui_fn_names_bare <- remove_fn_namespace( ui_fn_names_namespaced)
-
-
-# Make sure that the ui_name passed through has the proper namespace attached to
-# it
-namespace_ui_fn <- function(ui_name){
-  if (ui_name %in% ui_fn_names_namespaced) return(ui_name)
-
-  index_of_name <- which(ui_fn_names_bare == ui_name)
-  if (identical(index_of_name, integer(0L))) {
-    stop("The ui function ", ui_name, " is not in the list of known functions.")
-  }
-
-  ui_fn_names_namespaced[index_of_name]
-}
