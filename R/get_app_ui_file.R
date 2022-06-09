bdd3e4d262d3091762369a829d20a43103bfe282
Nick Strayer <nick.strayer@gmail.com>
2022-06-09 18:19:29 -0400

    Cleaned up launch editor by removing dead code and separating larger helpers into their own scripts

---
+++ R/get_app_ui_file.R
@@ -0,0 +1,25 @@
+get_app_ui_file <- function(app_loc) {
+  # We first try and look for a single app file
+  single_file_app_script <- fs::path(app_loc, "app.R")
+
+  if (fs::file_exists(single_file_app_script)) {
+    return(
+      list(path = single_file_app_script, type = "single-file")
+    )
+  }
+
+
+  plain_ui_file <- fs::path(app_loc, "ui.R")
+
+  if (fs::file_exists(plain_ui_file)) {
+    return(
+      list(path = plain_ui_file, type = "multi-file")
+    )
+  }
+
+  stop(
+    "Can't find an app.R or ui.R file in the provided app_loc. ",
+    "Make sure your working directory is properly set"
+  )
+
+}
--- R/launch_editor.R
+++ R/launch_editor.R
@@ -306,41 +306,6 @@ log_background_app <- function(lines) {
 }
 
 
-get_app_ui_file <- function(app_loc) {
-  # We first try and look for a single app file
-  single_file_app_script <- fs::path(app_loc, "app.R")
-
-  if (fs::file_exists(single_file_app_script)) {
-    return(
-      list(path = single_file_app_script, type = "single-file")
-    )
-  }
-
-
-  plain_ui_file <- fs::path(app_loc, "ui.R")
-
-  if (fs::file_exists(plain_ui_file)) {
-    return(
-      list(path = plain_ui_file, type = "multi-file")
-    )
-  }
-
-  stop(
-    "Can't find an app.R or ui.R file in the provided app_loc. ",
-    "Make sure your working directory is properly set"
-  )
-
-}
-
-
-save_ui_to_file <- function(ui_string, ui_file_loc) {
-  writeLines(
-    text = ui_string,
-    con = ui_file_loc
-  )
-}
-
-
 validate_ui_fn_call <- function(uiName, uiArguments, log_fn) {
   tryCatch(
     {
