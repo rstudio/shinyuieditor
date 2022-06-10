080c1a361389baeb19153c8d77c11d3be8658448
Nick Strayer <nick.strayer@gmail.com>
2022-06-10 11:28:50 -0400

    Document the ui code generating function

--- R/ui_code_to_tree.R
+++ R/ui_code_to_tree.R
@@ -1,3 +1,12 @@
+#' Convert ui code expression to ui tree IR
+#'
+#' @param ui_expr Language object containing code generate an app ui
+#' @param packages List of any packages that need to be loaded into the
+#'   namespace when evaluating the `ui_expr`
+#'
+#' @return A UI tree intermediate representation that can be sent to ui editor
+#'   front-end
+#'
 ui_code_to_tree <- function(ui_expr, packages = c()) {
 
   # Setup an environment for parsing that has the proper libraries in it
@@ -8,7 +17,7 @@ ui_code_to_tree <- function(ui_expr, packages = c()) {
   update_ui_nodes(ui_tree)
 }
 
-
+# Create an environment and attach a series of packages namespaces onto it
 create_env_with_packages <- function(packages) {
   env <- new.env(parent = rlang::caller_env())
 
