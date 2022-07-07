bc4d7efda89c0a2401d936ec5970dcb03202e3a3
Nick Strayer <nick.strayer@gmail.com>
2022-07-07 17:18:45 -0400

    No need to format the json pretty for the binary message passing of the websocket

--- inst/ui-editor-react/src/websocket_hooks/sendWsMessage.tsx
+++ inst/ui-editor-react/src/websocket_hooks/sendWsMessage.tsx
@@ -6,7 +6,7 @@ export function sendWsMessage(
   ws: WebSocket,
   msg: STATE_UPDATE_TYPES | PREVIEW_APP_TYPES
 ) {
-  const msg_blob = new Blob([JSON.stringify(msg, null, 2)], {
+  const msg_blob = new Blob([JSON.stringify(msg)], {
     type: "application/json",
   });
   ws.send(msg_blob);
