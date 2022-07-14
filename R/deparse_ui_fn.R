843c3428d1c887d723941c7a343a426ab74b64f6
Nick Strayer <nick.strayer@gmail.com>
2022-07-14 14:51:26 -0400

    Simplified logic to remove the separation of args and children again

--- R/deparse_ui_fn.R
+++ R/deparse_ui_fn.R
@@ -13,7 +13,7 @@
 #'
 deparse_ui_fn <- function(ui_tree, remove_namespace = FALSE) {
   namespaces_removed <- list()
-  deparse_ui_fn_internal <- function(ui_tree, is_arg = FALSE) {
+  deparse_ui_fn_internal <- function(ui_tree) {
 
     # Is the tree node just a primitive value? In that case we don't need to do
     # any special parsing
@@ -26,28 +26,25 @@ deparse_ui_fn <- function(ui_tree, remove_namespace = FALSE) {
       return(unknown_code_unwrap(ui_tree))
     }
 
-    if (is_arg) {
-      # An argument that is not a primitive type must be a (potentially named)
-      # list
+    # An argument that is not a primitive type must be a (potentially named)
+    # list. In the future this may need to be expanded to more things like plain
+    # arrays but right now lists are the only things that make it past
+    if (!is_ui_tree_node(ui_tree, throw_error = FALSE)) {
       return(plain_list_to_expr(ui_tree))
     }
 
     # If we've made if this far we should be in a full-blown ui node
-    validate_ui_tree_node(ui_tree)
 
     # We can then recurse through the arguments/children to build up the proper
     # argument structure to be reconstructed with call2
-    all_args <- lapply(
+    deparsed_arguments <- lapply(
       ui_tree$uiArguments,
-      deparse_ui_fn_internal,
-      is_arg = TRUE
+      deparse_ui_fn_internal
     )
-    all_children <- lapply(
+    deparsed_children <- lapply(
       ui_tree$uiChildren,
-      deparse_ui_fn_internal,
-      is_arg = FALSE
+      deparse_ui_fn_internal
     )
-
 
     ui_fn_name <- ui_tree$uiName
     if (remove_namespace) {
@@ -60,8 +57,8 @@ deparse_ui_fn <- function(ui_tree, remove_namespace = FALSE) {
     # Now we can reconstruct the original function call with names attached
     rlang::call2(
       parse(text = ui_fn_name)[[1]],
-      !!!all_args,
-      !!!all_children
+      !!!deparsed_arguments,
+      !!!deparsed_children
     )
   }
 
@@ -76,11 +73,19 @@ plain_list_to_expr <- function(x) {
 }
 
 
-validate_ui_tree_node <- function(node) {
+is_ui_tree_node <- function(node, throw_error = FALSE) {
   if (is.null(node$uiName)) {
-    stop("Improperly formatted ui tree found - missing uiName property")
+    if (throw_error) {
+      stop("Improperly formatted ui tree found - missing uiName property")
+      return(FALSE)
+    }
   }
   if (is.null(node$uiArguments)) {
-    stop("Improperly formatted ui tree found - missing uiArguments property")
+    if (throw_error) {
+      stop("Improperly formatted ui tree found - missing uiArguments property")
+    }
+    return(FALSE)
   }
+
+  TRUE
 }
