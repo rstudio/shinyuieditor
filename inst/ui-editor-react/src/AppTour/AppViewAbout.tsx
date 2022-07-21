fd787fa8ccad338b6e88451cb24adee0cbc44d5e
Nick Strayer <nick.strayer@gmail.com>
2022-07-21 12:47:31 -0400

    More fleshed out tour text

---
+++ inst/ui-editor-react/src/AppTour/AppViewAbout.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+export const AppViewAbout = (
+  <div>
+    <p>
+      The app view shows a skeleton view of the current state of your app's UI.
+    </p>
+    <p>
+      You can click on elements to select them or drag them around to move them.
+    </p>
+    <p>Cards can be resized by dragging resize handles on the sides.</p>
+    <p>
+      Rows and Columns can be resized by dragging between tracts and added by
+      hovering over the left and top respectively to reveal the tract controls
+      widget.
+    </p>
+
+    <p>
+      <a href="https://rstudio.github.io/shinyuieditor/articles/how-to.html#show-size-widget">
+        More info
+      </a>
+    </p>
+  </div>
+);
