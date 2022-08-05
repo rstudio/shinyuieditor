679d8f5dd24412b244e4d1a35f6d74f6ff3c6a21
Nick Strayer <nick.strayer@gmail.com>
2022-08-05 17:31:01 -0400

    Host on port 3000 so the cypress goes to the right place

--- inst/ui-editor-react/vite.config.ts
+++ inst/ui-editor-react/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 
 const setup = ({ mode }) => {
   return defineConfig({
+    server: { port: 3000 },
     plugins: [react(), tsconfigPaths()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
