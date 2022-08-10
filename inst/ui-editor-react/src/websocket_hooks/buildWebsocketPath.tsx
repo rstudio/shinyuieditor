b75c8cd8b58b05953a1520e76aa363d4aa6e1843
Nick Strayer <nick.strayer@gmail.com>
2022-08-10 14:16:39 -0400

    More cleaning up and better comments for the websocket path creation function
    Also fixes bug where dev mode's early exit left off the websocket protocol prefix.

--- inst/ui-editor-react/src/websocket_hooks/buildWebsocketPath.tsx
+++ inst/ui-editor-react/src/websocket_hooks/buildWebsocketPath.tsx
@@ -1,15 +1,16 @@
 import { DEV_MODE } from "env_variables";
 
 export function buildWebsocketPath() {
-  // If we're using the dev proxy we should just go straight to websocket.
-  // Otherwise use the same location as the main app
-  if (DEV_MODE) return "localhost:8888";
-
-  const loc = window.location;
+  // If we're in dev mode the app itself will be hosted on the dev server which
+  // is not where the websocket communication happens. In this situation we rely
+  // on the backend of the ui editor to be running on port `8888` of localhost.
+  const domain = DEV_MODE
+    ? "localhost:8888"
+    : window.location.host + window.location.pathname;
 
   // The type of the websocket protocol will change depending on if we're on a
   // http (typically localhost) or https (something like workbench)
-  const protocol = loc.protocol === "https:" ? "wss:" : "ws:";
+  const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
 
-  return protocol + "//" + loc.host + loc.pathname;
+  return protocol + "//" + domain;
 }
