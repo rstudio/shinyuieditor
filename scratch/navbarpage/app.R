87ae5cce136dd28251e5c978c0aed0556d27b3e6
Nick Strayer <nick.strayer@gmail.com>
2022-08-23 17:15:03 -0400

    Setup example app that uses navbar page for reference

---
+++ scratch/navbarpage/app.R
@@ -0,0 +1,88 @@
+library(shiny)
+library(gt)
+
+# Here's a comment about this app
+
+
+ui <- navbarPage(
+  "App Title",
+  theme = bslib::bs_theme(),
+  tabPanel(
+    "Settings",
+    sliderInput(
+      inputId = "bins",
+      label = "Number of Bins",
+      min = 12L,
+      max = 100L,
+      value = 30L
+    )
+  ),
+  tabPanel(
+    "Blue Plot",
+    plotOutput("bluePlot")
+  ),
+  tabPanel(
+    "Grey Plot",
+    plotOutput("distPlot")
+  )
+)
+
+
+other_ui <- "hello there"
+table_data <- head(sp500, 10)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+
+  output$bluePlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'steelblue', border = 'white')
+  })
+
+
+  output$stockTable <- render_gt(
+    {
+      # Define the start and end dates for the data range
+      start_date <- "2010-06-07"
+      end_date <- "2010-06-14"
+
+      # Create a gt table based on preprocessed
+      # `sp500` table data
+      gt(table_data) %>%
+        tab_header(
+          title = "S&P 500",
+          subtitle = glue::glue("{start_date} to {end_date}")
+        ) %>%
+        fmt_date(
+          columns = date,
+          date_style = 3
+        ) %>%
+        fmt_currency(
+          columns = c(open, high, low, close),
+          currency = "USD"
+        ) %>%
+        fmt_number(
+          columns = volume,
+          suffixing = TRUE
+        )
+    },
+    width = "100%",
+    height  = "100%"
+  )
+}
+
+shinyApp(ui, server)
+
