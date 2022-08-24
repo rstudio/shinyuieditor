18944605762a007cbc36838a36a5b1d3b95aba33
Nick Strayer <nick.strayer@gmail.com>
2022-08-24 11:29:43 -0400

    Separate logic for prebuilt tree defining to its own script

---
+++ inst/ui-editor-react/src/websocket_hooks/backupUiTree.tsx
@@ -0,0 +1,80 @@
+import { PREBUILT_TREE } from "env_variables";
+import type { ShinyUiNode } from "Shiny-Ui-Elements/uiNodeTypes";
+
+// Ui Tree used if there's no backend connection
+export const basicGridPage: ShinyUiNode = {
+  uiName: "gridlayout::grid_page",
+  uiArguments: {
+    areas: [
+      ["header", "header"],
+      ["sidebar", "plot"],
+      ["sidebar", "plot"],
+    ],
+    row_sizes: ["100px", "1fr", "1fr"],
+    col_sizes: ["250px", "1fr"],
+    gap_size: "1rem",
+  },
+  uiChildren: [
+    {
+      uiName: "gridlayout::grid_card_text",
+      uiArguments: {
+        area: "header",
+        content: "My App",
+        alignment: "start",
+        is_title: true,
+      },
+    },
+    {
+      uiName: "gridlayout::grid_card",
+      uiArguments: {
+        area: "sidebar",
+      },
+      uiChildren: [
+        {
+          uiName: "shiny::sliderInput",
+          uiArguments: {
+            inputId: "mySlider",
+            label: "Slider",
+            min: 2,
+            max: 11,
+            value: 7,
+          },
+        },
+        {
+          uiName: "shiny::numericInput",
+          uiArguments: {
+            inputId: "myNumericInput",
+            label: "Numeric Input",
+            min: 2,
+            max: 11,
+            value: 7,
+            width: "100%",
+          },
+        },
+      ],
+    },
+    {
+      uiName: "gridlayout::grid_card_plot",
+      uiArguments: {
+        area: "plot",
+      },
+    },
+  ],
+};
+
+// Super basic tree for when running e2e tests
+const testingUiTree: ShinyUiNode = {
+  uiName: "gridlayout::grid_page",
+  uiArguments: {
+    areas: [
+      [".", "."],
+      [".", "."],
+    ],
+    row_sizes: ["1fr", "1fr"],
+    col_sizes: ["1fr", "1fr"],
+    gap_size: "1rem",
+  },
+  uiChildren: [],
+};
+
+export const backupUiTree = PREBUILT_TREE ? basicGridPage : testingUiTree;
--- inst/ui-editor-react/src/websocket_hooks/useSyncUiWithBackend.tsx
+++ inst/ui-editor-react/src/websocket_hooks/useSyncUiWithBackend.tsx
@@ -12,6 +12,8 @@ import {
   useWebsocketBackend,
 } from "websocket_hooks/useConnectToWebsocket";
 
+import { backupUiTree } from "./backupUiTree";
+
 type BackendConnectionStatus = "loading" | "no-backend" | "connected";
 
 export type OutgoingStateMsg =
@@ -77,13 +79,7 @@ export function useSyncUiWithBackend() {
 
       // For the demo versions of the app we want there to be a predefined tree
       // instead of an empty grid
-      if (PREBUILT_TREE) {
-        setTree(backupUiTree);
-        console.log("Running in static mode");
-      } else {
-        setTree(testingUiTree);
-        console.log("Running in test mode");
-      }
+      setTree(backupUiTree);
     }
   }, [setTree, status, ws]);
 
@@ -102,67 +98,6 @@ export function useSyncUiWithBackend() {
 
   return { status: connectionStatus, tree };
 }
-
-// Ui Tree used if there's no backend connection
-const backupUiTree: ShinyUiNode = {
-  uiName: "gridlayout::grid_page",
-  uiArguments: {
-    areas: [
-      ["header", "header"],
-      ["sidebar", "plot"],
-      ["sidebar", "plot"],
-    ],
-    row_sizes: ["100px", "1fr", "1fr"],
-    col_sizes: ["250px", "1fr"],
-    gap_size: "1rem",
-  },
-  uiChildren: [
-    {
-      uiName: "gridlayout::grid_card_text",
-      uiArguments: {
-        area: "header",
-        content: "My App",
-        alignment: "start",
-        is_title: true,
-      },
-    },
-    {
-      uiName: "gridlayout::grid_card",
-      uiArguments: {
-        area: "sidebar",
-      },
-      uiChildren: [
-        {
-          uiName: "shiny::sliderInput",
-          uiArguments: {
-            inputId: "mySlider",
-            label: "Slider",
-            min: 2,
-            max: 11,
-            value: 7,
-          },
-        },
-        {
-          uiName: "shiny::numericInput",
-          uiArguments: {
-            inputId: "myNumericInput",
-            label: "Numeric Input",
-            min: 2,
-            max: 11,
-            value: 7,
-            width: "100%",
-          },
-        },
-      ],
-    },
-    {
-      uiName: "gridlayout::grid_card_plot",
-      uiArguments: {
-        area: "plot",
-      },
-    },
-  ],
-};
 
 // Super basic tree for when running e2e tests
 const testingUiTree: ShinyUiNode = {
