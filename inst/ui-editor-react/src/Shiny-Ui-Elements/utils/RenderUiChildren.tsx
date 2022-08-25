2ad085d0055096d248a9d7cf4524fec4728d4a71
Nick Strayer <nick.strayer@gmail.com>
2022-08-25 15:40:25 -0400

    Forgot to pass path down to children

--- inst/ui-editor-react/src/Shiny-Ui-Elements/utils/RenderUiChildren.tsx
+++ inst/ui-editor-react/src/Shiny-Ui-Elements/utils/RenderUiChildren.tsx
@@ -1,5 +1,6 @@
 import UiNode from "components/UiNode";
 import type { NodePath, ShinyUiChildren } from "Shiny-Ui-Elements/uiNodeTypes";
+import { makeChildPath } from "Shiny-Ui-Elements/uiNodeTypes";
 
 /**
  * Render basic Ui children
@@ -16,8 +17,10 @@ export function RenderUiChildren({
   return (
     <>
       {uiChildren.map((childNode, i) => {
-        const nodePath = [...parentPath, i];
-        return <UiNode key={nodePath.join("-")} {...childNode} />;
+        const nodePath = makeChildPath(parentPath, i);
+        return (
+          <UiNode key={nodePath.join("-")} path={nodePath} {...childNode} />
+        );
       })}
     </>
   );
