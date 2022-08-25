9a45d70bbc2fb2f89189ad373d29d1204e0cd175
Nick Strayer <nick.strayer@gmail.com>
2022-08-25 14:38:29 -0400

    Fix click to select propagation event in tabs

--- inst/ui-editor-react/src/components/Tabs/Tabset/Tab.tsx
+++ inst/ui-editor-react/src/components/Tabs/Tabset/Tab.tsx
@@ -11,11 +11,20 @@ export const Tab = ({
   isActive: boolean;
   onSelect: () => void;
 }) => {
+  const handleSelect: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    console.log("Clicked on a tab!");
+    e.stopPropagation();
+    onSelect();
+  };
   return (
     <div
       className={classes.tab}
       data-active-tab={isActive}
-      onClick={() => onSelect()}
+      // Note the use of onClickCapture here. This is needed to prevent the
+      // native click listener from hearing the click event because normally the
+      // click event is propagated all the way to the root before react starts
+      // handling it
+      onClickCapture={handleSelect}
     >
       {name}
     </div>
