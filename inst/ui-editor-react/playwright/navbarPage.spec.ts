fcd1a03e99f09f248aa8b4e8dd901b0017f8aa37
Nick Strayer <nick.strayer@gmail.com>
2022-08-26 11:28:45 -0400

    Start building out the playwright test for navbar pages

--- inst/ui-editor-react/playwright/navbarPage.spec.ts
+++ inst/ui-editor-react/playwright/navbarPage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 import { basicNavbarPage } from "../src/state/backupUiTree";
 
-test("Drag and drop an item onto the grid and name area", async ({ page }) => {
+test("Basic usage of navbar page", async ({ page }) => {
   await page.route("/testing-tree", (route) =>
     route.fulfill({
       status: 200,
@@ -11,4 +11,19 @@ test("Drag and drop an item onto the grid and name area", async ({ page }) => {
   );
 
   await page.goto("/");
+
+  await page.locator("text=Plot 1").click();
+
+  await page
+    .locator(
+      `[aria-label="tab panel Plot 1"] [aria-label="shiny::plotOutput element"]`
+    )
+    .click();
+
+  await page.locator("text=Delete Element").click();
+
+  await page.dragAndDrop(
+    "text=/^Dynamic UI Output$/",
+    `[aria-label="tab panel Plot 1"]`
+  );
 });
