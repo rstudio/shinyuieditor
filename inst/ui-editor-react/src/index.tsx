1f8b58f099589974606fbaf3c05680c7fdae4fd0
Nick Strayer <nick.strayer@rstudio.com>
2022-09-01 15:18:01 -0400

    Update to the new createRoot syntax for react 18

--- inst/ui-editor-react/src/index.tsx
+++ inst/ui-editor-react/src/index.tsx
@@ -1,12 +1,7 @@
-import * as React from "react";
-
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
