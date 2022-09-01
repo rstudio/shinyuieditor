baec2152ccc30f568404d1b7717a8926263f0a7a
Nick Strayer <nick.strayer@rstudio.com>
2022-09-01 16:54:46 -0400

    Dont import textinput when it's not used

--- inst/ui-editor-react/src/Shiny-Ui-Elements/ShinyTabsetPanel/SettingsPanel.tsx
+++ inst/ui-editor-react/src/Shiny-Ui-Elements/ShinyTabsetPanel/SettingsPanel.tsx
@@ -1,4 +1,3 @@
-import { TextInput } from "components/Inputs/TextInput/TextInput";
 import type { SettingsUpdaterComponent } from "Shiny-Ui-Elements/uiNodeTypes";
 
 import type { TabsetPanelSettings } from ".";
