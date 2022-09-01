f9259519e638c722daa257d320095d4d20432ef9
Nick Strayer <nick.strayer@rstudio.com>
2022-09-01 16:11:13 -0400

    Move some general helpers from navbarpage into tabPanel as they are more closely related there

--- inst/ui-editor-react/src/Shiny-Ui-Elements/ShinyNavbarPage/ShinyNavbarPage.tsx
+++ inst/ui-editor-react/src/Shiny-Ui-Elements/ShinyNavbarPage/ShinyNavbarPage.tsx
@@ -2,37 +2,13 @@ import TabPanel from "components/Tabs/TabPanel/TabPanel";
 import Tabset from "components/Tabs/Tabset/Tabset";
 import UiNode from "components/UiNode/UiNode";
 import { makeChildPath, pathToString } from "Shiny-Ui-Elements/nodePathUtils";
-import { shinyTabPanelDefaultSettings } from "Shiny-Ui-Elements/ShinyTabPanel";
-import type {
-  ShinyUiNode,
-  ShinyUiNodeByName,
-  UiNodeComponent,
-} from "Shiny-Ui-Elements/uiNodeTypes";
+import type { UiNodeComponent } from "Shiny-Ui-Elements/uiNodeTypes";
+
+import { isTabPanelNode } from "../ShinyTabPanel/tabPanelHelpers";
 
 import type { NavbarPageSettings } from "./index";
 
 import classes from "./ShinyNavbarPage.module.css";
-
-type TabPanelNode = ShinyUiNodeByName["shiny::tabPanel"];
-function isTabPanelNode(node: ShinyUiNode): node is TabPanelNode {
-  return node.uiName === "shiny::tabPanel";
-}
-
-export const newTabPanelNode: ShinyUiNode = {
-  uiName: "shiny::tabPanel",
-  uiArguments: shinyTabPanelDefaultSettings,
-  uiChildren: [],
-};
-
-export function wrapNodeInTabPanel(node: ShinyUiNode): ShinyUiNode {
-  // Already wrapped?
-  if (node.uiName === "shiny::tabPanel") return node;
-
-  return {
-    ...newTabPanelNode,
-    uiChildren: [node],
-  };
-}
 
 const ShinyNavbarPage: UiNodeComponent<NavbarPageSettings> = ({
   uiArguments: { title },
---
+++ inst/ui-editor-react/src/Shiny-Ui-Elements/ShinyTabPanel/tabPanelHelpers.tsx
@@ -0,0 +1,26 @@
+import { shinyTabPanelDefaultSettings } from "Shiny-Ui-Elements/ShinyTabPanel";
+import type {
+  ShinyUiNode,
+  ShinyUiNodeByName,
+} from "Shiny-Ui-Elements/uiNodeTypes";
+
+type TabPanelNode = ShinyUiNodeByName["shiny::tabPanel"];
+export function isTabPanelNode(node: ShinyUiNode): node is TabPanelNode {
+  return node.uiName === "shiny::tabPanel";
+}
+
+export function wrapNodeInTabPanel(node: ShinyUiNode): ShinyUiNode {
+  // Already wrapped?
+  if (node.uiName === "shiny::tabPanel") return node;
+
+  return {
+    ...newTabPanelNode,
+    uiChildren: [node],
+  };
+}
+
+export const newTabPanelNode: ShinyUiNode = {
+  uiName: "shiny::tabPanel",
+  uiArguments: shinyTabPanelDefaultSettings,
+  uiChildren: [],
+};
