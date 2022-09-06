3dc64daed438a42dc677e1b963493794cd9438f8
Nick Strayer <nick.strayer@rstudio.com>
2022-09-06 12:06:23 -0400

    Add option to render custom order for settings inputs with renderProps

--- inst/ui-editor-react/src/components/Inputs/SettingsFormBuilder/SettingsFormBuilder.stories.tsx
+++ inst/ui-editor-react/src/components/Inputs/SettingsFormBuilder/SettingsFormBuilder.stories.tsx
@@ -11,21 +11,45 @@ const defaultSettings = {
   name: "test",
   value: 3,
 };
-
 const settingsInfo = {
   name: { default: "name default", label: "This is my name" },
   value: { default: 42, label: "This is a value" },
 };
-export const Primary = () => {
+
+export const AutoBuild = () => {
   const [value, setValue] = React.useState(defaultSettings);
 
   return (
-    <div>
-      <SettingsFormBuilder
-        settings={value}
-        settingsInfo={settingsInfo}
-        onChange={setValue}
-      />
-    </div>
+    <SettingsFormBuilder
+      settings={value}
+      settingsInfo={settingsInfo}
+      onChange={setValue}
+    />
+  );
+};
+
+export const RenderProps = () => {
+  const [value, setValue] = React.useState(defaultSettings);
+
+  return (
+    <SettingsFormBuilder
+      settings={value}
+      settingsInfo={settingsInfo}
+      onChange={setValue}
+      renderInputs={(Inputs) => {
+        return (
+          <>
+            <section>
+              <h2>Number inputs</h2>
+              {Inputs.value}
+            </section>
+            <section>
+              <h2>Text Inputs</h2>
+              {Inputs.name}
+            </section>
+          </>
+        );
+      }}
+    />
   );
 };
--- inst/ui-editor-react/src/components/Inputs/SettingsFormBuilder/SettingsFormBuilder.tsx
+++ inst/ui-editor-react/src/components/Inputs/SettingsFormBuilder/SettingsFormBuilder.tsx
@@ -1,4 +1,5 @@
 // Extract out only the keys that map to properties of type T
+import "./styles.scss";
 
 type KnownArgTypes = string | number;
 type UiNodeSettingsObj = Record<string, KnownArgTypes>;
@@ -9,59 +10,116 @@ type SettingsInfo<Obj extends UiNodeSettingsObj> = {
 
 type OnChangeCallback<Obj extends UiNodeSettingsObj> = (values: Obj) => void;
 
+type InputComponentsMap<Settings extends UiNodeSettingsObj> = Record<
+  keyof Settings,
+  JSX.Element
+>;
+
+type InputComponentsMap2<Settings extends UiNodeSettingsObj> = {
+  [K in keyof Settings]: JSX.Element;
+};
+
 type SettingsFormBuilderProps<Settings extends UiNodeSettingsObj> = {
   settings: Settings;
   settingsInfo: SettingsInfo<Settings>;
   onChange: OnChangeCallback<Settings>;
+  renderInputs?: (inputsComps: InputComponentsMap<Settings>) => JSX.Element;
 };
 
+function keysOf<T extends Object>(obj: T): Array<keyof T> {
+  return Array.from(Object.keys(obj)) as any;
+}
 export function SettingsFormBuilder<Settings extends UiNodeSettingsObj>({
   settings,
   settingsInfo,
   onChange,
+  renderInputs,
 }: SettingsFormBuilderProps<Settings>) {
-  return (
-    <form>
-      Building settings form for settings
-      {Object.keys(settings).map((name) => {
-        const value = settings[name];
-        let InputComp: JSX.Element;
+  const NameInputComp = keysOf(settings).map((name) => {
+    if (typeof name !== "string")
+      throw new Error("How did that non-string key get in here?");
 
-        if (typeof value === "string") {
-          InputComp = (
-            <input
-              type="text"
-              value={value}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                onChange({
-                  ...settings,
-                  [name]: e.target.value,
-                });
-              }}
-            />
-          );
-        } else {
-          InputComp = (
-            <input
-              type="number"
-              value={value}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                onChange({
-                  ...settings,
-                  [name]: +e.target.value,
-                });
-              }}
-            />
-          );
+    const value = settings[name];
+    return [
+      name,
+      <SettingsInput
+        key={name}
+        name={name}
+        label={settingsInfo[name].label}
+        value={value}
+        onChange={(updatedValue) =>
+          onChange({
+            ...settings,
+            [name]: updatedValue,
+          })
         }
+      />,
+    ];
+  });
 
-        return (
-          <label key={name}>
-            {settingsInfo[name].label}
-            {InputComp}
-          </label>
-        );
-      })}
+  const Inputs = Object.fromEntries(
+    NameInputComp
+  ) as InputComponentsMap2<Settings>;
+
+  return (
+    <form className="SettingsFormBuilder">
+      {renderInputs ? renderInputs(Inputs) : Object.values(Inputs)}
     </form>
   );
+}
+
+function SettingsInput<T extends KnownArgTypes>({
+  label,
+  value,
+  name,
+  onChange,
+}: {
+  label: string;
+  value: T;
+  name: string;
+  onChange: (newVal: T) => void;
+}) {
+  return (
+    <label className="SettingsInput" key={name}>
+      {label}
+      <SettingsInput2 value={value} onChange={onChange} />
+    </label>
+  );
+}
+
+type UpdateCallback<T> = (newVal: T) => void;
+type SettingsInputProps<T> = {
+  value: T;
+  onChange: UpdateCallback<T>;
+};
+function SettingsInput2<T extends KnownArgTypes>({
+  value,
+  onChange,
+}: SettingsInputProps<T>) {
+  if (typeof value === "string") {
+    return (
+      <input
+        type="text"
+        value={value}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const newValue = e.target.value;
+          (onChange as UpdateCallback<string>)(newValue);
+        }}
+      />
+    );
+  }
+
+  if (typeof value === "number") {
+    return (
+      <input
+        type="number"
+        value={value}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          (onChange as UpdateCallback<number>)(Number(e.target.value));
+        }}
+      />
+    );
+  }
+
+  throw new Error("Dont know how to handle this input type yet");
 }
