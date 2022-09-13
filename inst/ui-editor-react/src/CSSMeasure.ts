c742157cda2c09f16a8a8dd48c935b92124ab870
Nick Strayer <nick.strayer@rstudio.com>
2022-09-13 11:11:52 -0400

    Add CSS Measure type predicate function

--- inst/ui-editor-react/src/CSSMeasure.ts
+++ inst/ui-editor-react/src/CSSMeasure.ts
@@ -1,2 +1,7 @@
 export type CSSMeasure = `${number}${CSSUnits}` | "auto";
 export type CSSUnits = "fr" | "px" | "rem" | "%";
+
+const findMeasureRegex = /(^[\d|.])\s*(px|%|rem|fr)|(^auto$)/g;
+export function isCSSMeasure(x: string): x is CSSMeasure {
+  return findMeasureRegex.test(x);
+}
