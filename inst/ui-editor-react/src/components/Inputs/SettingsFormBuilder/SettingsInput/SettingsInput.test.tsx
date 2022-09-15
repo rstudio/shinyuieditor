1b15200c1e705abd36f05b559e0f25db22fe1c10
Nick Strayer <nick.strayer@rstudio.com>
2022-09-15 12:29:25 -0400

    Add test of css input and optional inputs

--- inst/ui-editor-react/src/components/Inputs/SettingsFormBuilder/SettingsInput/SettingsInput.test.tsx
+++ inst/ui-editor-react/src/components/Inputs/SettingsFormBuilder/SettingsInput/SettingsInput.test.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import React from "react";
 
 import { render, screen, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { vi } from "vitest";
 // import jest from "jest";
 
+import type { SettingsUpdateAction } from "./SettingsInput";
 import { SettingsInput } from "./SettingsInput";
 
 describe("Required inputs work", async () => {
@@ -52,25 +54,88 @@ describe("Required inputs work", async () => {
     });
   });
 
-  //   it("CSS input", async () => {
-  //     const onChange = vi.fn();
-  //     render(
-  //       <SettingsInput
-  //         type="cssMeasure"
-  //         value={"4rem"}
-  //         name="cssVal"
-  //         defaultValue="4rem"
-  //         label="CSS Input"
-  //         requiredOrOptional="required"
-  //         onChange={onChange}
-  //       />
-  //     );
+  it("CSS input", async () => {
+    const onChange = vi.fn();
+    render(
+      <SettingsInput
+        type="cssMeasure"
+        value={"4rem"}
+        name="cssVal"
+        defaultValue="4rem"
+        label="My CSS Input"
+        requiredOrOptional="required"
+        onChange={onChange}
+      />
+    );
 
-  //     // screen.debug();
-  //     await waitFor(() => {
-  //       expect(screen.getByLabelText("CSS Input")).toHaveAccessibleDescription(
-  //         /css unit input/
-  //       );
-  //     });
-  //   });
+    await waitFor(() => {
+      expect(screen.getByLabelText("Css Unit Input")).toBeInTheDocument();
+      expect(screen.getByLabelText("My CSS Input")).toBeInTheDocument();
+    });
+  });
+});
+
+function StatefullOptionalText({
+  initialVal,
+  defaultVal,
+}: {
+  initialVal: string;
+  defaultVal: string;
+}) {
+  const [value, setValue] = React.useState<string | undefined>(initialVal);
+  const updateValue = (action: SettingsUpdateAction) => {
+    if (action.type === "UPDATE") {
+      setValue(action.value as string);
+    }
+    if (action.type === "REMOVE") {
+      setValue(undefined);
+    }
+  };
+  return (
+    <SettingsInput
+      name="myStringVal"
+      value={value}
+      defaultValue={defaultVal}
+      type="string"
+      label="Optional String Input"
+      requiredOrOptional="optional"
+      onChange={updateValue}
+    />
+  );
+}
+
+describe("Inputs can be optional", async () => {
+  const initialVal = "initial value";
+  const defaultVal = "default value";
+  it("String input", async () => {
+    render(
+      <StatefullOptionalText initialVal={initialVal} defaultVal={defaultVal} />
+    );
+
+    expect(screen.getByLabelText(`Use myStringVal argument`)).toBeChecked();
+
+    // Starts with initial value
+    expect(screen.getByLabelText("Optional String Input")).toHaveValue(
+      initialVal
+    );
+
+    // Click to disable argument
+    userEvent.click(screen.getByLabelText(`Use myStringVal argument`));
+    await waitFor(() => {
+      // Now the input should be unset
+      expect(screen.getByLabelText("Optional String Input")).toHaveTextContent(
+        /unset/i
+      );
+    });
+
+    // Click to re-enable
+    userEvent.click(screen.getByLabelText(`Use myStringVal argument`));
+
+    // Now the value should be the default value
+    await waitFor(() => {
+      expect(screen.getByLabelText("Optional String Input")).toHaveValue(
+        defaultVal
+      );
+    });
+  });
 });
