23f20392066ae9037a0d16bdcdd37ba653f5da7c
Nick Strayer <nick.strayer@rstudio.com>
2022-09-20 12:14:50 -0400

    Add settings info for grid_card

--- inst/ui-editor-react/src/Shiny-Ui-Elements/GridlayoutGridCard/index.tsx
+++ inst/ui-editor-react/src/Shiny-Ui-Elements/GridlayoutGridCard/index.tsx
@@ -1,5 +1,6 @@
 import containerIcon from "assets/icons/shinyContainer.png";
 import type { CSSMeasure } from "components/Inputs/CSSUnitInput/CSSMeasure";
+import type { UiNodeSettingsInfo } from "components/Inputs/SettingsFormBuilder/inputFieldTypes";
 
 import type { UiComponentInfo } from "../uiNodeTypes";
 
@@ -14,6 +15,26 @@ export type GridCardSettings = {
   item_gap?: CSSMeasure;
 };
 
+export const GridCardSettingsInfo: UiNodeSettingsInfo = {
+  area: {
+    label: "Name of grid area",
+    inputType: "string",
+    defaultValue: "default-area",
+  },
+  title: {
+    inputType: "string",
+    label: "Panel title",
+    defaultValue: "My Card",
+    optional: true,
+  },
+  item_gap: {
+    inputType: "cssMeasure",
+    label: "Gap size between contents",
+    defaultValue: "15px",
+    units: ["px", "rem"],
+  },
+};
+
 export const gridlayoutGridCardDefaultSettings: GridCardSettings = {
   area: "default-area",
   // item_alignment: "top",
@@ -23,6 +44,7 @@ export const gridlayoutGridCardDefaultSettings: GridCardSettings = {
 export const gridlayoutGridCardInfo: UiComponentInfo<GridCardSettings> = {
   title: "Grid Card",
   UiComponent: GridlayoutGridCard,
+  settingsInfo: GridCardSettingsInfo,
   SettingsComponent: GridlayoutGridCardSettings,
   acceptsChildren: true,
   defaultSettings: gridlayoutGridCardDefaultSettings,
