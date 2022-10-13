bc028e5e82d963584ad63ac31071598a20064a33
Nick Strayer <nick.strayer@gmail.com>
2022-10-13 11:57:05 -0400

    Add plotly output ui node
    Update plotly ui node

---
+++ inst/ui-editor-react/src/Shiny-Ui-Elements/PlotlyPlotlyOutput/index.tsx
@@ -0,0 +1,39 @@
+import icon from "assets/icons/shinyPlot.png";
+
+import type { UiComponentInfo } from "../uiNodeTypes";
+
+import PlotlyPlotlyOutput from "./PlotlyPlotlyOutput";
+
+export type PlotlyOutputSettings = {
+  outputId: string;
+  width?: string;
+  height?: string;
+};
+
+export const plotlyPlotlyOutputInfo: UiComponentInfo<PlotlyOutputSettings> = {
+  title: "Plotly Plot",
+  UiComponent: PlotlyPlotlyOutput,
+  settingsInfo: {
+    outputId: {
+      inputType: "string",
+      label: "Output ID for plot",
+      defaultValue: "plot",
+    },
+    width: {
+      label: "Width",
+      inputType: "cssMeasure",
+      defaultValue: "100%",
+    },
+    height: {
+      label: "Height",
+      inputType: "cssMeasure",
+      defaultValue: "400px",
+    },
+  },
+  acceptsChildren: false,
+  iconSrc: icon,
+  category: "Plotting",
+  description: "Output for interactive `plotly` plots.",
+};
+
+export default plotlyPlotlyOutputInfo;
