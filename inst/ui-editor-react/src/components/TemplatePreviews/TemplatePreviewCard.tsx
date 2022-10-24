f77b88a0c07c4e0982b83d183b058fc53a1faa97
Nick Strayer <nick.strayer@rstudio.com>
2022-10-24 17:16:07 -0400

    Add extra code info to template info type

--- inst/ui-editor-react/src/components/TemplatePreviews/TemplatePreviewCard.tsx
+++ inst/ui-editor-react/src/components/TemplatePreviews/TemplatePreviewCard.tsx
@@ -7,10 +7,39 @@ import type { ShinyUiNode } from "Shiny-Ui-Elements/uiNodeTypes";
 
 import { AppTemplatePreview } from "./AppTemplatePreview";
 
+/**
+ * Defines basic information needed to build an app template for the template viewer
+ */
 export type TemplateInfo = {
+  /**
+   * Displayed name of the template in the chooser view
+   */
   title: string;
+  /** Long form description of the template available on hover. This can use
+   * markdown formatting
+   */
   description: string;
+  /**
+   * Main tree definining the template. Used for generating preview and also the
+   * main ui definition of the template
+   */
   uiTree: ShinyUiNode;
+  otherCode: {
+    /**
+     * Extra code that will be coppied unchanged above the ui definition
+     */
+    uiExtra?: string;
+    /**
+     * Extra code that will be copied unchanged above server funtion definition
+     */
+    serverExtra?: string;
+
+    /**
+     * Body of server function. This will be wrapped in the code
+     * `function(input, output){....}`
+     */
+    serverFunctionBody?: string;
+  };
 };
 
 export type LayoutType = "grid" | "navbarPage";
--- inst/ui-editor-react/src/components/TemplatePreviews/app_templates.tsx
+++ inst/ui-editor-react/src/components/TemplatePreviews/app_templates.tsx
@@ -12,20 +12,40 @@ export const app_templates: TemplateInfo[] = [
     title: "Basic Grid App",
     uiTree: basicGridPage,
     description: "I am a basic grid app",
+    otherCode: {
+      serverFunctionBody: `
+      print("Hi from the server of basic grid app")
+      `,
+    },
   },
   {
     title: "Basic Tabs App",
     uiTree: minimalPage,
     description: "I am a basic navbarPage app",
+    otherCode: {
+      serverFunctionBody: `
+      print("Hi from the server of Basic Tabs App")
+      `,
+    },
   },
   {
     title: "Navbar App",
     uiTree: basicNavbarPage,
     description: "I am a basic navbarPage app",
+    otherCode: {
+      serverFunctionBody: `
+      print("Hi from the server of Navbar App")
+      `,
+    },
   },
   {
     title: "Minimal App",
     uiTree: testingUiTree,
     description: "Almost nothing here",
+    otherCode: {
+      serverFunctionBody: `
+      print("Hi from the server of Minimal App")
+      `,
+    },
   },
 ];
