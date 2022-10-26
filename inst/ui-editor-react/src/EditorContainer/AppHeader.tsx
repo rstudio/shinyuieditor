a1b8eb1161b685e1ba1511cb5322ca80c0c255a1
Nick Strayer <nick.strayer@rstudio.com>
2022-10-26 16:55:43 -0400

    Remove button to test template chooser message

--- inst/ui-editor-react/src/EditorContainer/AppHeader.tsx
+++ inst/ui-editor-react/src/EditorContainer/AppHeader.tsx
@@ -1,23 +1,13 @@
 import { AppTour } from "AppTour";
 import SvgShinyLogo from "components/Icons/ShinyLogo";
-import { app_templates } from "components/TemplatePreviews/app_templates";
-import { useRequestTemplate } from "components/TemplatePreviews/useRequestTemplate";
 import { UndoRedoButtons } from "components/UndoRedoButtons/UndoRedoButtons";
 
 export function AppHeader() {
-  const requestTemplate = useRequestTemplate();
-
   return (
     <header>
       <SvgShinyLogo className="shiny-logo" />
       <h1 className="app-title">Shiny UI Editor</h1>
-      <button
-        onClick={() => {
-          requestTemplate({ ...app_templates[0], outputType: "single-file" });
-        }}
-      >
-        Template req
-      </button>
+
       <div className="right">
         <AppTour />
         <div className="divider" />
