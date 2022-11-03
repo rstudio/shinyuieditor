97ea77150b33b5c314503c63dc298a368bdee017
Nick Strayer <nick.strayer@rstudio.com>
2022-11-03 12:35:16 -0400

    Got build and serve working with esbuild

--- inst/ui-editor-react/src/env_variables.ts
+++ inst/ui-editor-react/src/env_variables.ts
@@ -1,19 +1,30 @@
 // These pull environment variables into more standard ones so they can be
 // imported like normal
 
+declare var DEV_MODE_ESBUILD: boolean;
+declare var SHOW_FAKE_PREVIEW_ESBUILD: boolean;
+declare var TESTING_MODE_ESBUILD: boolean;
+
 /**
  * Are we in development mode?
  */
-export const DEV_MODE = import.meta.env.DEV;
+// export const DEV_MODE = import.meta.env.DEV;
+export const DEV_MODE = DEV_MODE_ESBUILD;
+// process.env.DEV_MODE;
 
 /**
  * Should we show the app preview window with a fake app in it or should we hide
  * the preview window in the case a preview app destination is not provided?
  */
-export const SHOW_FAKE_PREVIEW =
-  import.meta.env.VITE_SHOW_FAKE_PREVIEW === "true";
+export const SHOW_FAKE_PREVIEW = SHOW_FAKE_PREVIEW_ESBUILD;
 
 /**
  * Is the app running in our e2e tests?
  */
-export const TESTING_MODE = import.meta.env.VITE_TESTING_MODE === "true";
+export const TESTING_MODE = TESTING_MODE_ESBUILD;
+
+console.log("Env Vars", {
+  DEV_MODE,
+  TESTING_MODE,
+  SHOW_FAKE_PREVIEW,
+});
