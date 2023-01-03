2148d461d7748f4cd68155f8423fb5531287880e
Nick Strayer <nick.strayer@gmail.com>
2023-01-03 18:25:29 -0500

    Add new function that tries to serialize an app files AST as simply as possible

--- R/serialize_ast.R
+++ R/serialize_ast.R
@@ -1,80 +1,3 @@
-# Take a parsed R expression and turn it into a fully serializable ast
-# representation. 
-serialize_ast <- function(expr, source_ref = NULL) {
-
-  if (!is_serializable_node(expr)) {
-    stop("Unknown expression type, can't parse. typeof(node) = ", typeof(expr))
-  }
-  node_names <- names(expr)
-   
-  ast_node <- list()
-  ast_node_names <- c()
-
-  # Keep track of nodes that should be removed from result because they are null
-  # to keep ast compact. Null nodes are non-critical parts of the AST such as
-  # the text of a function call. We will ignore them
-  null_nodes <- c()
-
-  # We use a for loop here instead of an apply function because we need access
-  # to the index for querying source refs
-  for (i in seq_along(expr)) {
-    x <- expr[[i]]
-
-    # Things like df[,1] will have a "missing" node in the ast for the first
-    # argument of `[`,
-    parsed_node <- if (missing(x)) {
-      "<MISSING>"
-    } else if (identical(class(x), "srcref")) {
-      # Don't want to clutter with function source-reference 
-      null_nodes <- c(null_nodes, i)
-      next
-    } else if (is.atomic(x)) {
-      # Numbers, and characters etc..
-      x
-    } else if (is.symbol(x) || is_namespace_call(x)) {
-      # Things like variable names and other syntactically relevant symbols
-      deparse(x)
-    } else if (is_array_call(x)) {
-      eval(x)
-    } else {
-      # This will error if we give it a non-ast-valid node so no need to do
-      # exhaustive checks in this logic
-      serialize_ast(x, attr(expr, "srcref")[[i]])   
-    } 
-
-    ast_node[[i]] <- parsed_node   
-    ast_node_names[i] <- node_names[i]
-  }
-
-  
-  names(ast_node) <- ast_node_names
-
-  # Remove null nodes if neccesary
-  if (length(null_nodes) > 0) {
-    ast_node <- ast_node[-null_nodes]
-  }
-
-  # If source ref info was passed in for the current node, add position data to
-  # a metadata field
-  if (!is.null(source_ref)) {
-    ast_node[["__meta__"]] <- get_source_position(source_ref)
-  }
-
-  if (is.null(names(ast_node))) {
-    names(ast_node) <- seq_along(ast_node)
-  }
-  
-  ast_node
-}
-
-# Translate the raw integer array into meaningful position values. 
-# Tuple is (line#, column#)
-get_source_position <- function(source_ref) {
-  list( 
-    start_pos = c(source_ref[[1]], source_ref[[5]]),
-    end_pos = c(source_ref[[3]], source_ref[[6]])
-  )
-}
 
 is_namespace_call <- function(expr) {
   identical(as.character(expr[[1]]), "::")
@@ -84,9 +7,64 @@ is_array_call <- function(expr) {
   identical(as.character(expr[[1]]), "c")
 }
 
-is_serializable_node <- function(x) {
-  node_type <- typeof(x)
-  identical(node_type, "pairlist") || 
-    identical(node_type, "language") || 
-    identical(node_type, "expression")
+serialize_ast <- function(expr, source_ref = NULL) {
+  num_nodes <- length(expr)
+  node_names <- names(expr)
+   
+  ast_node <- list()
+  ast_node_names <- c()
+
+  for (i in 1:num_nodes) {
+
+    # Using indices here could cause issues, in the situation where we do something like 
+    parsed_node <- parse_ast_node(expr, i)
+
+    if (is.null(parsed_node)) {
+      next
+    }
+
+    ast_node[[i]] <- parsed_node   
+    ast_node_names[i] <- node_names[i]
+  }
+  names(ast_node) <- ast_node_names
+
+  if (!is.null(source_ref)) {
+    ast_node[["__metadata__"]] <- list( 
+      start_line = source_ref[[1]],
+      end_line = source_ref[[3]],
+      start_column = source_ref[[5]],
+      end_column = source_ref[[6]]
+    )
+  }
+  
+  ast_node
+}
+
+parse_ast_node <- function(expr, i) {
+  x <- expr[[i]]
+
+  # Things like df[,1] will have a "missing" node in the ast for the first
+  # argument of `[`,
+  if (missing(x)) {
+    return("<MISSING>")
+  }   
+  
+  # Don't want to clutter with function source-reference 
+  if (identical(class(x), "srcref")) {
+    return(NULL)
+  }
+
+  if (is.symbol(x)) {
+    deparse(x)
+  } else if (is.pairlist(x)) {
+    as.list(x) 
+  } else if (!is.call(x)) {
+    x 
+  } else if (is_namespace_call(x)) {
+    deparse((x))
+  } else if (is_array_call(x)) {
+    eval((x)) 
+  } else {
+    serialize_ast(x, attr(expr, "srcref")[[i]])   
+  }
 }
--- scratch/start_editor_non_interactive.R
+++ scratch/start_editor_non_interactive.R
@@ -11,7 +11,6 @@ library(lobstr)
 file_lines <- readLines("scratch/single-file-app/app.R")
 parsed <- parse(text = file_lines, keep.source = TRUE)
 
-
 ui_expr <- rlang::expr(
   gridlayout::grid_page(
     layout = c(
@@ -34,20 +33,108 @@ ui_expr <- rlang::expr(
   )
 )
 
-# f <- function(a) {
-#   a * 2
-# }
-
-# serialize_ast(f)
 
 fn_expr <- rlang::expr(
   function(a, b = 2) {
     a + b
   }
 )
-serialize_ast(fn_expr)
+
 
 full_ast <- serialize_ast(parsed)
-# jsonlite::toJSON(full_ast, auto_unbox = TRUE)
-# full_ast
+tree(full_ast)
+# is_namespace_call <- function(expr) {
+#   identical(as.character(expr[[1]]), "::")
+# }
+
+# is_array_call <- function(expr) {
+#   identical(as.character(expr[[1]]), "c")
+# }
+
+# lang_to_list <- function(expr, node_metadata = NULL) {
+#   num_nodes <- length(expr)
+   
+#   ast_node <- list()
+
+#   for (i in 1:num_nodes) {
+#     x <- expr[[i]]
+
+#     # Things like df[,1] will have a "missing" node in the ast for the first
+#     # argument of `[`,
+#     if (missing(x)) {
+#       x <- "<MISSING>"
+#     }   
+    
+#     # Don't want to clutter with function source-reference 
+#     if (identical(class(x), "srcref")) {
+#       next
+#     }
+    
+#     ast_node[[i]] <- if (is.symbol(x)) {
+#       deparse(x)
+#     } else if (is.pairlist(x)) {
+#       as.list(x) 
+#     } else if (!is.call(x)) {
+#       x 
+#     } else if (is_namespace_call(x)) {
+#       deparse((x))
+#     } else if (is_array_call(x)) {
+#       eval((x)) 
+#     } else {
+
+#       subnode_metadata <- if (is.null(attr(expr, "srcref"))) {
+#         NULL
+#       } else {
+#         source_ref <- attr(expr, "srcref")[[i]]
+#         list( 
+#           start_line = source_ref[[1]],
+#           end_line = source_ref[[3]],
+#           start_column = source_ref[[5]],
+#           end_column = source_ref[[6]]
+#         )
+#       }
+#       lang_to_list(x, subnode_metadata)   
+#     }
+#   }
+#   names(ast_node) <- names(expr)
+
+#   if (!is.null(node_metadata)) {
+#     ast_node[["__metadata__"]] <- node_metadata
+#   }
+#   ast_node
+# }
+
+# lang_to_list(ui_expr)
+# tree(lang_to_list(ui_expr))
+# jsonlite::toJSON(lang_to_list(fn_expr))
+
+# attr(parsed, "srcref")
+
+# full_ast <- lang_to_list(parsed)
 # tree(full_ast)
+# lobstr::tree(parsed[1], show_attributes = TRUE)
+# lobstr::tree(parsed, show_attributes = TRUE)
+# lobstr::tree(full_ast)
+# names(full_ast)
+
+# lobstr::tree(lang_to_list(ui_expr))
+# jsonlite::toJSON(lang_to_list(ui_expr), pretty = TRUE, auto_unbox = TRUE)
+
+# namespace_expr <- as.list(ui_expr)[[1]]
+
+# deparse(namespace_expr)
+
+# lobstr::tree(lang_to_list(ui_expr))
+# lobstr::tree(ui_expr)
+# as.list(ui_expr)
+
+# lapply(
+#   X = as.list(ui_expr),
+#   FUN = function(x) {
+#     if (is.call(x)) "Call" else "Base"
+#   }
+# )
+
+# as.character(getAST(ui_expr))
+# # jsonlite::toJSON(ui_expr)
+# # lobstr::tree(shinyuieditor:::ui_code_to_tree(ui_expr))
