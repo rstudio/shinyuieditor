2f8642477c735aa74669e06796e3814fbd428fc8
Nick Strayer <nick.strayer@rstudio.com>
2023-01-17 11:48:26 -0500

    Add logic for printing nested function calls

--- inst/editor/src/ast_parsing/build_function_text.test.ts
+++ inst/editor/src/ast_parsing/build_function_text.test.ts
@@ -9,7 +9,6 @@ describe("Can turn AST into function call text with formatting", () => {
       build_function_text([
         { val: "my_fun", type: "s" },
         { name: "a", val: 1, type: "n" },
-        //   { name: "b", val: "two", type: "c" },
       ])
     ).toBe(`my_fun(a = 1)`);
   });
@@ -33,8 +32,17 @@ describe("Can turn AST into function call text with formatting", () => {
       ])
     ).toBe(`my_fun(\n  char_arg = "a",\n  sym_arg = b\n)`);
   });
+});
 
-  test("Nested or complicated args are abbreviated", () => {
+describe("Nested function calls are properly indented", () => {
+  test("Single arg functions can sit on a single line", () => {
+    // prettier-ignore
+    const expected_output = 
+`my_fun(
+  char_arg = "a",
+  nested_arg = nested(a = 1)
+)`;
+
     expect(
       build_function_text([
         { val: "my_fun", type: "s" },
@@ -48,7 +56,119 @@ describe("Can turn AST into function call text with formatting", () => {
           type: "e",
         },
       ])
-    ).toBe(`my_fun(\n  char_arg = "a",\n  nested_arg = <...>\n)`);
+    ).toBe(expected_output);
+  });
+
+  test("Multi-arg functions are broken up over multiple lines and indented", () => {
+    // prettier-ignore
+    const expected_output = 
+`my_fun(
+  char_arg = "a",
+  nested_arg = nested(
+    a = 1,
+    b = 2
+  )
+)`
+
+    expect(
+      build_function_text([
+        { val: "my_fun", type: "s" },
+        { name: "char_arg", val: "a", type: "c" },
+        {
+          name: "nested_arg",
+          val: [
+            { val: "nested", type: "s" },
+            { name: "a", val: 1, type: "n" },
+            { name: "b", val: 2, type: "n" },
+          ],
+          type: "e",
+        },
+      ])
+    ).toBe(expected_output);
+  });
+  test("Double nesting works", () => {
+    // prettier-ignore
+    const expected_output = 
+`my_fun(
+  a = 1,
+  n1 = nested(
+    b = 2,
+    n2 = nested2(
+      c = 3,
+      d = 4
+    )
+  )
+)`
+
+    expect(
+      build_function_text([
+        { val: "my_fun", type: "s" },
+        { name: "a", val: 1, type: "n" },
+        {
+          name: "n1",
+          val: [
+            { val: "nested", type: "s" },
+            { name: "b", val: 2, type: "n" },
+            {
+              name: "n2",
+              val: [
+                { val: "nested2", type: "s" },
+                { name: "c", val: 3, type: "n" },
+                { name: "d", val: 4, type: "n" },
+              ],
+              type: "e",
+            },
+          ],
+          type: "e",
+        },
+      ])
+    ).toBe(expected_output);
+  });
+
+  test("Will smartly put a nested argument on a new line even if it's the only argument", () => {
+    // prettier-ignore
+    const expected_output = 
+`my_fun(
+  n1 = nested(
+    b = 2,
+    c = 3
+  )
+)`
+
+    expect(
+      build_function_text([
+        { val: "my_fun", type: "s" },
+        {
+          name: "n1",
+          val: [
+            { val: "nested", type: "s" },
+            { name: "b", val: 2, type: "n" },
+            { name: "c", val: 3, type: "n" },
+          ],
+          type: "e",
+        },
+      ])
+    ).toBe(expected_output);
+  });
+  test("Short arrays or lists can be kept on single line", () => {
+    // prettier-ignore
+    const expected_output = `my_fun(vec = c(1, 2, 3))`
+
+    expect(
+      build_function_text([
+        { val: "my_fun", type: "s" },
+        {
+          name: "vec",
+          val: [
+            { val: "c", type: "s" },
+            { val: 1, type: "n" },
+            { val: 2, type: "n" },
+            { val: 3, type: "n" },
+          ],
+          type: "e",
+        },
+      ])
+    ).toBe(expected_output);
   });
 });
 
--- inst/editor/src/ast_parsing/build_function_text.ts
+++ inst/editor/src/ast_parsing/build_function_text.ts
@@ -1,32 +1,38 @@
 import type { Array_Or_List_AST } from "./flatten_list";
+import { get_ast_is_array_or_list } from "./flatten_list";
 import type { R_AST, R_AST_Node } from "./r_ast";
 
-export function build_function_text(fn_call_node: R_AST): string {
-  const [fn_name, ...args] = fn_call_node;
+const INDENT_SPACES = 2;
+const INDENT = " ".repeat(INDENT_SPACES);
+/** Newline with indent */
+const NL_INDENT = `\n${INDENT}`;
+
+export function build_function_text(call_node: R_AST): string {
+  // If the function called is an array or list, use the custom printer for that
+  if (get_ast_is_array_or_list(call_node)) {
+    return print_array_or_list(call_node);
+  }
+
+  const [fn_name, ...args] = call_node;
 
   if (typeof fn_name.val !== "string") {
     return "Unknown Ui Code";
   }
 
-  const num_args = args.length;
-  const is_multi_arg = num_args > 1;
+  const is_multi_arg = args.length > 1;
+  const fn_args_list = args.map(print_fn_argument);
+  const fn_args_collapsed = fn_args_list.join(
+    `,${is_multi_arg ? NL_INDENT : " "}`
+  );
 
-  const fn_call: string = `${fn_name.val}(${is_multi_arg ? "\n  " : ""}`;
+  // We want to put the args on a new line from the function name if there are
+  // multiple arguments or one of the arguments is a nested function call
+  // itself.
+  const is_multi_line_call = is_multi_arg || fn_args_collapsed.includes("\n");
 
-  const fn_args: string = args
-    .map((node, i) => {
-      const is_last_arg = i === num_args - 1;
-      const arg_separator = is_multi_arg
-        ? is_last_arg
-          ? "\n)"
-          : ",\n  "
-        : ")";
-
-      return `${print_fn_argument(node)}${arg_separator}`;
-    })
-    .join("");
-
-  return fn_call + fn_args;
+  return `${fn_name.val}(${
+    is_multi_line_call ? NL_INDENT : ""
+  }${fn_args_collapsed}${is_multi_line_call ? "\n" : ""})`;
 }
 
 function print_fn_argument(node: R_AST_Node): string {
@@ -53,13 +59,16 @@ function print_node_val({ val, type }: R_AST_Node): string {
       return val;
     }
     case "e": {
-      // TODO: Check if this is an array or list and print out if it is
-      return "<...>";
+      return indent_line_breaks(build_function_text(val));
     }
     case "u": {
       return "<...>";
     }
   }
+}
+
+function indent_line_breaks(txt: string): string {
+  return txt.replaceAll(/\n/g, `${NL_INDENT}`);
 }
 
 export function print_array_or_list([call, ...elements]: Array_Or_List_AST) {
@@ -72,15 +81,8 @@ export function print_array_or_list([call, ...elements]: Array_Or_List_AST) {
   );
 
   if (total_element_length > 60) {
-    return `c(\n  ${elements_printed.join(",\n  ")}\n)`;
+    return `c(${NL_INDENT}${elements_printed.join(`,${NL_INDENT}`)}\n)`;
   }
 
   return `${call.val}(${elements_printed.join(", ")})`;
 }
-
-// const test = print_array_or_list([
-//   { val: "c", type: "s" },
-//   { val: "a suuuuper long", type: "c" },
-//   { val: "character vec with many arguments", type: "c" },
-//   { val: "splits to different lines", type: "c" },
-// ]);
