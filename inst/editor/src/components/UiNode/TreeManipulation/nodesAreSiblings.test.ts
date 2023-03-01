d9ffc0e8a325e4634ca3d90c1f74f996cb364864
Nick Strayer <nick.strayer@rstudio.com>
2023-03-01 16:51:37 -0500

    Update logic so a node is not it's own sibling in the tree

--- inst/editor/src/components/UiNode/TreeManipulation/nodesAreSiblings.test.ts
+++ inst/editor/src/components/UiNode/TreeManipulation/nodesAreSiblings.test.ts
@@ -1,15 +1,23 @@
-import { nodesAreSiblings } from "./nodesAreSiblings";
+import {
+  nodesAreSiblings,
+  nodesShareImmediateParent,
+} from "./nodesAreSiblings";
 
 describe("Can detect when siblings", () => {
   test("A is a sibling of B", () => {
     expect(nodesAreSiblings([0, 1, 2], [0, 1, 3])).toEqual(true);
   });
+
   test("A is not a sibling of B", () => {
     expect(nodesAreSiblings([0, 2, 2], [0, 1, 3])).toEqual(false);
     expect(nodesAreSiblings([0, 1], [0, 1, 3])).toEqual(false);
   });
 
-  test("A node is its own sibling", () => {
-    expect(nodesAreSiblings([0, 1], [0, 1])).toEqual(true);
+  test("A node is not its own sibling", () => {
+    expect(nodesAreSiblings([0, 1], [0, 1])).toEqual(false);
+  });
+
+  test("A node does share its own common parent", () => {
+    expect(nodesShareImmediateParent([0, 1], [0, 1])).toEqual(true);
   });
 });
