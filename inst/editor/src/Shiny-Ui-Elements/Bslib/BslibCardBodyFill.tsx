8b983517311226ca3e29400340bfa69f397530bd
Nick Strayer <nick.strayer@rstudio.com>
2023-03-07 14:47:54 -0500

    Add max and min height args to card_body_fill

--- inst/editor/src/Shiny-Ui-Elements/Bslib/BslibCardBodyFill.tsx
+++ inst/editor/src/Shiny-Ui-Elements/Bslib/BslibCardBodyFill.tsx
@@ -1,17 +1,17 @@
-import type { UiComponentInfo, UiNodeComponentProps } from "../uiNodeTypes";
+import type { CSSMeasure } from "../../components/Inputs/CSSUnitInput/CSSMeasure";
+import type {
+  ShinyUiNodeByName,
+  UiComponentInfo,
+  UiNodeComponentProps,
+} from "../uiNodeTypes";
 
 import { CardBody } from "./Utils/CardBody";
 import { CardChildrenWithDropNodes } from "./Utils/ChildrenWithDropNodes";
-export const bslibCardBodyInfo: UiComponentInfo<CardBodySettings> = {
-  title: "Card Body",
-  UiComponent: BslibCardBody,
-  settingsInfo: {},
-  acceptsChildren: true,
-  category: "Cards",
-  description: "body holder for bslib cards",
-};
 
-export type CardBodySettings = {};
+export type CardBodySettings = {
+  max_height?: CSSMeasure;
+  min_height?: CSSMeasure;
+};
 
 function BslibCardBody({
   uiArguments,
@@ -29,3 +29,41 @@ function BslibCardBody({
     </CardBody>
   );
 }
+
+export const bslibCardBodyInfo: UiComponentInfo<CardBodySettings> = {
+  title: "Card Body",
+  UiComponent: BslibCardBody,
+  settingsInfo: {
+    max_height: {
+      label: "Max allowed height",
+      inputType: "cssMeasure",
+      optional: true,
+      defaultValue: (node) => {
+        if (!node) return "500px";
+
+        return (
+          (node as ShinyUiNodeByName["bslib::card_body_fill"]).uiArguments
+            .min_height ?? "500px"
+        );
+      },
+      units: ["px", "%"],
+    },
+    min_height: {
+      label: "Min allowed height",
+      inputType: "cssMeasure",
+      optional: true,
+      defaultValue: (node) => {
+        if (!node) return "100px";
+
+        return (
+          (node as ShinyUiNodeByName["bslib::card_body_fill"]).uiArguments
+            .max_height ?? "100px"
+        );
+      },
+      units: ["px", "%"],
+    },
+  },
+  acceptsChildren: true,
+  category: "Cards",
+  description: "body holder for bslib cards",
+};
