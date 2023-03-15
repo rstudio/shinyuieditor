69cacbe7364c831215d75e379be8d44f11a858be
Nick Strayer <nick.strayer@rstudio.com>
2023-03-15 10:49:26 -0400

    Remove hard-coded width from plots so they behave properly filling their containers

--- inst/editor/src/Shiny-Ui-Elements/ShinyPlotOutput/ShinyPlotOutput.tsx
+++ inst/editor/src/Shiny-Ui-Elements/ShinyPlotOutput/ShinyPlotOutput.tsx
@@ -8,10 +8,7 @@ import classes from "./styles.module.css";
 const ShinyPlotOutput: UiNodeComponent<
   ShinyPlotOutputProps,
   { TakesChildren: false }
-> = ({
-  uiArguments: { outputId, width = "300px", height = "200px" },
-  wrapperProps,
-}) => {
+> = ({ uiArguments: { outputId, width, height = "400px" }, wrapperProps }) => {
   return (
     <div
       className={classes.container}
