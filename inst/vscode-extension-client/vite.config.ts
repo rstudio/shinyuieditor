365f342e825bec62d701e1d7c1ba704f4111c249
Nick Strayer <nick.strayer@rstudio.com>
2023-03-16 14:45:40 -0400

    Clean up vite config for extension client build

--- inst/vscode-extension-client/vite.config.ts
+++ inst/vscode-extension-client/vite.config.ts
@@ -1,14 +1,12 @@
 import { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
 
 const setup = ({ mode }) => {
-  console.log("Building with mode", mode);
   return defineConfig({
     base: "./",
-    server: { port: 3000 },
     build: {
       outDir: "../vscode-extension/media/build",
       lib: {
@@ -26,15 +24,6 @@ const setup = ({ mode }) => {
     plugins: [react(), tsconfigPaths()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
-    },
-    test: {
-      include: [`src/**/*.test.{ts,tsx}`],
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "src/setupTests.js",
-      deps: {
-        inline: true,
-      },
     },
   });
 };
