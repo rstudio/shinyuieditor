d2b8df1593cd513fb6f727662e40effbc8d696d8
Nick Strayer <nick.strayer@rstudio.com>
2023-03-23 12:10:15 -0400

    Add context for ability to go back a step in the app state

--- inst/editor/src/EditorContainer/EditorContainer.tsx
+++ inst/editor/src/EditorContainer/EditorContainer.tsx
@@ -1,19 +1,27 @@
+import React from "react";
+
 import { useSyncUiWithBackend } from "../backendCommunication/useSyncUiWithBackend";
 
 import { sizes_inline_styles } from "./App_Layout_Sizes";
 import styles from "./EditorContainer.module.css";
 import { EditorView } from "./EditorView";
 import { HeaderView } from "./HeaderView";
+import { HistoryGoBackwardProvider } from "./HistoryGoBackwardProvider";
 import { LostConnectionPopup } from "./LostConnectionPopup";
 
 export function EditorContainer() {
   const { state, history } = useSyncUiWithBackend();
 
   return (
-    <div className={styles.EditorContainer} style={sizes_inline_styles}>
-      <HeaderView state={state} history={history} />
-      <EditorView state={state} />
-      <LostConnectionPopup />
-    </div>
+    <HistoryGoBackwardProvider
+      goBackward={history.goBackward}
+      canGoBackward={history.canGoBackward}
+    >
+      <div className={styles.EditorContainer} style={sizes_inline_styles}>
+        <HeaderView state={state} history={history} />
+        <EditorView state={state} />
+        <LostConnectionPopup />
+      </div>
+    </HistoryGoBackwardProvider>
   );
 }
