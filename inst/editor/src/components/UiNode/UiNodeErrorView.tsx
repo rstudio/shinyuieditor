3163c03d8de8178d30ef408650043f473b12302e
Nick Strayer <nick.strayer@rstudio.com>
2023-03-23 15:07:55 -0400

    add link to create a github issue about error

--- inst/editor/src/components/UiNode/UiNodeErrorView.tsx
+++ inst/editor/src/components/UiNode/UiNodeErrorView.tsx
@@ -1,26 +1,37 @@
+import { useStore } from "react-redux";
+
 import { useHistoryGoBackward } from "../../EditorContainer/HistoryGoBackwardProvider";
-import type { ShinyUiNode } from "../../Shiny-Ui-Elements/uiNodeTypes";
+import type {
+  NodePath,
+  ShinyUiNode,
+} from "../../Shiny-Ui-Elements/uiNodeTypes";
+import type { RootState } from "../../state/store";
+import { generate_ui_node_github_error_link } from "../../utils/generate_issue_reports";
 import { TooltipButton } from "../PopoverEl/Tooltip";
 
 import styles from "./UiNodeErrorView.module.css";
 
 export function UiNodeErrorView({
   node,
+  path,
   error,
   resetErrorBoundary,
 }: {
   node: ShinyUiNode;
+  path: NodePath;
   error: Error;
   resetErrorBoundary: () => void;
 }) {
   const { goBackward, canGoBackward } = useHistoryGoBackward();
+
+  const store = useStore<RootState>();
 
   return (
     <div className={styles.container}>
       <h3 className={styles.header}>
         Something went wrong rendering {node.uiName}()
       </h3>
-      <p className={styles.information}>Here's the error message</p>
+      <p className={styles.information}>Error message:</p>
       <code className={styles.error_msg}>{error.message}</code>
 
       <div className={styles.actions}>
@@ -46,6 +57,17 @@ export function UiNodeErrorView({
             Undo last change
           </TooltipButton>
         ) : null}
+        <a
+          href={generate_ui_node_github_error_link({
+            node,
+            path,
+            app_state: store.getState(),
+          })}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Submit bug report
+        </a>
       </div>
     </div>
   );
