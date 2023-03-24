f7ee1da8c86aa11d9a8865e6c587d65ebf4bb942
Nick Strayer <nick.strayer@gmail.com>
2023-03-24 15:31:34 -0400

    Make drag-and-drop test more specific with its selectors to make sure it's testing that elements are where they should be

--- inst/editor/playwright/drag-and-drop.spec.ts
+++ inst/editor/playwright/drag-and-drop.spec.ts
@@ -26,16 +26,20 @@ test("Drag and drop an item onto the grid and name area", async ({ page }) => {
   );
 
   // Fill in the area naming popup
-
   await page.locator('[aria-label="Name of new grid area"]').fill("settings");
   // Click text=Done
   await page.locator("text=Done").click();
 
   // Select the action button and change its label to "Action Button"
-  await page.locator("text=My Button").click();
-  await page.locator('[aria-label="Label"]').fill("Action Button");
+
+  const updated_button_label = "My Updated Label";
+  const card_locator = page.locator("[data-grid-area='settings']");
+
+  await card_locator.locator("text=My Button").click();
+
+  await page.locator('[aria-label="Label"]').fill(updated_button_label);
 
   await expect(
-    page.locator("button", { hasText: "Action Button" })
+    card_locator.locator("button", { hasText: updated_button_label })
   ).toBeVisible();
 });
