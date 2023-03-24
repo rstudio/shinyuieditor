c10050024b41480eb19727cc45ce8adf0d1e6423
Nick Strayer <nick.strayer@rstudio.com>
2023-03-24 12:16:42 -0400

    Make Button component ref-forwardable

--- inst/editor/src/components/Inputs/Button/Button.tsx
+++ inst/editor/src/components/Inputs/Button/Button.tsx
@@ -1,29 +1,37 @@
 import type { ButtonHTMLAttributes } from "react";
+import React from "react";
 
 import { mergeClasses } from "../../../utils/mergeClasses";
 
 import classes from "./Button.module.css";
 
 type ButtonVariant = "delete" | "regular" | "icon" | "transparent";
-const Button: React.FC<
-  {
-    variant?: ButtonVariant | ButtonVariant[];
-    className?: string;
-  } & ButtonHTMLAttributes<HTMLButtonElement>
-> = ({ children, variant = "regular", className, ...passthroughProps }) => {
-  const variant_classes: string = variant
-    ? Array.isArray(variant)
-      ? variant.map((v) => classes[v]).join(" ")
-      : classes[variant]
-    : "";
-  return (
-    <button
-      className={mergeClasses(classes.button, variant_classes, className)}
-      {...passthroughProps}
-    >
-      {children}
-    </button>
-  );
-};
+
+type PopoverButtonProps = {
+  variant?: ButtonVariant | ButtonVariant[];
+  className?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = React.forwardRef<HTMLButtonElement, PopoverButtonProps>(
+  (
+    { children, variant = "regular", className, ...passthroughProps },
+    propRef
+  ) => {
+    const variant_classes: string = variant
+      ? Array.isArray(variant)
+        ? variant.map((v) => classes[v]).join(" ")
+        : classes[variant]
+      : "";
+    return (
+      <button
+        ref={propRef}
+        className={mergeClasses(classes.button, variant_classes, className)}
+        {...passthroughProps}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
