d42d4f9e6ad9cfb29b72e089254a2191a7723e16
Nick Strayer <nick.strayer@gmail.com>
2023-03-24 15:30:48 -0400

    Fix bug where a non-card item dragged onto the grid would not actually get wrapped in a card like it should

--- inst/editor/src/Shiny-Ui-Elements/Gridlayout/Utils/isValidGridItem.tsx
+++ inst/editor/src/Shiny-Ui-Elements/Gridlayout/Utils/isValidGridItem.tsx
@@ -1,18 +1,21 @@
-import type {
-  ShinyUiNodeNames,
-  MakeShinyUiNode,
-  ShinyUiNode,
-} from "../../uiNodeTypes";
-import type { GridItemSettings } from "../GridlayoutCard";
+import type { UnionToTuple } from "util-functions/src/TypescriptUtils";
 
-export type GridItemNode = MakeShinyUiNode<GridItemSettings>;
+import type { KnownShinyUiNode, ShinyUiNode } from "../../uiNodeTypes";
 
-const gridItemNodes = new Set<ShinyUiNodeNames>([
-  "gridlayout::grid_card_text",
+export type GridItemNode = Extract<
+  KnownShinyUiNode,
+  { uiName: `gridlayout::${string}`; uiArguments: { area: string } }
+>;
+
+const gridAwareNodeNamesTuple: UnionToTuple<GridItemNode["uiName"]> = [
   "gridlayout::grid_card",
+  "gridlayout::grid_card_text",
   "gridlayout::grid_card_plot",
-]);
+];
+
+// Place in a set so our checking of the node name is O(1)
+const gridItemNodes = new Set(gridAwareNodeNamesTuple);
 
 export function isValidGridItem(node: ShinyUiNode): node is GridItemNode {
-  return gridItemNodes.has(node.uiName as ShinyUiNodeNames);
+  return gridItemNodes.has(node.uiName as GridItemNode["uiName"]);
 }
