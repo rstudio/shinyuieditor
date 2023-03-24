4a2c201603a5209ba4372a8742542781d819d0d4
Nick Strayer <nick.strayer@rstudio.com>
2023-03-24 16:41:22 -0400

    Add error boundary at the app root level to catch anything else we missed

--- inst/editor/src/SUE.tsx
+++ inst/editor/src/SUE.tsx
@@ -1,12 +1,21 @@
 import type { BackendConnection } from "communication-types";
 import { makeMessageDispatcher } from "communication-types/src/BackendConnection";
+import type { FallbackProps } from "react-error-boundary";
+import { ErrorBoundary } from "react-error-boundary";
 
 import "./App.css";
 import { setupStaticBackend } from "./backendCommunication/staticBackend";
 import { BackendConnectionProvider } from "./backendCommunication/useBackendMessageCallbacks";
+import { GeneralErrorView } from "./components/ErrorCatcher/GeneralErrorView";
 import { EditorContainer } from "./EditorContainer/EditorContainer";
 import type { ShinyUiRootNode } from "./Shiny-Ui-Elements/uiNodeTypes";
 import ReduxProvider from "./state/ReduxProvider";
+import styles from "./SUE.module.css";
+import {
+  generate_gh_issue_url,
+  generate_serialized_state_for_error,
+} from "./utils/generate_issue_reports";
+import { mergeClasses } from "./utils/mergeClasses";
 
 export type SUE_Props = {
   backendDispatch?: BackendConnection;
@@ -42,9 +51,33 @@ export function SUE({
 
   return (
     <ReduxProvider>
-      <BackendConnectionProvider {...dispatch}>
-        <EditorContainer />
-      </BackendConnectionProvider>
+      <ErrorBoundary fallbackRender={WholeAppErrorFallback}>
+        <BackendConnectionProvider {...dispatch}>
+          <EditorContainer />
+        </BackendConnectionProvider>
+      </ErrorBoundary>
     </ReduxProvider>
   );
 }
+
+const WholeAppErrorFallback = (fallbackProps: FallbackProps) => {
+  return (
+    <div className={styles.error_fallback_container}>
+      <div className={mergeClasses("card", styles.error_fallback)}>
+        <h2>Uh oh! you shouldn't be here...</h2>
+        <GeneralErrorView
+          header="ShinyUiEditor encountered an error"
+          generateIssueLink={(state_at_error) =>
+            generate_gh_issue_url({
+              title: "Error in ShinyUiEditor at root",
+              body: `Error at root of editor:\n${generate_serialized_state_for_error(
+                state_at_error
+              )}`,
+            })
+          }
+          {...fallbackProps}
+        />
+      </div>
+    </div>
+  );
+};
