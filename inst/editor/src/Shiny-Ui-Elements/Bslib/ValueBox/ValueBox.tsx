f9cf45d244b288f6de3483899abf0803d8645d35
Nick Strayer <nick.strayer@rstudio.com>
2023-03-31 17:04:19 -0400

    Add showcase side to valuebox arguments

--- inst/editor/src/Shiny-Ui-Elements/Bslib/ValueBox/ValueBox.tsx
+++ inst/editor/src/Shiny-Ui-Elements/Bslib/ValueBox/ValueBox.tsx
@@ -1,5 +1,6 @@
 import icon from "../../../assets/icons/shinyContainer.png";
 import { DropWatcherPanel } from "../../../DragAndDropHelpers/DropWatcherPanel";
+import { mergeClasses } from "../../../utils/mergeClasses";
 import { nodeInfoFactory } from "../../nodeInfoFactory";
 import type { ShinyUiNode, UiNodeComponent } from "../../uiNodeTypes";
 
@@ -11,6 +12,7 @@ type ValueBoxArgs = {
   title: string;
   showcase_icon: string;
   value: ShinyUiNode;
+  showcase_layout?: "left" | "right";
 };
 
 const ValueBox: UiNodeComponent<ValueBoxArgs, { TakesChildren: true }> = ({
@@ -21,7 +23,12 @@ const ValueBox: UiNodeComponent<ValueBoxArgs, { TakesChildren: true }> = ({
 }) => {
   return (
     <div className={styles.container} {...wrapperProps}>
-      <div className={styles.value_box}>
+      <div
+        className={mergeClasses(
+          styles.value_box,
+          uiArguments.showcase_layout === "right" ? styles.showcase_right : null
+        )}
+      >
         <div className={styles.showcase}>
           <BsIcon icon_name={uiArguments.showcase_icon} />
         </div>
@@ -70,6 +77,17 @@ export const bslibValueBoxInfo = nodeInfoFactory<ValueBoxArgs>()({
         uiArguments: {
           outputId: "valueBoxValue",
         },
+      },
+    },
+    showcase_layout: {
+      label: "Showcase Side",
+      inputType: "radio",
+      defaultValue: "left",
+      optional: true,
+      optionsPerColumn: 2,
+      choices: {
+        left: { label: "Left" },
+        right: { label: "Right" },
       },
     },
   },
