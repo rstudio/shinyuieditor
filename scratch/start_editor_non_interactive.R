7d253de36bed89daca32e0c5bda2d584a09bbb60
Nick Strayer <nick.strayer@rstudio.com>
2023-04-07 14:39:23 -0400

    Fix problem where the header and footer would show up in cards even if they werent present. Was due to overzealous use of the :has() selector which is not yet supported in the webviews of vscode

--- scratch/start_editor_non_interactive.R
+++ scratch/start_editor_non_interactive.R
@@ -3,6 +3,6 @@ library(lobstr)
 launch_editor(
   app_loc =  here::here("scratch/single-file-app/"),
   port = 8888,
-  launch_browser = TRUE,
+  launch_browser = FALSE,
   stop_on_browser_close = TRUE
 )
