ef278168796752574139b90c61972cd605c5ede0
Nick Strayer <nick.strayer@rstudio.com>
2023-04-10 16:13:28 -0400

    Simplify the navbar page example tree

--- inst/editor/src/index.tsx
+++ inst/editor/src/index.tsx
@@ -7,8 +7,8 @@ import { setupWebsocketBackend } from "./backendCommunication/websocketBackend";
 import { DEV_MODE } from "./env_variables";
 import { runSUE } from "./runSUE";
 import type { ShinyUiRootNode } from "./Shiny-Ui-Elements/uiNodeTypes";
-// import { basicNavbarPage as devModeTree } from "./state/sample_ui_trees/basicNavbarPage";
-import { bslibCards as devModeTree } from "./state/sample_ui_trees/bslibCards";
+import { basicNavbarPage as devModeTree } from "./state/sample_ui_trees/basicNavbarPage";
+// import { bslibCards as devModeTree } from "./state/sample_ui_trees/bslibCards";
 // import { errorTestingTree as devModeTree } from "./state/sample_ui_trees/errorTesting";
 // const devModeTree = "TEMPLATE_CHOOSER" as ShinyUiRootNode;
 
--- inst/editor/src/state/sample_ui_trees/basicNavbarPage.tsx
+++ inst/editor/src/state/sample_ui_trees/basicNavbarPage.tsx
@@ -6,56 +6,46 @@ import type { KnownShinyUiNode } from "../../Shiny-Ui-Elements/uiNodeTypes";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 export const basicNavbarPage = {
-  id: "navbarPage",
+  id: 'navbarPage',
   namedArgs: {
-    title: "My Navbar Page",
-    collapsible: false,
+    title: 'My Navbar Page',
+    collapsible: false
   },
   children: [
     {
-      id: "tabPanel",
+      id: 'tabPanel',
       namedArgs: {
-        title: "Settings",
+        title: 'Settings'
       },
       children: [
         {
-          id: "tabsetPanel",
-          namedArgs: {},
-          children: [],
-        },
-      ],
+          id: 'sliderInput',
+          namedArgs: {
+            inputId: 'inputId',
+            label: 'Slider Input',
+            min: 0,
+            max: 10,
+            value: 5,
+            width: '100%'
+          }
+        }
+      ]
     },
     {
-      id: "tabPanel",
+      id: 'tabPanel',
       namedArgs: {
-        title: "Plot 1",
+        title: 'Plot 1'
       },
       children: [
         {
-          id: "plotOutput",
+          id: 'plotOutput',
           namedArgs: {
-            outputId: "MyPlot",
-            width: "100%",
-            height: "100%",
-          },
-        },
-      ],
-    },
-    {
-      id: "tabPanel",
-      namedArgs: {
-        title: "Plot 2",
-      },
-      children: [
-        {
-          id: "plotOutput",
-          namedArgs: {
-            outputId: "MyOtherPlot",
-            width: "50%",
-            height: "50%",
-          },
-        },
-      ],
-    },
-  ],
+            outputId: 'MyPlot',
+            width: '100%',
+            height: '100%'
+          }
+        }
+      ]
+    }
+  ]
 } satisfies KnownShinyUiNode;
