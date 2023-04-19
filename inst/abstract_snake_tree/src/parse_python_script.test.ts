a4a75a6936014f41e9f42527e1975b8034aec5dd
Nick Strayer <nick.strayer@rstudio.com>
2023-04-19 10:20:24 -0400

    Flesh out basic tests

--- inst/abstract_snake_tree/src/parse_python_script.test.ts
+++ inst/abstract_snake_tree/src/parse_python_script.test.ts
@@ -1,6 +1,8 @@
 import { parse_python_script } from ".";
 
 import { get_assignment_nodes } from "./get_assignment_nodes";
+import { get_ui_assignment } from "./get_ui_assignment";
+import { node_to_uitree } from "./node_to_uitree";
 const simple_app_script = `
 from shiny import App, render, ui
 
@@ -25,11 +27,40 @@ describe("Parse simple app", () => {
 
   test("Can find assignment nodes", () => {
     const parsed = parse_python_script(simple_app_script);
-    const assignment_nodes = get_assignment_nodes(parsed.rootNode);
-    expect(assignment_nodes.keys).toStrictEqual(["app_ui", "app"]);
+    const assignment_nodes = get_assignment_nodes(parsed);
+    const assignment_node_names = [...assignment_nodes.keys()];
+    expect(assignment_node_names).toEqual(["app_ui", "app"]);
   });
 
   // Test that we can find ui node
+  test("Can find ui node", () => {
+    const parsed = parse_python_script(simple_app_script);
+    const assignment_nodes = get_assignment_nodes(parsed);
+    const ui_node = get_ui_assignment(assignment_nodes);
+    expect(ui_node).not.toBeNull();
+  });
+
+  test("Returns null if no ui node to find", () => {
+    const no_ui = `
+from shiny import App, render, ui
+
+print("Hello world")
+`;
+    const assignment_nodes = get_assignment_nodes(parse_python_script(no_ui));
+    const ui_node = get_ui_assignment(assignment_nodes);
+    expect(ui_node).toBeNull();
+  });
 
   // Test that the parsed ui node looks good
+  test("Can parse ui node", () => {
+    const ui_node = get_ui_assignment(
+      get_assignment_nodes(parse_python_script(simple_app_script))
+    );
+
+    if (!ui_node) {
+      throw new Error("ui_node is null");
+    }
+
+    expect(node_to_uitree(ui_node)).not.toBeNull();
+  });
 });
