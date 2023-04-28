dc8c227fad567fe2999550563f07b3beded7fe3a
Nick Strayer <nick.strayer@rstudio.com>
2023-04-28 14:58:06 -0400

    Update python example app to have read server code

--- scratch/python/app.py
+++ scratch/python/app.py
@@ -1,20 +1,40 @@
 from shiny import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 app_ui = ui.page_navbar(
-    ui.nav(
-        "Settings",
-        ui.input_slider(
-            id="inputId", label="Slider Input", min=0, max=10, value=5, width="100%"
-        ),
-    ),
-    ui.nav("Plot 1", ui.output_plot(id="MyPlot", width="100%", height="100%")),
-    title="My Navbar Page",
-    collapsible=False,
+  ui.nav(
+    "It's Alive!",
+    ui.input_slider(
+      id = "n",
+      label = "Slider Input",
+      min = 5,
+      max = 100,
+      value = 25,
+      width = "100%"
+    )
+  ),
+  ui.nav(
+    "Plot 1",
+    ui.output_plot(
+      id = "MyPlot",
+      width = "100%",
+      height = "400px"
+    )
+  ),
+  title = "My cool app",
+  collapsible = False
 )
 
 
 def server(input, output, session):
-    pass
+    @output
+    @render.plot(alt="A histogram")
+    def MyPlot():
+        np.random.seed(19680801)
+        x = 100 + 15 * np.random.randn(437)
+        plt.hist(x, input.n(), density=True)
+    
 
 
 app = App(app_ui, server)
