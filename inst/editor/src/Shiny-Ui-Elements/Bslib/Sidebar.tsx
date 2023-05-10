2403952bdbc791b3449cb4bd8b4f1d7294f6cee1
Nick Strayer <nick.strayer@gmail.com>
2023-05-10 14:41:04 -0400

    Collapser now has proper outlining and sets selection as you would expect when opening or closing sidebar

--- inst/editor/src/Shiny-Ui-Elements/Bslib/Sidebar.tsx
+++ inst/editor/src/Shiny-Ui-Elements/Bslib/Sidebar.tsx
@@ -1,11 +1,16 @@
-import { ChevronLeft } from "react-bootstrap-icons";
+import { useState } from "react";
 
+import { ChevronLeft } from "react-bootstrap-icons";
+import { getParentPath } from "ui-node-definitions/src/TreeManipulation/getParentPath";
+
+import { useSetCurrentSelection } from "../../state/selectedPath";
 import { add_editor_info_by_id } from "../utils/add_editor_info_to_ui_node";
 
 import classes from "./Sidebar.module.css";
 
 export const bslibSidebar = add_editor_info_by_id("sidebar", {
   UiComponent: ({ namedArgs, children = [], path, wrapperProps }) => {
+    const setSelection = useSetCurrentSelection();
     return (
       <div {...wrapperProps}>
         <div className={classes.sidebar}>
@@ -13,19 +18,57 @@ export const bslibSidebar = add_editor_info_by_id("sidebar", {
             <h3>{namedArgs.title}</h3>
             <p>Sidebar children will go here</p>
           </div>
-          <div
-            className={classes.openToggle}
-            onClick={(e) => {
-              e.preventDefault();
-              e.currentTarget.parentElement?.classList.toggle(
-                classes.collapsed
-              );
+          <CollapserTab
+            onCollapse={() => {
+              setSelection(getParentPath(path));
             }}
-          >
-            <ChevronLeft />
-          </div>
+            onExpand={() => {
+              setSelection(path);
+            }}
+          />
         </div>
       </div>
     );
   },
 });
+
+function CollapserTab({
+  onCollapse,
+  onExpand,
+}: {
+  onCollapse: () => void;
+  onExpand: () => void;
+}) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <div
+      className={classes.openToggle}
+      title={`${collapsed ? "Expand" : "Collapse"} sidebar`}
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const parent_classes = e.currentTarget.parentElement?.classList;
+        if (!parent_classes) {
+          // This should always exist, but just in case...
+          return;
+        }
+
+        const collapsing = !parent_classes.contains(classes.collapsed);
+
+        if (collapsing) {
+          onCollapse();
+          setCollapsed(true);
+          parent_classes.add(classes.collapsed);
+        } else {
+          onExpand();
+          setCollapsed(false);
+          parent_classes.remove(classes.collapsed);
+        }
+      }}
+    >
+      <ChevronLeft />
+    </div>
+  );
+}
