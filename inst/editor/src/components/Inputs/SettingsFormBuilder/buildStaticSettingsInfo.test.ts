fba2da63b192d5edd836c0a3a468ea488ad56c75
Nick Strayer <nick.strayer@rstudio.com>
2023-05-11 11:30:49 -0400

    Fix path

--- inst/editor/src/components/Inputs/SettingsFormBuilder/buildStaticSettingsInfo.test.ts
+++ inst/editor/src/components/Inputs/SettingsFormBuilder/buildStaticSettingsInfo.test.ts
@@ -1,7 +1,7 @@
 import {
   getFirstTabName,
   getTabNames,
-} from "ui-node-definitions/src/Shiny/page_navbar";
+} from "ui-node-definitions/src/Bslib/page_navbar";
 import type { ShinyUiParentNode } from "ui-node-definitions/src/ShinyUiNode";
 import type { Equal, Expect } from "util-functions/src/TypescriptUtils";
 
