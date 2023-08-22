c3a270c90a0a06437fea41f7ea4ef953a567e91d
Nick Strayer <nick.strayer@rstudio.com>
2023-08-22 17:47:13 -0400

    We were not actually ensuring the app script was open before as the function was silently failing due to incorrect argument order and lack of checks for invalid state

--- R/rstudioapi_utils.R
+++ R/rstudioapi_utils.R
@@ -36,7 +36,7 @@ select_server_code <- function(locations, app_loc, app_type) {
     # Make sure the server-containing script is the main active file it's
     # in the right file incase the user has navigated away
 
-    ensure_server_script_open(app_type, app_loc)
+    ensure_server_script_open(app_type = app_type, app_loc = app_loc)
     ranges <- lapply(
         X = locations,
         FUN = client_loc_to_rstudioapi_range
@@ -48,13 +48,13 @@ select_server_code <- function(locations, app_loc, app_type) {
 #' @param snippet Code to be inserted
 #' @param insert_at Location to insert the code at
 #' @param app_loc Path to directory containing Shiny app to be visually edited
-#' (either containing an `app.R` or both a `ui.R` and `server.R`).  
+#' (either containing an `app.R` or both a `ui.R` and `server.R`).
 #' @param app_type Type of app to get the server script for. Can be
 #' "SINGLE-FILE" or "MULTI-FILE". As returned by `get_app_file_type()`
 #' @keywords internal
 insert_server_code <- function(snippet, insert_at, app_loc, app_type) {
 
-  ensure_server_script_open(app_type, app_loc)
+  ensure_server_script_open(app_loc = app_loc, app_type = app_type)
 
   # Remove any vscode snippet related syntax from the snippet as it's not
   # supported in rstudio
@@ -71,7 +71,7 @@ insert_server_code <- function(snippet, insert_at, app_loc, app_type) {
 
 ensure_server_script_open <- function(app_loc, app_type) {
   rstudioapi::navigateToFile(
-    file = get_path_to_app_server_script(app_type, app_loc)
+    file = get_path_to_app_server_script(app_type = app_type, app_loc = app_loc)
   )
 }
 
@@ -89,9 +89,12 @@ get_path_to_app_server_script <- function(app_type, app_loc) {
   if (identical(app_type, "SINGLE-FILE")) {
     return(fs::path(app_loc, "app.R"))
   }
+
   if (identical(app_type, "MULTI-FILE")) {
     return(fs::path(app_loc, "server.R"))
   }
+
+  stop("Failed to figure out path")
 }
 
 
