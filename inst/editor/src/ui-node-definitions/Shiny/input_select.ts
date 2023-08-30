ea1dbb70c09c2ac3ba2322d95df00bd6af88afa2
Nick Strayer <nick.strayer@rstudio.com>
2023-08-30 11:48:08 -0400

    Add selected and width arguments back to select inputs

--- inst/editor/src/ui-node-definitions/Shiny/input_select.ts
+++ inst/editor/src/ui-node-definitions/Shiny/input_select.ts
@@ -1,10 +1,15 @@
+import type { CSSMeasure } from "../inputFieldTypes";
 import type { NamedList } from "../NamedList";
 import { nodeInfoFactory } from "../nodeInfoFactory";
+import type { ShinyUiNode } from "../ShinyUiNode";
 
 export type ShinySelectInputProps = {
   inputId: string;
   label: string;
   choices: NamedList;
+  // Right now we only have single-select select inputs
+  selected?: string;
+  width?: CSSMeasure;
 };
 
 export const input_select = nodeInfoFactory<ShinySelectInputProps>()({
@@ -40,8 +45,43 @@ export const input_select = nodeInfoFactory<ShinySelectInputProps>()({
         "choice b": "b",
       },
     },
+    selected: {
+      label: "Selected",
+      inputType: "dropdown",
+      defaultValue: (x) => {
+        const choice_values = getChoiceValues(x);
+
+        if (choice_values.length === 0) {
+          return "First Value";
+        }
+
+        return choice_values[0];
+      },
+      choices: getChoiceValues,
+      optional: true,
+    },
+
+    width: {
+      inputType: "cssMeasure",
+      label: "Width",
+      defaultValue: "100%",
+      units: ["%", "px", "rem"],
+      optional: true,
+    },
   },
   category: "Inputs",
   description:
     "Create a select list that can be used to choose a single or multiple items from a list of values.",
 });
+
+function getChoiceValues(x: ShinyUiNode | undefined): string[] {
+  if (x?.id !== "selectInput") {
+    throw new Error(
+      `Expected id to be selectInput, but got ${
+        x?.id ?? "unknown type"
+      } instead`
+    );
+  }
+
+  return Object.values((x.namedArgs as ShinySelectInputProps).choices);
+}
