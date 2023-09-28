f142bd590f6fe243bad27e5227766cdeae20a9db
Nick Strayer <nick.strayer@rstudio.com>
2023-09-28 15:09:37 -0400

    Cleanup unused imports and rename variable looped over in card sections

--- inst/website/src/components/CardSection.tsx
+++ inst/website/src/components/CardSection.tsx
@@ -8,17 +8,17 @@ export function CardSection() {
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
       {sections.map(
-        (action, actionIdx) =>
-          action.onHomepage && (
+        (section, sectionIndex) =>
+          section.onHomepage && (
             <div
-              key={action.title}
+              key={section.title}
               className={classNames(
-                actionIdx === 0
+                sectionIndex === 0
                   ? "rounded-tl-lg rounded-tr-lg sm:rounded-tr-none"
                   : "",
-                actionIdx === 1 ? "sm:rounded-tr-lg" : "",
-                actionIdx === sections.length - 2 ? "sm:rounded-bl-lg" : "",
-                actionIdx === sections.length - 1
+                sectionIndex === 1 ? "sm:rounded-tr-lg" : "",
+                sectionIndex === sections.length - 2 ? "sm:rounded-bl-lg" : "",
+                sectionIndex === sections.length - 1
                   ? "rounded-bl-lg rounded-br-lg sm:rounded-bl-none"
                   : "",
                 "group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
@@ -30,19 +30,19 @@ export function CardSection() {
                     "bg-rstudio-blue/30 text-gray-800 inline-flex rounded-lg p-3 ring-4 ring-white"
                   }
                 >
-                  <action.icon className="h-6 w-6" aria-hidden="true" />
+                  <section.icon className="h-6 w-6" aria-hidden="true" />
                 </span>
               </div>
               <div className="mt-8">
                 <h3 className="text-base font-semibold leading-6 text-gray-900">
-                  <a href={action.href} className="focus:outline-none">
+                  <a href={section.href} className="focus:outline-none">
                     {/* Extend touch target to entire panel */}
                     <span className="absolute inset-0" aria-hidden="true" />
-                    {action.title}
+                    {section.title}
                   </a>
                 </h3>
                 <p className="mt-2 text-sm text-gray-500">
-                  {action.description}
+                  {section.description}
                 </p>
               </div>
               <span
