400d77fbb1c57df784eb96d6bfc3e06c3f68bef4
Nick Strayer <nick.strayer@rstudio.com>
2023-09-29 15:59:41 -0400

    Fix internal links for assets

--- inst/website/src/lib/utils.ts
+++ inst/website/src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Make a routing-safe link robust to changes in the base url
+ * @param url Base URL for page to be visited. Should not start with a slash
+ * @returns Link to an internal page in the website. This is useful because the
+ * base url may change and this way we don't have to update every link to match
+ */
+export function internalLink(url: string) {
+  // Remove leading slash if it exists
+  const cleanUrl = url.startsWith("/") ? url.slice(1) : url;
+
+  return `${import.meta.env.BASE_URL}/${cleanUrl}`;
 }
