c1fed01465985c5bca9ccb3e2591142cbdcb19dc
Nick Strayer <nick.strayer@rstudio.com>
2023-10-03 17:36:24 -0400

    Add icon for syncing that can be turned on and off. Needs to actually have actions hooked up to state.

--- inst/editor/src/SettingsPanel/SettingsInput/IdInput.tsx
+++ inst/editor/src/SettingsPanel/SettingsInput/IdInput.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import { Link45deg } from "react-bootstrap-icons";
 
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../../components/PopoverEl/FloatingPopover";
 import { getAllBindingIds } from "../../EditorContainer/getAllBindingIds";
 import { useCurrentAppInfo } from "../../state/app_info";
 import type { InputComponentByType } from "../../ui-node-definitions/inputFieldTypes";
 import { makeLabelId } from "../../ui-node-definitions/inputFieldTypes";
+import { mergeClasses } from "../../utils/mergeClasses";
 import { useUpToDateServerLocations } from "../useUpToDateServerLocations";
 
 export function IdInput({
@@ -21,15 +27,26 @@ export function IdInput({
   const appInfo = useCurrentAppInfo();
   const serverLocations = useUpToDateServerLocations();
 
+  const [syncStatus, setSyncStatus] = React.useState<
+    "synced" | "unsynced" | null
+  >(null);
   const [currValue, setCurrValue] = React.useState(value);
   const [invalidMsg, setInvalidMsg] = React.useState<null | string>(null);
-
-  if (appInfo.mode !== "MAIN") return null;
 
   // Check if the current value exists in the server locations
   const boundToServer =
     serverLocations?.input_positions[value] !== undefined ||
     serverLocations?.output_positions[value] !== undefined;
+
+  React.useEffect(() => {
+    if (boundToServer) {
+      setSyncStatus("synced");
+    } else {
+      setSyncStatus("unsynced");
+    }
+  }, [boundToServer]);
+
+  if (appInfo.mode !== "MAIN") return null;
 
   const app_tree = appInfo.ui_tree;
 
@@ -72,7 +89,46 @@ export function IdInput({
     <>
       <div className="flex items-center gap-1">
         <input {...common_props} type="text" />
-        {boundToServer && <Link45deg className="text-xl text-rstudio-blue" />}
+        {boundToServer && (
+          <Tooltip placement="right-start" initialOpen={true}>
+            <TooltipTrigger>
+              <div className="relative">
+                <Link45deg
+                  className={mergeClasses(
+                    "text-xl relative",
+                    syncStatus === "synced"
+                      ? ["text-rstudio-blue"]
+                      : [
+                          "text-danger after:block after:absolute after:w-px after:bg-danger",
+                        ]
+                  )}
+                  onClick={() => {
+                    setSyncStatus((prev) =>
+                      prev === "unsynced" ? "synced" : "unsynced"
+                    );
+                  }}
+                />
+                {syncStatus === "unsynced" && (
+                  <div
+                    className="bg-danger/50 w-[2px] absolute left-[50%] top-1 bottom-1 pointer-events-none"
+                    style={{ transform: "rotate(320deg)" }}
+                  />
+                )}
+              </div>
+            </TooltipTrigger>
+            <TooltipContent className="max-w-xs">
+              <p className="my-1">
+                {syncStatus === "synced"
+                  ? "Changing ID will cause uses of this ID in server code to update."
+                  : "ID syncing is disabled. Changes to this ID will not be reflected in server code."}
+              </p>
+              <p className="my-1">
+                Click to {syncStatus === "synced" ? "disable" : "enable"}{" "}
+                syncing.
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        )}
       </div>
       {invalidMsg && (
         <div className="text-danger">
