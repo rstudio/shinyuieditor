df6be2eaf2a856de77eaa5fb0eeae080c131fe00
Nick Strayer <nick.strayer@rstudio.com>
2023-10-03 16:02:36 -0400

    Add link icon next to id input when there is an associated server section with that id

--- inst/editor/src/SettingsPanel/SettingsInput/IdInput.tsx
+++ inst/editor/src/SettingsPanel/SettingsInput/IdInput.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { Link45deg } from "react-bootstrap-icons";
+
 import { getAllBindingIds } from "../../EditorContainer/getAllBindingIds";
 import { useCurrentAppInfo } from "../../state/app_info";
 import type { InputComponentByType } from "../../ui-node-definitions/inputFieldTypes";
 import { makeLabelId } from "../../ui-node-definitions/inputFieldTypes";
+import { useUpToDateServerLocations } from "../useUpToDateServerLocations";
 
 export function IdInput({
   id,
@@ -16,11 +19,17 @@ export function IdInput({
   // UI tree down instead. Right now performance seems fine though especially
   // since this is a leaf node
   const appInfo = useCurrentAppInfo();
+  const serverLocations = useUpToDateServerLocations();
 
   const [currValue, setCurrValue] = React.useState(value);
   const [invalidMsg, setInvalidMsg] = React.useState<null | string>(null);
 
   if (appInfo.mode !== "MAIN") return null;
+
+  // Check if the current value exists in the server locations
+  const boundToServer =
+    serverLocations?.input_positions[value] !== undefined ||
+    serverLocations?.output_positions[value] !== undefined;
 
   const app_tree = appInfo.ui_tree;
 
@@ -61,7 +70,10 @@ export function IdInput({
 
   return (
     <>
-      <input {...common_props} type="text" />
+      <div className="flex items-center gap-1">
+        <input {...common_props} type="text" />
+        {boundToServer && <Link45deg className="text-xl text-rstudio-blue" />}
+      </div>
       {invalidMsg && (
         <div className="text-danger">
           <small>{invalidMsg}</small>
