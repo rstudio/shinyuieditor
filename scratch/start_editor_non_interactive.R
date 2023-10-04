14fe51e983b094409620046aab138c573cc74c10
Nick Strayer <nick.strayer@rstudio.com>
2023-10-04 13:48:19 -0400

    Reset running script to dev mode

--- scratch/start_editor_non_interactive.R
+++ scratch/start_editor_non_interactive.R
@@ -3,6 +3,6 @@ devtools::load_all(".")
 launch_editor(
   app_loc =  here::here("scratch/a-brand-new-app3"),
   port = 8888,
-  # launch_browser = FALSE,
-  # stop_on_browser_close = FALSE
+  launch_browser = FALSE,
+  stop_on_browser_close = FALSE
 )
