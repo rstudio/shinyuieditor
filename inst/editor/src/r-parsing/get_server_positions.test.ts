d49871b62d3fc14b502f43a138ccac10ddeb657b
Nick Strayer <nick.strayer@rstudio.com>
2023-10-10 11:07:11 -0400

    Strip out some left-over multifile logic

--- inst/editor/src/r-parsing/get_server_positions.test.ts
+++ inst/editor/src/r-parsing/get_server_positions.test.ts
@@ -1,44 +1,8 @@
 import { setup_r_parser } from "treesitter-parsers";
 
 import { getServerPositions } from "./get_server_positions";
-import { findServerDefInRApp } from "./parse_multifile_r_apps";
 import { parseRApp } from "./parse_r_app";
 import { parseRScript } from "./parse_r_script";
-
-describe("Can find output positions in server of multifile app", async () => {
-  const my_parser = await setup_r_parser();
-  const server_node = findServerDefInRApp(
-    parseRScript(
-      my_parser,
-      `library(ggplot2)
-        
-        server <- function(input, output) {
-          output$dists <- renderPlot({
-            ggplot(
-              ChickWeight,
-              aes(x = weight)
-            ) +
-              facet_wrap(input$distFacet) +
-              geom_density(fill = "#fa551b", color = "#ee6331") +
-              ggtitle(paste("Distribution of weights by", input$name)))
-          })
-        }`
-    ).rootNode
-  );
-
-  const { input_positions, output_positions } = getServerPositions(server_node);
-
-  test("Output bindings", () => {
-    expect(output_positions.has("dists")).toBe(true);
-    expect(output_positions.has("distFacet")).toBe(false);
-  });
-
-  test("Input bindings", () => {
-    expect(input_positions.has("distFacet")).toBe(true);
-    expect(input_positions.has("name")).toBe(true);
-    expect(input_positions.has("dists")).toBe(false);
-  });
-});
 
 describe("Can find output positions in server of single file app", async () => {
   const { server_node } = parseRApp(
--- inst/editor/src/r-parsing/index.ts
+++ inst/editor/src/r-parsing/index.ts
@@ -3,7 +3,6 @@ export type Primatives = string | number | boolean;
 export { generateRAppScriptTemplate } from "./generate_app_script_template";
 export { generate_r_output_binding } from "./generate_output_binding";
 
-export { findUiDefInRApp as get_ui_node_from_r_multifile_app } from "./parse_multifile_r_apps";
 // export { get_server_positions } from "./get_server_positions";
 // export { find_ui_and_server_in_singlefile_app } from "./find_ui_and_server_in_singlefile_app";
 // export { find_ui_and_server_in_multifile_r_app } from "./parse_multifile_r_apps";
