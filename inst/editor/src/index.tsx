aef1a8f2ac25b17fb170460d3d1c22f0d34f6826
Nick Strayer <nick.strayer@rstudio.com>
2023-10-11 14:14:58 -0400

    Setup newer version of nav_panel app

--- inst/editor/src/index.tsx
+++ inst/editor/src/index.tsx
@@ -20,6 +20,69 @@ import { runSUE } from "./runSUE";
 // const language: LanguageMode = "PYTHON";
 // const language: LanguageMode = "R";
 
+// const devModeApp = `library(shiny)
+// library(bslib)
+// library(ggplot2)
+
+// # install.packages("palmerpenguins")
+// data(penguins, package = "palmerpenguins")
+
+// ui <- page_navbar(
+//   title = "Penguins dashboard",
+//   sidebar = selectInput(
+//     selected = "species",
+//     inputId = "color_by",
+//     label = "Color by",
+//     choices = c(
+//       "species",
+//       "island",
+//       "sex"
+//     )
+//   ),
+//   collapsible = nav_spacer(),
+//   nav_panel(
+//         "Bill Length",
+//         card(
+//           full_screen = TRUE,
+//           card_header("Bill Length"),
+//           plotOutput("bill_length")
+//         )
+//       ),
+//   nav_panel(
+//         "Bill Depth",
+//         card(
+//           full_screen = TRUE,
+//           card_header("Bill depth"),
+//           plotOutput("bill_depth")
+//         )
+//       ),
+//   nav_panel(
+//         "Body Mass",
+//         card(
+//           full_screen = TRUE,
+//           card_header("Body Mass"),
+//           plotOutput("body_mass")
+//         )
+//       ),
+//   nav_item(tags$a("Posit", href = "https://posit.co"))
+// )
+
+// server <- function(input, output) {
+//   gg_plot <- reactive({
+//     ggplot(penguins) +
+//       geom_density(aes(fill = !!input$color_by), alpha = 0.2) +
+//       theme_bw(base_size = 16) +
+//       theme(axis.title = element_blank())
+//   })
+
+//   output$bill_length <- renderPlot(gg_plot() + aes(bill_length_mm))
+//   output$bill_depth <- renderPlot(gg_plot() + aes(bill_depth_mm))
+//   output$body_mass <- renderPlot(gg_plot() + aes(body_mass_g))
+// }
+
+// shinyApp(ui, server)
+// `;
+
 const devModeApp = `library(shiny)
 library(bslib)
 library(ggplot2)
@@ -29,42 +92,45 @@ data(penguins, package = "palmerpenguins")
 
 ui <- page_navbar(
   title = "Penguins dashboard",
-  sidebar = selectInput(
-    selected = "species",
-    inputId = "color_by",
-    label = "Color by",
-    choices = c(
-      "species",
-      "island",
-      "sex"
+  collapsible = nav_spacer(),
+  sidebar = sidebar(
+    title = "Sidebar Title",
+    selectInput(
+       selected = "species",
+       inputId = "color_by",
+       label = "Color by",
+       choices = list(
+         "species" = "species",
+         "island" = "island",
+         "sex" = "sex"
+       )
+     ),
+  ),
+  nav_panel(
+    title = "Bill Length",
+    card(
+      full_screen = TRUE,
+      card_header("Bill Length"),
+      plotOutput(outputId = "bill_length")
     )
   ),
-  collapsible = nav_spacer(),
   nav_panel(
-        "Bill Length", 
-        card(
-          full_screen = TRUE,
-          card_header("Bill Length"),
-          plotOutput("bill_length")
-        )
-      ),
+    title = "Bill Depth",
+    card(
+      full_screen = TRUE,
+      card_header("Bill depth"),
+      card_body(),
+      plotOutput(outputId = "bill_depth")
+    )
+  ),
   nav_panel(
-        "Bill Depth", 
-        card(
-          full_screen = TRUE,
-          card_header("Bill depth"),
-          plotOutput("bill_depth")
-        )
-      ),
-  nav_panel(
-        "Body Mass", 
-        card(
-          full_screen = TRUE,
-          card_header("Body Mass"),
-          plotOutput("body_mass")
-        )
-      ),
-  nav_item(tags$a("Posit", href = "https://posit.co"))
+    title = "Body Mass",
+    card(
+      full_screen = TRUE,
+      card_header("Body Mass"),
+      plotOutput(outputId = "body_mass")
+    )
+  )
 )
 
 server <- function(input, output) {
@@ -80,9 +146,7 @@ server <- function(input, output) {
   output$body_mass <- renderPlot(gg_plot() + aes(body_mass_g))
 }
 
-shinyApp(ui, server)
-`;
-
+shinyApp(ui, server)`;
 const language: LanguageMode = "R";
 const app_script =
   DEV_MODE && typeof devModeApp !== "string"
