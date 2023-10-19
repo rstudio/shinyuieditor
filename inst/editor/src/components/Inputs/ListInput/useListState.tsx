28e7aa83631224274700d69692f5cd9db6734661
Nick Strayer <nick.strayer@rstudio.com>
2023-10-19 15:01:07 -0400

    Simplify useListState to derive almost all of its state instead of having to keep ui tree and internal state in sync

--- inst/editor/src/components/Inputs/ListInput/useListState.tsx
+++ inst/editor/src/components/Inputs/ListInput/useListState.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-import { sameObject } from "util-functions/src/equalityCheckers";
-
 import type {
   InputComponentByType,
   NamedList,
 } from "../../../ui-node-definitions/inputFieldTypes";
 
-export type ItemArray = {
+type ItemArray = {
   id: number;
   key: string;
   value: string;
 }[];
 
-type NewItemValue = Required<InputComponentByType<"list">>["newItemValue"];
+type ListMode = "key-value" | "value-only";
 
 /**
  * Go from the internal state representation of an array of items to the
@@ -25,9 +23,9 @@ type NewItemValue = Required<InputComponentByType<"list">>["newItemValue"];
  */
 function itemTypeArrayToNamedList(
   array: ItemArray,
-  valueOnlyMode: boolean
+  listMode: ListMode
 ): NamedList {
-  if (valueOnlyMode) {
+  if (listMode === "value-only") {
     return array.map((item) => item.value);
   }
 
@@ -54,6 +52,39 @@ export function namedListToItemTypeArray(list: NamedList): ItemArray {
   }));
 }
 
+function isValueOnlyMode(value: NamedList): value is string[] {
+  return Array.isArray(value);
+}
+
+/**
+ * Update the key of an object while making sure the position of the item in the
+ * object is preserved
+ * @param obj object keyed by a string
+ * @param oldKey Previous key value
+ * @param newKey Key value to update to
+ * @returns A new object with the key updated to new value
+ */
+function swapKeysForObject<ValueType>(
+  obj: Record<string, ValueType>,
+  oldKey: string,
+  newKey: string
+): Record<string, ValueType> {
+  // Convert object to array of key-value pairs
+  const entries = Object.entries(obj);
+
+  // Find the index of the item with the old key
+  const index = entries.findIndex(([key]) => key === oldKey);
+
+  // Update the key of the item at the index
+  entries[index][0] = newKey;
+
+  // Repack the array of key-value pairs into an object
+  return entries.reduce((obj, [key, value]) => {
+    obj[key] = value;
+    return obj;
+  }, {} as Record<string, ValueType>);
+}
+
 export function useListState({
   value,
   onChange,
@@ -61,194 +92,186 @@ export function useListState({
 }: {
   value: NamedList;
   onChange: (x: NamedList) => void;
-  newItemValue: NewItemValue;
+  newItemValue: Required<InputComponentByType<"list">>["newItemValue"];
 }) {
   // value-only to avoid so many negative conditions
-  const [valueOnlyMode, setValueOnlyMode] = React.useState<boolean>(false);
   const [keyValueMismatches, setKeyValueMismatches] =
     React.useState<ItemArray | null>(null);
 
-  const [state, setState] = React.useState<ItemArray>([]);
-
-  const numItems = state.length;
-
-  // Make sure we start with the correct mode for the value type
-  React.useEffect(() => {
-    setValueOnlyMode(Array.isArray(value));
-  }, [value]);
-
-  // The purpose of this useEffect is to handle updating the state to reflect
-  // new values sent in via the value prop. This is to keep the state in sync
-  // with the value prop so we don't have stale arguments. There is probably
-  // a cleaner way to do this but I'm not sure what it is.
-  React.useEffect(() => {
-    const valuesFromState = itemTypeArrayToNamedList(state, valueOnlyMode);
-
-    if (sameObject(valuesFromState, value)) {
-      // Same array detected. No changes should be made
-      return;
-    }
-
-    setState(namedListToItemTypeArray(value));
-  }, [state, value, valueOnlyMode]);
-
-  React.useEffect(() => {
-    if (!valueOnlyMode) {
-      return;
-    }
-
-    const mismatches = getKeyValueMismatches(state);
-
-    if (mismatches === null) {
-      return;
-    }
-
-    setKeyValueMismatches(mismatches);
-  }, [state, valueOnlyMode]);
+  const listMode = isValueOnlyMode(value) ? "value-only" : "key-value";
 
   const deleteItem = React.useCallback(
     (itemId: number) => {
+      if (isValueOnlyMode(value)) {
+        const newValue = [...value];
+
+        newValue.splice(itemId, 1);
+
+        onChange(newValue);
+
+        return;
+      }
+
       const newValue = { ...value };
 
-      if (Array.isArray(newValue)) {
-        newValue.splice(itemId, 1);
-      } else {
-        delete newValue[state[itemId].key];
-      }
+      // Key to delete
+      delete newValue[Object.keys(newValue)[itemId]];
 
       onChange(newValue);
     },
-    [onChange, state, value]
+    [onChange, value]
   );
 
   // Adding an item resets all the ids so we dont get crazy ids if the list has
   // been edited a lot
   const addItem = React.useCallback(() => {
+    const numItems = isValueOnlyMode(value)
+      ? value.length
+      : Object.keys(value).length;
+
     const newItem =
       typeof newItemValue === "function"
         ? newItemValue(numItems + 1)
         : newItemValue;
 
-    onChange({ ...value, [newItem.key]: newItem.value });
-  }, [newItemValue, numItems, onChange, value]);
+    if (isValueOnlyMode(value)) {
+      onChange([...value, newItem.value]);
+    } else {
+      onChange({ ...value, [newItem.key]: newItem.value });
+    }
+  }, [newItemValue, onChange, value]);
 
   const reorderItems = React.useCallback(
     (newItemsOrder: ItemArray) => {
       // The first time this is called it's an empty array so we can ignore that
       if (newItemsOrder.length === 0) return;
 
-      onChange(itemTypeArrayToNamedList(newItemsOrder, valueOnlyMode));
+      onChange(itemTypeArrayToNamedList(newItemsOrder, listMode));
     },
-    [onChange, valueOnlyMode]
+    [listMode, onChange]
   );
 
   const updateKey = React.useCallback(
     ({ index: i, newKey }: { index: number; newKey: string }) => {
-      // Copy the state array so we can modify it
-      const newState = [...state];
-
-      // Update the item we want to change's key
-      newState[i] = { ...newState[i], key: newKey };
-
-      // If the new key is already in the list, we need to delete the
-      // old item with that key.
-      // TODO: Pause here and give a validation error until the user has updated the key
-      const itemWithSameKeyIndex = newState.findIndex(
-        (item, j) => item.key === newKey && j !== i
-      );
-      if (itemWithSameKeyIndex !== -1) {
-        newState.splice(itemWithSameKeyIndex, 1);
+      if (isValueOnlyMode(value)) {
+        throw new Error("Cannot update key in value-only mode");
       }
+
+      // Make sure previous key isn't in the list
+      if (newKey in value) {
+        throw new Error("Key already exists");
+      }
+
+      // Get previous key value
+      const prevKey = Object.keys(value)[i];
+
+      const newValue = swapKeysForObject(value, prevKey, newKey);
 
       // Now convert the state array back into a NamedList and update
       // the value
-      onChange(itemTypeArrayToNamedList(newState, valueOnlyMode));
+      onChange(newValue);
     },
-    [onChange, state, valueOnlyMode]
+    [onChange, value]
   );
 
   const updateValue = React.useCallback(
     ({ index: i, newValue }: { index: number; newValue: string }) => {
-      // Copy the state array so we can modify it
-      const newState = [...state];
+      if (isValueOnlyMode(value)) {
+        const newValueArray = [...value];
 
-      // Update the item we want to change's key
-      newState[i] = { ...newState[i], value: newValue };
+        newValueArray[i] = newValue;
 
-      // Keep key in sync with value if we're not in the keyInValue mode
-      if (valueOnlyMode) {
-        newState[i] = { ...newState[i], key: newValue };
+        onChange(newValueArray);
+
+        return;
       }
 
-      // Now convert the state array back into a NamedList and update
-      // the value
-      onChange(itemTypeArrayToNamedList(newState, valueOnlyMode));
+      // Key-Value mode
+      const newList = { ...value };
+
+      const prevKey = Object.keys(value)[i];
+      newList[prevKey] = newValue;
+
+      onChange(newList);
     },
-    [valueOnlyMode, onChange, state]
+    [onChange, value]
   );
+
+  const onValueModeToggle = (simpleMode: boolean) => {
+    if (simpleMode) {
+      // If we're going into simple mode we need to check for mismatches
+
+      if (isValueOnlyMode(value)) {
+        throw new Error("Trying to simplify a value-only list");
+      }
+
+      const keyValueMismatches = getKeyValueMismatches(value);
+
+      if (keyValueMismatches !== null) {
+        // If there are mismatches, we need to show the popup
+        setKeyValueMismatches(keyValueMismatches);
+
+        return;
+      }
+    }
+
+    // Make change to data-type
+    swapKeyValueMode(simpleMode ? "value-only" : "key-value");
+  };
 
   // If the user has entered the simple only-value mode, we need to ensure that
   // the keys and values are the same. The value takes precedence over the key,
   // so we need to update the keys to match the values
-  const mergeKeysAndValues = React.useCallback(() => {
-    const newState = [...state];
+  const swapKeyValueMode = React.useCallback(
+    (mode: ListMode) => {
+      const valueOnlyMode = isValueOnlyMode(value);
 
-    newState.forEach((item) => {
-      item.key = item.value;
-    });
+      if (mode === (valueOnlyMode ? "value-only" : "key-value")) {
+        throw new Error("Trying to update to the same list mode");
+      }
 
-    onChange(itemTypeArrayToNamedList(newState, true));
-    setValueOnlyMode(true);
-    setKeyValueMismatches(null);
-  }, [onChange, state]);
+      const newValue = valueOnlyMode
+        ? value.reduce<Record<string, string>>((obj, value) => {
+            obj[value] = value;
+            return obj;
+          }, {})
+        : Object.values(value);
+
+      onChange(newValue);
+      setKeyValueMismatches(null);
+    },
+    [onChange, value]
+  );
 
   const onCancelSimplify = () => {
     setKeyValueMismatches(null);
   };
 
-  const onValueModeToggle = (simpleMode: boolean) => {
-    if (!simpleMode) {
-      // We can always go to the more complicated mode
-      setValueOnlyMode(false);
-      return;
-    }
-
-    const keyValueMismatches = getKeyValueMismatches(state);
-
-    if (keyValueMismatches === null) {
-      // If the keys and values are the same, we can always go to the simple mode
-      setValueOnlyMode(true);
-      return;
-    }
-
-    // If we can't go to the simple mode, we need to show the mismatch message
-    setKeyValueMismatches(keyValueMismatches);
-  };
-
   return {
-    state,
-    setState,
+    flatList: namedListToItemTypeArray(value),
     reorderItems,
     deleteItem,
     addItem,
     updateKey,
     updateValue,
-    mergeKeysAndValues,
-    valueOnlyMode,
+    swapKeyValueMode,
+    listMode,
     onValueModeToggle,
     keyValueMismatches,
     onCancelSimplify,
   };
 }
 
-function getKeyValueMismatches(x: ItemArray): ItemArray | null {
-  const mismatches = x.filter(
-    (item) => item.key !== item.value && item.key !== ""
-  );
+function getKeyValueMismatches(x: Record<string, unknown>): ItemArray | null {
+  const mismatches = Object.entries(x)
+    .filter(([key, value]) => typeof value === "string" && key !== value)
+    .map(([key, value], i) => ({ key, value, id: i }));
 
   if (mismatches.length === 0) {
     return null;
   }
 
-  return mismatches;
+  // Forcing this type is okay because it already has been type checked because we said
+  // key
+  return mismatches as ItemArray;
 }
