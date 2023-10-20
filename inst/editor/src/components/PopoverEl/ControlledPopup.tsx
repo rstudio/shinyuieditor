4dcd80501d7e802854a6d87af28a836c91fdcf23
Nick Strayer <nick.strayer@rstudio.com>
2023-10-20 11:50:10 -0400

    Put key-value mode toggle behind a settings modal.

--- inst/editor/src/components/PopoverEl/ControlledPopup.tsx
+++ inst/editor/src/components/PopoverEl/ControlledPopup.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import type { Placement } from "@floating-ui/react";
+
 import { mergeClasses } from "../../utils/mergeClasses";
 
 import { Tooltip, TooltipContent, TooltipTrigger } from "./FloatingPopover";
@@ -11,34 +13,43 @@ const accentBarStyles = {
   info: "bg-rstudio-blue",
 };
 
+type ControlledPopeverProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  content: React.ReactNode;
+  accent?: keyof typeof accentBarStyles;
+  placement?: Placement;
+  description: string;
+};
+
 export function ControlledPopup({
   isOpen,
   onClose,
   children,
   content,
   accent,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-  content: React.ReactNode;
-  accent?: keyof typeof accentBarStyles;
-}) {
+  placement = "left",
+  description,
+}: ControlledPopeverProps) {
   return (
     <Tooltip
       open={isOpen}
       // By letting the tooltip have this control we can make sure that the
       // user can close the tooltip by clicking outside of it
       onOpenChange={() => onClose()}
-      placement="left"
+      placement={placement}
     >
-      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipTrigger asChild noToggle>
+        {children}
+      </TooltipTrigger>
 
       <TooltipContent>
         <div
           className={mergeClasses(
             "p-2 rounded-standard bg-rstudio-white w-64 relative"
           )}
+          aria-label={description}
         >
           {accent && (
             <div
@@ -52,5 +63,25 @@ export function ControlledPopup({
         </div>
       </TooltipContent>
     </Tooltip>
+  );
+}
+
+// TODO: Flesh this out with associated trigger and content components
+// in the namespaced ClickToOpenPopover.Trigger and ClickToOpenPopover.Content format
+export function ClickToOpenPopover({
+  onClose,
+  ...props
+}: Omit<ControlledPopeverProps, "isOpen">) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <ControlledPopup
+      isOpen={isOpen}
+      onClose={() => {
+        setIsOpen(false);
+        onClose();
+      }}
+      {...props}
+    />
   );
 }
